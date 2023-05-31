820fd4daeda6db46a73bc80ae6abb48aad688f3d
Steven Stackhouse <stackmm@gmail.com>
2023-05-31 14:00:33 -0600

    add relationships to Invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,9 @@
 class Invoice < ApplicationRecord
+  belongs_to :customer
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
+
   enum status: {"in progress": 0, "completed": 1, "cancelled": 2}
 end
