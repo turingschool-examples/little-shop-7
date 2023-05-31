2a3f463ed00496ad7fdfc8d0c60ddd54eb9bccbe
Julian Beldotti <julian.beldotti@gmail.com>
2023-05-31 18:58:41 -0500

    Test: Add tests for adding links to the admin dashboard

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -3,9 +3,17 @@ require "rails_helper"
 RSpec.describe "/admin", type: :feature do
   describe "admin dashboard" do
     it "will display a header indicating you are on the dashboard" do
-      visit "/admin"
+      visit admin_path
       
       expect(page).to have_content("Admin Dashboard")
+    end
+    
+    it "should display links to admin/merchants and admin/invoices admin/dashboard" do
+      visit admin_path
+      save_and_open_page
+      expect(page).to have_link("Dashboard", :href => admin_path)
+      expect(page).to have_link("Merchants", :href => admin_merchants_path)
+      expect(page).to have_link("Invoices", :href => admin_invoices_path)
     end
   end
 end
