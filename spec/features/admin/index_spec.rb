05b606905fba21923546a259bf06be349888429b
JULIAN BELDOTTI <121738722+JCBeldo@users.noreply.github.com>
2023-05-31 18:04:30 -0500

    Update index_spec.rb
    Removed commented out user story

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,11 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "/admin", type: :feature do
-  # 19. Admin Dashboard
-
-  # As an admin,
-  # When I visit the admin dashboard (/admin)
-  # Then I see a header indicating that I am on the admin dashboard
   describe "admin dashboard" do
     it "will display a header indicating you are on the dashboard" do
       visit "/admin"
