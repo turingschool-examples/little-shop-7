ad66b361a137725a77704b719d4fa1d6bb72e73f
Nick Sacco <nicksacco10@gmail.com>
2023-06-01 15:22:17 -0700

    us26 wip

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -17,10 +17,24 @@ RSpec.describe "Admin Merchant Show Page", type: :feature do
 
       visit admin_merchants_path
       click_link(@merchant_2.name)
-
+      
       expect(current_path).to eq(admin_merchant_path(@merchant_2))
       expect(page).to have_content("#{@merchant_2.name}'s Show Page")
       expect(page).to_not have_content("#{@merchant_1.name}'s Show Page")
+    end
+  end
+  
+  describe "Show page has a link to update a specific merchant" do
+    it "can click a link to edit merchant that leads to an edit form" do
+      visit admin_merchant_path(@merchant_1)
+      
+      expect(page).to have_link("Update #{@merchant_1.name}")
+      expect(page).to_not have_link("Update #{@merchant_2.name}")
+      
+      click_link("Update #{@merchant_1.name}")
+
+      expect(current_path).to eq()
+
     end
   end
 end
