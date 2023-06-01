b10ffa7dc68b027d436fa82632fa9caa703abdcd
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-01 16:05:59 -0600

    fix index action

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -1,5 +1,5 @@
 class Merchants::ItemsController < ApplicationController
   def index
-    @merchants = Merchant.find(params[:merchant_id])
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
