84c99354afbd74821d9771de6731fac332e49a00
Austin Woods <austinwoods99@gmail.com>
2023-06-01 10:12:14 -0400

    Complete user story 2

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -8,5 +8,13 @@ RSpec.describe "Merchant Dashboard" do
 
       expect(page).to have_content("Dealer of Death")
     end
+
+    it "has links to invoices index" do 
+      merchant = Merchant.create!(id: 1, name: "Dealer of Death", status: 1)
+      visit "/merchants/#{merchant.id}/dashboard"
+
+      expect(page).to have_link("My Invoices")
+      expect(page).to have_link("My Items")
+    end
   end
 end
