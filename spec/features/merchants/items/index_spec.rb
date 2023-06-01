e746292b087acfd5802fabcea946aa105d96baf0
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-01 14:03:37 -0600

    delete save and open page

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "Merchant Items Index Page" do
     # User Story 6
     it "displays a list of the names of all my items" do
       visit merchants_items_path(merchant_id: @merchant.id) #since the index page is nested under a specific merchant, need to provide the merchant_id as a parameter to generate the correct URL. The merchant_id: @merchant.id part is specifying the value for the merchant_id parameter in the URL.
-save_and_open_page
+
       expect(page).to have_content("My Items")
       expect(page).to have_content(@item_1.name)
       expect(page).to_not have_content(@item_2.name)
