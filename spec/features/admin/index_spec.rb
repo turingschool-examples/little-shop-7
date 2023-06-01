9cbdab543bb39d38cd3f05b787b5982ef57cd606
Steven Stackhouse <stackmm@gmail.com>
2023-06-01 16:30:14 -0600

    create admin index page test for merchants and invoices links

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -6,12 +6,28 @@ RSpec.describe "Admin Dashboard Index Page" do
   end
 
   describe "Admin Dashboard Display" do
-    
     # User Story 19
     it "displays a header indicating that it is the admin dashboard" do
       visit admin_path
 
       expect(page).to have_content("Admin Dashboard")
+    end
+
+    # User Story 20
+    it "displays a link to the admin merchants index page" do
+      visit admin_path
+
+      expect(page).to have_link("Merchants", href: "/admin/merchants")
+      click_on("Merchants")
+      expect(page).to have_current_path("/admin/merchants")
+    end
+
+    it "displays a link to the admin invoices index page" do
+      visit admin_path
+
+      expect(page).to have_link("Invoices", href: "/admin/invoices")
+      click_on("Invoices")
+      expect(page).to have_current_path("/admin/invoices")
     end
   end
 end
