f2f80c9ed51ddfd2beb9a7215677be4e05ec302e
Bradley Milton <bmilton2412@gmail.com>
2023-05-31 18:48:08 -0700

    fixed invoice model status in progress to a string

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,6 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  # enum :status, ["pending", "packaged", "shipped"]
-  enum status: { in_progress: 0, completed: 1, cancelled: 2}
+  # enum :status ["pending", "packaged", "shipped"]
+  enum status: { "in progress": 0, completed: 1, cancelled: 2}
 end
