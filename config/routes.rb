1931dcaff1381ff7eb328901135a1837bc41a372
Garrett Gregor <garrett.gregor@gmail.com>
2023-06-01 17:18:38 -0600

    Refactor: update admin resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/", to: "application#welcome"
-  get "/admin", to: "admin#index"
+  # get "/admin", to: "admin#index"
 
+  resources :admin, only: [:index]
   namespace :admin do
     resources :merchants, only: [:index]
     resources :invoices, only: [:index]
