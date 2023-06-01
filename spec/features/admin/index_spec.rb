c5a2d67df506a0cc640a73b78c1b7cf62a87c201
JULIAN BELDOTTI <121738722+JCBeldo@users.noreply.github.com>
2023-05-31 19:06:20 -0500

    Update index_spec.rb
    Move save_and_open_page and update it block statement

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -8,12 +8,13 @@ RSpec.describe "/admin", type: :feature do
       expect(page).to have_content("Admin Dashboard")
     end
     
-    it "should display links to admin/merchants and admin/invoices admin/dashboard" do
+    it "should display links to admin/merchants, admin/invoices, and admin/dashboard" do
       visit admin_path
-      save_and_open_page
+      
       expect(page).to have_link("Dashboard", :href => admin_path)
       expect(page).to have_link("Merchants", :href => admin_merchants_path)
       expect(page).to have_link("Invoices", :href => admin_invoices_path)
     end
   end
 end
+#save_and_open_page
