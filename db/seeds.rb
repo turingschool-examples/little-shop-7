86f79c69142222d69798153077ffbb5a9cf5e411
Bradley Milton <bmilton2412@gmail.com>
2023-05-31 18:11:00 -0700

    adds master key to credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+{Transactioin}.destroy_all
+{Merchant}.destroy_all
+{Item}.destroy_all
+{Invoice}.destroy_all
+{InvoiceItem}.destroy_all
+{Customer}.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
