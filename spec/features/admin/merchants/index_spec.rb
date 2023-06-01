6c203679ec6a00368e42b3bc4d54cee32653360b
Nick Sacco <nicksacco10@gmail.com>
2023-06-01 11:26:29 -0700

    test: links to merchant show page tests

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -9,11 +9,21 @@ RSpec.describe "Admin Merchant Index Page", type: :feature do
   describe "When I visit the admin merchants index" do
     it "displays the name of each merchant in the system" do
       visit admin_merchants_path
-
+      
       expect(page).to have_content("Admin Merchant Index Page")
       expect(page).to have_content("Merchants:")
       expect(page).to have_content(@merchant_1.name)
       expect(page).to have_content(@merchant_2.name)
+    end
+    
+    it "each merchant name is a link to that merchant's show page" do
+      visit admin_merchants_path
+
+      expect(page).to have_link(@merchant_1.name)
+      expect(page).to have_link(@merchant_2.name)
+
+      click_link(@merchant_1.name)
+      expect(current_path).to eq("admin/merchants/#{@merchant_1.id}")
     end
   end
 end
