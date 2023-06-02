99c1cc6c2a746e0c6e8ee74949fec69ab65c3fe0
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-01 18:38:41 -0600

    test: add setup and tests for merchant itmes show page

---
+++ spec/features/merchants/items/show_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Items Show Page" do
+
+
+  describe "Merchant Items Show Display", type: :feature do
+    before(:each) do 
+      @merchant_1 = create(:merchant)
+      @merchant_2 = create(:merchant)
+      @item_1 = create(:item, merchant: @merchant_1)
+      @item_2 = create(:item, merchant: @merchant_1) 
+      @item_3 = create(:item, merchant: @merchant_2) 
+    end
+
+    #User Story 7
+    it "displays all attributes of the item" do
+      visit merchant_items_path(@merchant_1)
+
+      click_link @item_1.name
+
+      expect(page).to have_content(@item_1.name)
+      expect(page).to have_content(@item_1.descriptions)
+      expect(page).to have_content(@item_1.current_selling_price)
+    end
+  end
+end
