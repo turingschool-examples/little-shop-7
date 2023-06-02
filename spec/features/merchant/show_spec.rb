8d249dbfcc4156c03b6d7b2291c5b4f2a8bfabe9
Amy Spears <amspears007@gmail.com>
2023-06-02 16:10:46 -0600

    Add correct do and end to describe block and remove ! in view page

--- spec/features/merchant/show_spec.rb
+++ spec/features/merchant/show_spec.rb
@@ -8,13 +8,15 @@ RSpec.describe 'Merchant Dashboard' do
   describe "US1 '/merchants/:merchant_id/dashboard'" do 
     it 'merchant dashboard shows the name of the merchant' do
       visit "/merchants/#{@merchant_1.id}/dashboard" 
+      
       expect(page).to have_content("Welcome back, #{@merchant_1.name}")
     end
   end
 
-  describe "US2 /merchants/:merchant_id/dashboard"
+  describe "US2 /merchants/:merchant_id/dashboard" do
     it 'merchant dashboard items links' do 
       visit "/merchants/#{@merchant_1.id}/dashboard" 
+
       expect(page).to have_link('My Items')
       click_link('My Items')
       expect(current_path).to eq("/merchants/#{@merchant_1.id}/items")
@@ -22,8 +24,10 @@ RSpec.describe 'Merchant Dashboard' do
     
     it 'merchant dashboard invoices link' do 
       visit "/merchants/#{@merchant_1.id}/dashboard" 
+
       expect(page).to have_link('My Invoices')
       click_link('My Invoices')
       expect(current_path).to eq("/merchants/#{@merchant_1.id}/invoices")
     end
+  end
 end
