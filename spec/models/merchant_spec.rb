38e74a8e577a21fc1959ff7d78a8d9e2c8b57ca4
Nick Sacco <nicksacco10@gmail.com>
2023-06-02 16:01:50 -0700

    test: merchant enabled and disabled model test setup

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Merchant, type: :model do
   describe "Validations" do
     it { should validate_presence_of :name }
   end
+
+  describe "Class Methods" do
+    describe ".enabled_merchants" do
+      it "groups merchants based on their enabled status" do
+        expect(Merchant.enabled_merchants).to eq()
+      end
+
+      xit "groups merchants based on their disabled status" do
+        expect(Merchant.disabled_merchants).to eq()
+      end
+    end
+  end
 end
