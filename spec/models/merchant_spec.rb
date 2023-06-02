0b2f6f3fa1a44d9874f4dea34129d9bf9f31c9dd
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-02 12:51:00 -0600

    Test: merchant can be created (debugging)

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Merchant, type: :model do
   describe 'relationships' do 
     it {should have_many(:items)}
   end
+
+  describe 'existence' do 
+    it 'can be instantiated' do 
+      merchant = Merchant.create!(name: 'Helena Nabaoth')
+    end
+  end
 end
