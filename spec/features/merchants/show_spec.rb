e2d2872ec2938f21dff4e9f2a4aa7e3b99822023
Jesse Thomas <jgthomas3@gmail.com>
2023-06-02 17:18:46 -0600

    Test: add merchant show_spec

---
+++ spec/features/merchants/show_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "/merchants/:id/dashboard" do
+  describe "As a visitor" do
+    describe "when I visit a merchants show page" do
+      let!(:merchant_1) { create(:merchant) }
+      let!(:merchant_2) { create(:merchant) }
+
+      # User Story 1
+
+      it "directs me to the merchants dashboard where I see the merchants name" do
+        visit "/merchants/#{merchant_1.id}/dashboard"
+        expect(page).to have_content(merchant_1.name)
+        expect(page).to_not have_content(merchant_2.name)
+      end
+
+    end
+  end
+end
