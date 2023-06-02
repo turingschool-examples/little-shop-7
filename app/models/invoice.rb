5c9318f9a1ec7b773e2bd3fddfeeb5e639dc6d71
Amy Spears <amspears007@gmail.com>
2023-06-02 16:30:18 -0600

    Add all associations/relationships to merchant and invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,7 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  # enum :status ["pending", "packaged", "shipped"]
+  has_many :transactions
+  
   enum status: { "in progress": 0, completed: 1, cancelled: 2}
 end
