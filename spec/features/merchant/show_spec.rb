d5cc709d321737a0588df170c41259354361c8f8
Amy Spears <113188388+amspears007@users.noreply.github.com>
2023-06-02 14:52:24 -0600

    Update show_spec.rb
    Added US1 to describe block instead of leaving commented out

--- spec/features/merchant/show_spec.rb
+++ spec/features/merchant/show_spec.rb
@@ -4,9 +4,8 @@ RSpec.describe 'Merchant Dashboard' do
   before(:each) do 
     @merchant_1 = create(:merchant)
   end
-
-  #User Story 1
-  describe '/merchants/:merchant_id/dashboard' do 
+  
+  describe "US1 '/merchants/:merchant_id/dashboard'" do 
     it 'merchant dashboard shows the name of the merchant' do
       visit "/merchants/#{@merchant_1.id}/dashboard" 
       expect(page).to have_content("Welcome back, #{@merchant_1.name}")
