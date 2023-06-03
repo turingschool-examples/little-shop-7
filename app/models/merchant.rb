165afa01f02770fede0b994cad7862b1608c0927
Michael Callahan <mcalla242@gmail.com>
2023-06-03 09:40:11 -0600

    Add class method to filter Merchants by enabled and disabled status, and testing specs

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,14 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :items
   validates_presence_of :name
+
+  enum status: ["enabled", "disabled"] # enabled = 0, disabled = 1
+
+  def self.filter_enabled
+    Merchant.where(status: "enabled")
+  end
+
+  def self.filter_disabled
+    Merchant.where(status: "disabled")
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,9 +11,28 @@ RSpec.describe Merchant, type: :model do
   end
 
   before(:each) do 
-    @merchant = create(:merchant)
+    @merchant_1 = create(:merchant, status: 0)
+    @merchant_2 = create(:merchant, status: 0)
+    @merchant_3 = create(:merchant, status: 1)
   end
 
+  describe "#class methods" do
+    it "can filter records by enabled" do
+      original = [@merchant_1, @merchant_2, @merchant_3]
+      expected = [@merchant_1, @merchant_2]
+
+      expect(Merchant.all).to eq(original)
+      expect(Merchant.filter_enabled).to eq(expected)
+    end
+
+    it "can filter records by disabled" do
+      original = [@merchant_1, @merchant_2, @merchant_3]
+      expected = [@merchant_3]
+
+      expect(Merchant.all).to eq(original)
+      expect(Merchant.filter_disabled).to eq(expected)
+    end
+  end
 end
 
 
