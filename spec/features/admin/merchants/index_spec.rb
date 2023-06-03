53462a782a5aa52421da8470bf3c76b48ec2f546
Michael Callahan <mcalla242@gmail.com>
2023-06-03 10:02:18 -0600

    Add features for enabling and disabling merchants on admin index page, plus more test specs

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.describe "/admin/merchants" do
   describe "as an admin" do
-    let!(:merchant_1) { create(:merchant) }
-    let!(:merchant_2) { create(:merchant) }
-    let!(:merchant_3) { create(:merchant) }
+    let!(:merchant_1) { create(:merchant, status: 0) }
+    let!(:merchant_2) { create(:merchant, status: 0) }
+    let!(:merchant_3) { create(:merchant, status: 0) }
 
     it "displays the name of each merchant in the system" do
       visit "/admin/merchants"
 
-      within ".admin_merchants" do
+      within ".enabled_admin_merchants" do
         expect(page).to have_content("#{merchant_1.name}")
         expect(page).to have_content("#{merchant_2.name}")
         expect(page).to have_content("#{merchant_3.name}")
@@ -19,7 +19,7 @@ RSpec.describe "/admin/merchants" do
     it "displays a link to each of the merchants on the admin merchants index page" do
       visit "/admin/merchants"
 
-      within ".admin_merchants" do
+      within ".enabled_admin_merchants" do
         expect(page).to have_link("#{merchant_1.name}")
         expect(page).to have_link("#{merchant_2.name}")
         expect(page).to have_link("#{merchant_3.name}")
@@ -48,22 +48,26 @@ RSpec.describe "/admin/merchants" do
       expect(current_path).to eq("/admin/merchants")
 
       within ".enabled_admin_merchants" do
+        expect(page).to have_content("Enabled Merchants")
+        expect(page).to_not have_link("#{merchant_1.name}")
         expect(page).to have_link("#{merchant_2.name}")
         expect(page).to have_link("#{merchant_3.name}")
         
+        expect(page).to_not have_button("Disable #{merchant_1.name}")
         expect(page).to have_button("Disable #{merchant_2.name}")
         expect(page).to have_button("Disable #{merchant_3.name}")
       end
 
       within ".disabled_admin_merchants" do
+        expect(page).to have_content("Disabled Merchants")
         expect(page).to have_link("#{merchant_1.name}")
-        
-        expect(page).to have_button("Enable #{merchant_1.name}")
-      end
+        expect(page).to_not have_link("#{merchant_2.name}")
+        expect(page).to_not have_link("#{merchant_3.name}")
 
-      expect(merchant_1.status).to eq("disabled")
-      expect(merchant_2.status).to eq("enabled")
-      expect(merchant_3.status).to eq("enabled")
+        expect(page).to have_button("Enable #{merchant_1.name}")
+        expect(page).to_not have_button("Disable #{merchant_2.name}")
+        expect(page).to_not have_button("Disable #{merchant_3.name}")
+      end
     end
   end
 end
