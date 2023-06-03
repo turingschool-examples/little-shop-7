22a22f9109e3b8a7831f2320f1e89c813a219400
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-06-03 12:32:15 -0600

    FIx: remove unneccessary to_not have_content assertions for status and datetime attributes.

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -19,8 +19,6 @@ RSpec.describe "Admin Invoices Show Page" do
       expect(page).to have_content("Customer: #{@invoice_1.customer.first_name} #{@invoice_1.customer.last_name}")
 
       expect(page).to_not have_content(@invoice_2.id)
-      expect(page).to_not have_content(@invoice_2.status)
-      expect(page).to_not have_content(@invoice_2.formatted_time)
       expect(page).to_not have_content(@invoice_2.customer.first_name)
       expect(page).to_not have_content(@invoice_2.customer.last_name)
     end
