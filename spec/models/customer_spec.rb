5a6a2df2633c98c747016d3bb12d0f30fb05f262
Garrett Gregor <garrett.gregor@gmail.com>
2023-06-02 18:30:15 -0600

    Test: #successful_transaction_count

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -38,4 +38,37 @@ RSpec.describe Customer, type: :model do
     end
   end
 
+  describe "instance methods" do
+    let!(:customer_1) { create(:customer) }
+    let!(:customer_2) { create(:customer) }
+    let!(:customer_3) { create(:customer) }
+    let!(:customer_4) { create(:customer) }
+    let!(:customer_5) { create(:customer) }
+    let!(:customer_6) { create(:customer) }
+
+    let!(:invoice_1) { create(:invoice, customer_id: customer_1.id) }
+    let!(:invoice_2) { create(:invoice, customer_id: customer_2.id) }
+    let!(:invoice_3) { create(:invoice, customer_id: customer_3.id) }
+    let!(:invoice_4) { create(:invoice, customer_id: customer_4.id) }
+    let!(:invoice_5) { create(:invoice, customer_id: customer_5.id) }
+    let!(:invoice_6) { create(:invoice, customer_id: customer_6.id) }
+
+    let!(:trans_1_s) { create_list(:transaction, 1, result: 1, invoice_id: invoice_1.id) }
+    let!(:trans_2_s) { create_list(:transaction, 2, result: 1, invoice_id: invoice_2.id) }
+    let!(:trans_3_s) { create_list(:transaction, 3, result: 1, invoice_id: invoice_3.id) }
+    let!(:trans_4_s) { create_list(:transaction, 4, result: 1, invoice_id: invoice_4.id) }
+    let!(:trans_5_s) { create_list(:transaction, 5, result: 1, invoice_id: invoice_5.id) }
+    let!(:trans_6_f) { create_list(:transaction, 6, result: 0, invoice_id: invoice_6.id) }
+
+    context "#successful_transaction_count" do
+      it "returns the number of successful transactions" do
+        expect(customer_1.successful_transaction_count).to eq(1)
+        expect(customer_2.successful_transaction_count).to eq(2)
+        expect(customer_3.successful_transaction_count).to eq(3)
+        expect(customer_4.successful_transaction_count).to eq(4)
+        expect(customer_5.successful_transaction_count).to eq(5)
+        expect(customer_6.successful_transaction_count).to eq(0)
+      end
+    end
+  end
 end
