19bff76989f6c77f541490aa067fdd5da01733ab
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-03 17:08:05 -0600

    add show method to invoices controller

--- app/controllers/merchants/invoices_controller.rb
+++ app/controllers/merchants/invoices_controller.rb
@@ -3,4 +3,8 @@ class Merchants::InvoicesController < ApplicationController
     # merchant = Merchant.find(params[:id])
     # require 'pry'; binding.pry
   end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
 end
