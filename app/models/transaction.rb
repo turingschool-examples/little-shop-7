e403a50e4a63bdc102d377ff5b6ea963cbe923c2
Amy Spears <amspears007@gmail.com>
2023-06-02 20:17:38 -0600

    Add relationships and model spec with enums

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+  has_many :customers, through: :invoice
+
 
   enum result: {success: 0, failed: 1}
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   describe 'relationships' do 
     it {should belong_to(:invoice)}
+    it {should have_many(:customers).through(:invoice)}
 
     it { should define_enum_for(:result).with_values(success: 0, failed: 1)}
 
