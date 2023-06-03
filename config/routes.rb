007691f3ae6b6a5b5d3797183ae22b770ec5516a
Javen Wilson <javenb022@gmail.com>
2023-06-03 09:58:40 -0400

    Feat: adds show route and action

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   get "/admin/merchants/:id", to: "admin/merchants#show"
 
   get "/admin/invoices", to: "admin/invoices#index"
+  get "/admin/invoices/:id", to: "admin/invoices#show"
 end
