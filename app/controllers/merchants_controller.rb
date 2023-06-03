fd38cf4f0bb86cd4874f31c3b179b200bfa9618c
Blaine Glasgow <glasgowb@outlook.com>
2023-06-03 01:18:13 -0700

    Save pending for user story

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -2,4 +2,9 @@ class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def merchant_customer
+    require 'pry'; binding.pry
+    items[0].invoices[0].customer
+  end
 end
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -7,11 +7,19 @@ RSpec.describe "/merchants/:id/dashboard" do
       let!(:merchant_2) { create(:merchant) }
 
       let!(:item_1) { create(:item, merchant_id: merchant_1.id) }
-      let!(:item_2) { create(:item, merchant_id: merchant_1.id) }
+      let!(:item_2) { create(:item, merchant_id: merchant_2.id) }
+
+      let!(:invoice_1) { create(:invoice, customer_id: customer_1.id, status: 0)}
+      let!(:invoice_2) { create(:invoice, customer_id: customer_2.id, status: 1)}
+
+      let!(:invoice_item_1) { create(:invoice_item, unit_price: 1000, invoice_id: invoice_1.id, item_id: item_1.id) }
+
+      let!(:customer_1) { create(:customer) }
+      let!(:customer_2) { create(:customer) }
 
       # User Story 1
 
-      it "directs me to the merchants dashboard where I see the merchants name" do
+      xit "directs me to the merchants dashboard where I see the merchants name" do
         visit "/merchants/#{merchant_1.id}/dashboard"
         expect(page).to have_content(merchant_1.name)
         expect(page).to_not have_content(merchant_2.name)
@@ -23,7 +31,7 @@ RSpec.describe "/merchants/:id/dashboard" do
 
       # 2. Merchant Dashboard Links
 
-      it "links to the merchants items index" do
+      xit "links to the merchants items index" do
         visit "/merchants/#{merchant_1.id}/dashboard"
         click_link "My Items"
         expect(current_path).to eq("/merchants/#{merchant_1.id}/items")
@@ -35,7 +43,7 @@ RSpec.describe "/merchants/:id/dashboard" do
         expect(page).to have_content("#{merchant_2.name} Items")
       end
 
-      it "links to the merchants invoices index"do
+      xit "links to the merchants invoices index" do
         visit "/merchants/#{merchant_1.id}/dashboard"
         click_link "My Invoices"
         expect(current_path).to eq("/merchants/#{merchant_1.id}/invoices")
@@ -46,7 +54,21 @@ RSpec.describe "/merchants/:id/dashboard" do
         expect(current_path).to eq("/merchants/#{merchant_2.id}/invoices")
         expect(page).to have_content("#{merchant_2.name} Invoices")
       end
+      # 3. Merchant Dashboard Statistics - Favorite Customers
 
+      # As a merchant,
+      # When I visit my merchant dashboard (/merchants/:merchant_id/dashboard)
+      # Then I see the names of the top 5 customers
+      # who have conducted the largest number of successful transactions with my merchant
+      # And next to each customer name I see the number of successful transactions they have
+      # conducted with my merchant
+
+      it "see top 5 customers on on dashboard" do
+        visit "/merchants/#{merchant_1.id}/dashboard"
+        save_and_open_page
+        expect(page).to have_content("Top 5 Customers")
+        expect(page).to have_content(customer_1.name)
+      end
     end
   end
 end
