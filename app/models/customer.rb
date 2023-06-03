8f9fb346c8c71f0a8dd189119fe4e21f764c3b63
Garrett Gregor <garrett.gregor@gmail.com>
2023-06-02 18:35:47 -0600

    Feature: add #top_5_customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,14 @@ class Customer < ApplicationRecord
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
   has_many :transactions, through: :invoices
+
+  def self.top_5_customers
+    Customer.joins(:transactions)
+      .select(:id, :first_name, :last_name, "COUNT(transactions.result) AS transaction_count")
+      .where("transactions.result = 1")
+      .group(:id)
+      .order("transaction_count DESC")
+      .limit(5)
+  end
+
 end
