b5f0d9b2a1ab423e6262ac7f81325c95bc4234b2
Jesse Thomas <jgthomas3@gmail.com>
2023-06-03 21:45:34 -0600

    Feat: Add edit/update methods to M:items cont.

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -5,5 +5,24 @@ class Merchants::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @merchant = Merchant.find(@item.merchant_id)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    @merchant = Merchant.find(@item.merchant_id)
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to "/merchants/#{item.merchant_id}/items/#{item.id}"
+    flash[:notice] = "Item #{item.name} Successfully Updated!"
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price)
   end
 end
