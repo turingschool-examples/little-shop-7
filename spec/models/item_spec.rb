9ebf60cc8bb99baa8950648321658ed638a4302a
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-03 19:10:25 -0600

    Test: #date_format

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Item, type: :model do
     it {should have_many(:invoice_items)}
     it {should have_many(:invoices).through(:invoice_items)}
   end
+
+  describe 'instance methods' do 
+    it '#date_format' do 
+      freeze_time do 
+        allow(Time).to receive(:current).and_return Time.new(2000)
+        item = create(:item, created_at: Time.current) 
+        expect(item.date_format).to eq("Saturday, January 1, 2000")
+      end
+    end
+  end
 end
