bb160259b3a61818bc152bc1507ef83d6afd75cb
Jesse Sorman <jessesorman@gmail.com>
2023-06-04 11:37:04 -0400

    update merchant wip

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -4,6 +4,30 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def show
-    @merchant.find(params[:id])
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def create
+    merchant = Merchant.create(merchant_params)
+    redirect_to "/admin/merchants/#{merchant.id}"
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+    if merchant.update(merchant_params)
+      redirect_to "/admin/merchants/#{merchant.id}"
+      flash[:alert] = "Sucessfully edited merchant"
+    else
+      redirect_to "/admin/merchants/#{merchant.id}/edit"
+    end
+  end
+
+  private
+  def merchant_params
+    params.permit(:name)
   end
 end
--- spec/features/admin/merchant_spec.rb
+++ spec/features/admin/merchant_spec.rb
@@ -1,26 +1,39 @@
-require "rails_helper" do
+require "rails_helper"
 
-  RSpec.describe "Admin Merchant" do
+RSpec.describe "Admin Merchant" do
 
-    let!(:merchant1) {Merchant.create!(name: 'Stuff Emporium')}
-    let!(:merchant2) {Merchant.create!(name: 'Junk')}
-    let!(:merchant3) {Merchant.create!(name: 'Homemade stuff')}
-    let!(:merchant4) {Merchant.create!(name: 'Cool Stuff')}
-    
-    it "displays merchant index page" do
-      visit "/admin/merchants"
-      expect(page).to have_content("Name: Stuff Emporium")
-      expect(page).to have_content("Junk")
-      expect(page).to have_content("Homemade stuff")
-      expect(page).to have_content("Cool Stuff")
-    end
+  let!(:merchant1) {Merchant.create!(name: 'Stuff Emporium')}
+  let!(:merchant2) {Merchant.create!(name: 'Junk')}
+  let!(:merchant3) {Merchant.create!(name: 'Homemade stuff')}
+  let!(:merchant4) {Merchant.create!(name: 'Cool Stuff')}
+  
+  it "displays merchant index page" do
+    visit "/admin/merchants"
+    expect(page).to have_content("Name: Stuff Emporium")
+    expect(page).to have_content("Junk")
+    expect(page).to have_content("Homemade stuff")
+    expect(page).to have_content("Cool Stuff")
+  end
 
+  describe "admin merchant show page" do
     it "displays admin merchant show page" do
-      visit "/admin/merchants"
+      visit "admin/merchants"
       click_link "Stuff Emporium"
-      expect(current_path).to eqx("/admin/merchants/#{merchant1.id}")
+      expect(current_path).to eq("/admin/merchants/#{merchant1.id}")
       expect(page).to have_content("Stuff Emporium")
       expect(page).to_not have_content("Junk")
     end
+    
+    it "can update merchant" do
+      visit "/admin/merchants/#{merchant1.id}"
+      expect(page).to have_content("Stuff Emporium")
+      click_link "Update Merchant"
+      expect(current_path).to eq("/admin/merchants/#{merchant1.id}/edit")
+      fill_in("Name", with: "Savings Emporium")
+      click_link "Submit"
+      expect(current_path).to eq("/admin/merchants/#{merchant1.id}")
+
+    end
+
   end
 end
