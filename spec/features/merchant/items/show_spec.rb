2e2b7b4a31ac6e5258ddb60a4939e69dc2582a4e
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-06-05 16:16:27 -0600

    test edge case

--- spec/features/merchant/items/show_spec.rb
+++ spec/features/merchant/items/show_spec.rb
@@ -5,19 +5,17 @@ RSpec.describe 'Merchant Items Show Page', type: :feature do
   before(:each) do 
     @betty = create(:merchant)
     @joshy = create(:merchant)
-    @item_1 = create(:item, unit_price: 2222, merchant_id: @betty.id)
+    @item_1 = create(:item,unit_price: 2222, merchant_id: @betty.id)
     @item_2 = create(:item, merchant_id: @betty.id)
     @item_3 = create(:item, merchant_id: @betty.id)
-    @item_4 = create(:item, merchant_id: @joshy.id)
+    @item_4 = create(:item, unit_price: 7890, merchant_id: @joshy.id)
   end
   
   describe '/merchants/:merchant_id/items/:item_id' do 
-    it 'shows items attributeson view page' do
+    it 'shows items attributes on view page' do
       visit  merchant_item_path(@betty, @item_1)
-save_and_open_page
       within("h1") do
-      expect(page).to have_content("#{@betty.name}'s #{@item_1.name} Show Page") 
-
+        expect(page).to have_content("#{@betty.name}'s #{@item_1.name} Show Page") 
       end
       expect(page).to have_content(@item_1.name) 
       expect(page).to have_content("Description: #{@item_1.description}") 
@@ -26,14 +24,10 @@ save_and_open_page
       expect(page).to_not have_content(@item_3.name) 
       expect(page).to_not have_content(@item_4.name) 
     end
+    
+    it 'if item unit price ends in zero, unit price is still formatted' do 
+      visit  merchant_item_path(@joshy, @item_4)
+      expect(page).to have_content("Current Selling Price: $78.90")
+    end
   end
 end
-# Merchant Items Show Page
-    # As a merchant,
-    # When I click on the name of an item from the merchant items index page, (merchants/:merchant_id/items)
-    # Then I am taken to that merchant's item's show page (/merchants/:merchant_id/items/:item_id)
-    # And I see all of the item's attributes including:
-    
-    # Name
-    # Description
-    # Current Selling Price
