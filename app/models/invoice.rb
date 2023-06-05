889a604a1bff4be970f7d4ce5853116e4e5a1fd5
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-06-05 14:46:47 -0600

    Feat: add .incomp_inovices.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -10,6 +10,12 @@ class Invoice < ApplicationRecord
 
   enum status: ["cancelled", "completed", "in progress"] # can = 0 com = 1 in p = 2 need to remigrate and make integers
 
+  #class methods
+  def self.incomp_invoices
+    Invoice.joins(:invoice_items).where("invoice_items.status != ?", 1)
+  end
+  
+  #instance methods
   def revenue
     invoice_items.sum("unit_price * quantity")
   end
