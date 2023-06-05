ac0097a1194a204377b10074f7920e9020243033
Amy Spears <amspears007@gmail.com>
2023-06-05 15:38:37 -0600

    Add description in items factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :item do
     name {Faker::Appliance.equipment}
     unit_price {Faker::Number.number(digits: 4)}
+    description {Faker::Quote.famous_last_words}
     merchant
   end
 end
