97a6d42764d4f212f54e27cc695974cd40bc609e
Jesse Thomas <jgthomas3@gmail.com>
2023-06-05 22:13:30 -0600

    Feat: top_5_items_by_revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,5 +15,13 @@ class Item < ApplicationRecord
     where(status: "disabled")
   end
 
+  def self.top_5_items_by_revenue
+    joins(:transactions)
+    .select("items.*, sum(invoice_items.unit_price * invoice_items.quantity) AS total_revenue")
+    .where('transactions.result = 0')
+    .group("items.id")
+    .order("total_revenue desc")
+    .limit(5)
+  end
 end
 
