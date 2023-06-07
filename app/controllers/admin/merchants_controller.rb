728c74c160083e572397f556ebeeda80946c4098
Blaine Glasgow <glasgowb@outlook.com>
2023-06-07 13:52:08 -0700

    Add advanced routes to merchants and invoices controllers

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -10,9 +10,9 @@ class Admin::MerchantsController < ApplicationController
   def create
     merchant = Merchant.new(merchant_params)
     if merchant.save
-      redirect_to "/admin/merchants"
+      redirect_to admin_merchants_path
     else
-      redirect_to "/admin/merchants/new"
+      redirect_to new_admin_merchant_path
       flash[:alert] = "Error: #{error_message(merchant.errors)}"
     end
   end
@@ -29,12 +29,12 @@ class Admin::MerchantsController < ApplicationController
     @merchant = Merchant.find(params[:id])
     if params[:status] == "1"
       @merchant.update(status: 1)
-      redirect_to "/admin/merchants"
+      redirect_to admin_merchants_path
     elsif params[:status] == "0"
       @merchant.update(status: 0)
-      redirect_to "/admin/merchants"
+      redirect_to admin_merchants_path
     elsif @merchant.update(merchant_params)
-      redirect_to "/admin/merchants/#{@merchant.id}"
+      redirect_to admin_merchant_path(@merchant)
       flash[:notice] = "Merchant #{@merchant.id} has been successfully updated"
     end
   end
