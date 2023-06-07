fec833dcc600f4144c6355ce9019196bfd1d438b
Javen Wilson <javenb022@gmail.com>
2023-06-07 09:34:51 -0400

    Feat: app logo method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+  def self.app_logo
+    app_logo = PhotoBuilder.app_logo_info
+    app_logo.url
+  end
+
   def welcome
   end
+
 
   private
 
