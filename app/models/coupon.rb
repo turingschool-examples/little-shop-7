10cb1d018a5c52edce83203e4391177f48e419e5
Javen Wilson <javenb022@gmail.com>
2023-06-09 17:00:50 -0400

    Adds enums for coupons

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,6 +2,11 @@ class Coupon < ApplicationRecord
   belongs_to :merchant
   belongs_to :invoice, optional: true
 
-  validates_presence_of :name, :code
+  validates_presence_of :name
+  validates_presence_of :code, uniqueness: true
   validates :status, :value, :type, numericality: { only_integer: true }
+
+  enum status: ["disabled", "activated"]
+  enum type: ["percent", "dollar"]
+
 end
