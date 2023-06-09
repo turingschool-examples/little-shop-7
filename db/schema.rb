15274122d57fb75504b49d9546ebf3b0bf258abe
Javen Wilson <javenb022@gmail.com>
2023-06-09 13:34:48 -0400

    Adds coupon table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_220632) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_164949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.integer "status", default: 0
+    t.integer "value"
+    t.integer "type"
+    t.bigint "merchant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["merchant_id"], name: "index_coupons_on_merchant_id"
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
@@ -69,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_220632) do
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
+  add_foreign_key "coupons", "merchants"
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
