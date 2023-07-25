1ac0332085986536c9f85374cda6f115d97a487a
Chris Asercion <chris.asercion@gmail.com>
2023-07-25 14:14:04 -0600

    Adds items

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -17,7 +17,11 @@ RSpec.describe "Merchant Show Page" do
   # Then I see link to my merchant items index (/merchants/:merchant_id/items)
   # And I see a link to my merchant invoices index (/merchants/:merchant_id/invoices)
     it "I see a link to my merchant items index" do
+      @merchant_1 = Merchant.create(name: "Steve")
+      @item_1 = Item.create!(name: "Item Qui Esse", description: "Nihil autem sit odio inventore deleniti. Est laudantium ratione distinctio laborum. Minus voluptatem nesciunt assumenda dicta voluptatum porro.", unit_price: 75107, merchant_id: @merchant_1.id  )
+      @item_2 = Item.create!(name: "Item Autem Minima", description: "Cumque consequuntur ad. Fuga tenetur illo molestias enim aut iste. Provident quo hic aut. Aut quidem voluptates dolores. Dolorem quae ab alias tempora.", unit_price: 67076, merchant_id: @merchant_1.id )
 
+      visit "/merchants/#{@merchant_1.id}/dashboard"
     end
   end
 end
