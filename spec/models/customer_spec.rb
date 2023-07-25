c27c89fc7f4efcbdd37ca1851c573e20c15aac6c
Chris Asercion <chris.asercion@gmail.com>
2023-07-24 19:44:28 -0600

    Adds customer validations

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Customers, type: :model do
+RSpec.describe Customer, type: :model do
   describe "validations" do
     it {should validate_presence_of :first_name}
     it {should validate_presence_of :last_name}
