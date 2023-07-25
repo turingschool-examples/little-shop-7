1e16f9608d7ae280a5b27b9c98f36b12e35cb9d9
Jorja Fleming <aset284@gmail.com>
2023-07-25 11:44:57 -0600

    Add Model and model specs for invoice

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSped.describe Invoice, type: :model do
+  describe "relationships" do
+    it { should belong_to :customer }
+    it { should have_many :invoice_items }
+    it { should have_many(:items).through(:invoice_items) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :status }
+  end
+end
