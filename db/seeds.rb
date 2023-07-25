732e47a482d89c4d7c3d51d20a3b44e69c708369
Wil Fady <wrfady@gmail.com>
2023-07-25 13:38:24 -0400

    update: tasks rake

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Placeholder.destroy_all
+Placeholder.destroy_all
+Placeholder.destroy_all
+Placeholder.destroy_all
