9a6d426f62983049bddf139e2822fb6d12f58f95
andi lovetto <andilovetto@gmail.com>
2023-07-26 15:52:43 -0600

    feat: Matches wireframe admin merchant index

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,12 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   enum status: {"enable": 0, "disable": 1}
+
+  def self.enabled_merchants
+    where(status: 0)
+  end
+
+  def self.disabled_merchants
+    where(status: 1)
+  end
 end
