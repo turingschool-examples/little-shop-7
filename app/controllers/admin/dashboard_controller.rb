06df26aae7561ff44ead9c0e50890242b4679a5d
Wil Fady <wrfady@gmail.com>
2023-07-26 17:58:43 -0400

    show invoice dates and order oldest to newest
    Co-authored-by: Connor <richmond.connor@gmail.com>

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,6 @@
 class Admin::DashboardController < ApplicationController
 
   def index
-
     @top_customers = Customer.top_customers_with_transactions
     @incomplete_invoices = Invoice.unshipped_invoices
   end
