81c93a68f79ca588bdab36c4d82e075f20bbf5be
Parker Boeing <parker.boeing@gmail.com>
2023-07-26 00:43:45 -0600

    test: User story 14 merchant invoices index

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :merchants, only: [:index, :show] do
+    resources :invoices, only: [:index]
+  end
 end
