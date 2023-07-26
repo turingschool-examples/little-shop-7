30be2f2155ab97f826fefbc7365ef60738bb40aa
andi lovetto <andilovetto@gmail.com>
2023-07-26 14:44:11 -0600

    feat: Adds enum and changes data type for merchant status from string to integer for enum implementation

--- db/migrate/20230725201715_create_merchants.rb
+++ db/migrate/20230725201715_create_merchants.rb
@@ -2,7 +2,7 @@ class CreateMerchants < ActiveRecord::Migration[7.0]
   def change
     create_table :merchants do |t|
       t.string :name
-      t.string :status
+      t.integer :status
 
       t.timestamps
     end
