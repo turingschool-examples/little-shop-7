93dce1f5b7a3a83b9acd9eeac953d7d78a2759ff
Jorja Fleming <aset284@gmail.com>
2023-07-26 11:52:12 -0600

    Update name of admin dashboard

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -1,11 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Admin Dashboard Page", type: :feature do
-#   19. Admin Dashboard
-
-# As an admin,
-# When I visit the admin dashboard (/admin)
-# Then I see a header indicating that I am on the admin dashboard
+# US 19
   describe "When I visit the admin dashboard (/admin)" do
     it "I see a header indicating that I am on the admin dashboard" do
 
