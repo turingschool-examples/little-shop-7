a52038785ff65d340be7a5f6f99a680cb3631293
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-07-27 11:08:57 -0600

    removes dependent: :destroy

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,9 @@
 class Customer < ApplicationRecord 
-  has_many :invoices, dependent: :destroy
-  has_many :transactions, through: :invoices, dependent: :destroy
-  has_many :invoice_items, through: :invoices, dependent: :destroy
-  has_many :items, through: :invoice_items, dependent: :destroy 
-  has_many :merchants, through: :items, dependent: :destroy
+  has_many :invoices
+  has_many :transactions, through: :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
 
   def self.top_5_customers
     joins(:transactions).where(transactions: {result: "success"}).select("customers.*, count(result) AS result_count").group(:id).order("result_count desc").limit(5)
