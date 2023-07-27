a2c102ff25d6c5343aee1e4392a361e79e73bdc3
Allen Russell <garyrusselljr@gmail.com>
2023-07-26 21:49:28 -0600

    fix: Update seed file with .destroy_all for all models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-{Model}.destroy_all
+Customer.destroy_all
+InvoiceItem.destroy_all
+Invoice.destroy_all
+Item.destroy_all
+Merchant.destroy_all
+Transaction.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
