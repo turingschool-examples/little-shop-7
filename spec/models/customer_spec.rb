60fe452351dca1d2a493f2ab0dc3a25863e4604d
Zanna Fitch <zannafitch2004@gmail.com>
2023-07-26 20:25:15 -0500

    test: Full name method

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Customer, type: :model do
     it {should have_many(:transactions).through(:invoices)}
     it {should have_many(:items).through(:invoices)}
   end
+
+
+  describe "instance methods" do 
+    describe "#full_name" do 
+      it " give the first and last name" do 
+        customer1 = Customer.create!(first_name:"Paul", last_name: "McCartney")
+
+        expect(customer1.full_name).to eq("Paul McCartney")
+      end
+    end
+  end
 end
