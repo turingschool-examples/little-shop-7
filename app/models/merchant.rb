87ed03dd6e71e890887cdb50da9cd516d4c5277f
Wil Fady <wrfady@gmail.com>
2023-07-27 16:19:47 -0400

    toggle wip

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices, through: :items
+  enum status: {enabled: "Enabled", disabled: "Disabled"}
 
   def distinct_invoices
     invoices.distinct
--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
   resources :admin, controller: 'admin/dashboard', only: [:index]
 
   # Route for Merchants under Admin
-  namespace :admin, as: 'admin_merchants' do
-    resources :merchants, only: [:index]
-  end
+  # namespace :admin, as: 'admin_merchants' do
+  #   resources :merchants, only: [:index]
+      
+  # end
 
   namespace :admin do
-    resources :merchants, only: [:show, :edit, :update]
+    resources :merchants, only: [:index, :show, :edit, :update]
+        patch :toggle_status, on: :member
   end
 
 
