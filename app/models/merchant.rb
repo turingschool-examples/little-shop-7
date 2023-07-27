99f86a5627520f2150d32f0cad0f902de016a511
Zanna Fitch <zannafitch2004@gmail.com>
2023-07-26 20:21:52 -0500

    feat: add favorite_customers

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,28 +1,24 @@
 class Merchant < ApplicationRecord
   has_many :items
-  has_many :invoices, through: :items
+  has_many :invoice_items, through: :items
+  has_many :invoices, through: :invoice_items
   has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
 
   def favorite_customers
-    # WHAT WE WANT: top 5 customers with the most successful transactions
-    # | customer_name | count_transactions
-    # |----------------------------------
-    # | John          |      5
-    # | Ethan         |      4
-    # | Zanna         |      3
-    # | Jorja         |      3
-    # | Daniel        |      2
-
-    # get all the transactions related to this merchant
-    # group them by invoice id, which is related to a customer
-    # do a count and group by customer_id
-    # get the customer name
-
+    customers.joins(invoices: :transactions)
+      .where("transactions.result = ?","success")
+      # .where(transactions: {result: "success"})
+      .select("customers.*, count(transactions) AS success_transactions")
+      # .select("concat(customers.first_name, ' ', customers.last_name) AS full_name, count(transactions) AS success_transactions")
+      .group("customers.id")
+      # .group("full_name")
+      .order("success_transactions desc")
+      .limit(5)
+    # joins(:customer).order(customer.succesful_transaction_count).limit(5)
     # customers(order: {customer.merch_transactions(self.id), desc}).limit(5) 
-Customer.join(transactions: :invoice)
+    # customers.order(customer.succesful_transaction_count, desc).limit(5)
   end
 
-  def transaction_count
-    transactions.count
-  end
+
 end
