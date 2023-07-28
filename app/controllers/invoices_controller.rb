270be6fed557a9932f8d17361efd34c8d4c75d57
Allen Russell <garyrusselljr@gmail.com>
2023-07-28 08:59:19 -0600

    feat: Add update method in invoices controller, form selector on view

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,5 +7,15 @@ class InvoicesController < ApplicationController
   
   def show
     @invoice = Invoice.find(params[:id])
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoice = Invoice.find(params[:id])
+    @invoice.update({
+      status: params[:invoice][:status]})
+    
+    redirect_to merchant_invoice_path(@merchant, @invoice)
   end
 end
