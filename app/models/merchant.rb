ab32b1b76eb33b61e9bb42ad72c805e1a5c28757
Gabriel Torres <thomastgtorres21@gmail.com>
2023-07-27 22:55:43 -0500

    top five customers model method for us_3

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,20 +1,18 @@
 class Merchant < ApplicationRecord 
   validates :name, presence: true
-  
+
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
 
-
   def top_five_customers
-    require 'pry'; binding.pry
-    Customer
-    .joins(:transactions)
-    .where(transactions: { result: "success" })
-    .group('customers.id')
-    .order('COUNT(transactions.id) DESC')
-    .limit(5)
+    customers.joins(:transactions)
+                        .where("transactions.result = 'success'") 
+                        .group('customers.id')
+                        .select("CONCAT(customers.first_name, ' ', customers.last_name) AS customer_name, customers.*, COUNT(DISTINCT transactions.id) AS transaction_count")
+                        .order('transaction_count DESC')
+                        .limit(5)
   end
 end
