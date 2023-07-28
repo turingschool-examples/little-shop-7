0d7c1ab2f90db7984d35a89c197fc86998fb6201
andi lovetto <andilovetto@gmail.com>
2023-07-28 14:56:45 -0600

    test: Changes status to disable default; new_spec for new merchant form

--- spec/features/admin/merchants/edit_spec.rb
+++ spec/features/admin/merchants/edit_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.describe "admin merchants edit page" do
 
-  let!(:little_kin_shop) { create(:merchant, name: "Dave Schools") }
-  let!(:red_roses) { create(:merchant, name: "Sunny") }
-  let!(:halloween_face) { create(:merchant, name: "JB") }
-  let!(:tall_boy) { create(:merchant, status: 1) }
-  let!(:arleen) { create(:merchant, status: 1) }
-  let!(:bust_it_big) { create(:merchant, status: 1) }
+  let!(:little_kin_shop) { create(:merchant, name: "Dave Schools", status: 0) }
+  let!(:red_roses) { create(:merchant, name: "Sunny", status: 0) }
+  let!(:halloween_face) { create(:merchant, name: "JB", status: 0) }
+  let!(:tall_boy) { create(:merchant) }
+  let!(:arleen) { create(:merchant) }
+  let!(:bust_it_big) { create(:merchant) }
 
   before do
     visit edit_admin_merchant_path(little_kin_shop)
@@ -16,9 +16,8 @@ RSpec.describe "admin merchants edit page" do
   it "provides a form that autopopulates merchant info for editing" do
     expect(page).to have_field("Name", with: "Dave Schools")
     expect(page).to_not have_content("JB")
-    expect(page).to have_field("Status", with: "enable")
     fill_in "Name", with: "DAS"
-    click_button "Save"
+    click_button "Update Merchant"
     expect(current_path).to eq(admin_merchant_path(little_kin_shop))
     expect(page).to have_content("DAS")
     expect(:alert).to be_present
