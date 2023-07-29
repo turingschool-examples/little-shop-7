ade1354e77e58c6a35aebd9155a453ace8567bac
Ethan Black <ethanrossblack@gmail.com>
2023-07-28 18:11:05 -0600

    fix: fix test data

--- spec/features/merchants/invoices/index_spec.rb
+++ spec/features/merchants/invoices/index_spec.rb
@@ -23,17 +23,17 @@ RSpec.describe "Merchant Invoices Index page", type: :feature do
     # Invoices
     # @invoice_1 has two items from @merchant_1
     @invoice_1 = @customer_1.invoices.create!(status: 'in progress')
-    @invoice_1.invoice_items << InvoiceItem.create!(item_id: @item_1.id, quantity: 5, unit_price: 13635, status: 'packaged')
-    @invoice_1.invoice_items << InvoiceItem.create!(item_id: @item_2.id, quantity: 10, unit_price: 66747, status: 'shipped')
+    InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 5, unit_price: 13635, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 10, unit_price: 66747, status: 'shipped')
     
     # @invoice_2 has one item from @merchant_1 and one item from @merchant_2
     @invoice_2 = @customer_2.invoices.create!(status: 'cancelled')
-    @invoice_2.invoice_items << InvoiceItem.create!(item_id: @item_3.id, quantity: 9, unit_price: 23324, status: 'pending')
-    @invoice_2.invoice_items << InvoiceItem.create!(item_id: @item_4.id, quantity: 9, unit_price: 76941, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_3.id, quantity: 9, unit_price: 23324, status: 'pending')
+    InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_4.id, quantity: 9, unit_price: 76941, status: 'packaged')
 
     # @invoice 3 has one item from @merchant_2
     @invoice_3 = @customer_3.invoices.create!(status: 'completed')
-    @invoice_3.invoice_items << InvoiceItem.create!(item_id: @item_4.id, quantity: 12, unit_price: 34873, status: 'packaged')
+    InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_4.id, quantity: 12, unit_price: 34873, status: 'packaged')
   end
 
   describe "When I visit a merchant's invoices index (/merchants/:merchant_id/invoices)" do
