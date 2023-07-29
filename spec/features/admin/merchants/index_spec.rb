471afce41827b54e8a09565ce3c81e14fc891969
Wil Fady <wrfady@gmail.com>
2023-07-29 18:23:05 -0400

    test: admin merchants grouped by status

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe "/admin /merchants", type: :feature do
       merchant_3 = Merchant.create!(name: "Merchant 3")
 
       visit "/admin/merchants"
+
       within("tr#em-#{merchant_1.id}") do
         expect(page).to have_content(merchant_1.name)
         expect(page).to have_button("Disable")
@@ -48,6 +49,39 @@ RSpec.describe "/admin /merchants", type: :feature do
       
       expect(current_path).to eq("/admin/merchants")
       expect(page).to have_button("Enable")
+    end
+
+    it "has merchants listed in appropriate sections for enabled or disabled" do
+      merchant_1 = Merchant.create!(name: "Merchant 1")
+      merchant_2 = Merchant.create!(name: "Merchant 2")
+      merchant_3 = Merchant.create!(name: "Merchant 3", status: "disabled")
+      merchant_4 = Merchant.create!(name: "Merchant 4", status: "disabled")
+
+      visit "/admin/merchants"
+
+      within("tr#em-#{merchant_1.id}") do
+        expect(page).to have_content(merchant_1.name)
+        expect(page).to_not have_content(merchant_3.name)
+        expect(page).to_not have_content(merchant_4.name)
+      end
+
+      within("tr#em-#{merchant_2.id}") do
+        expect(page).to have_content(merchant_2.name)
+        expect(page).to_not have_content(merchant_3.name)
+        expect(page).to_not have_content(merchant_4.name)
+      end
+
+      within("tr#dm-#{merchant_3.id}") do
+        expect(page).to have_content(merchant_3.name)
+        expect(page).to_not have_content(merchant_1.name)
+        expect(page).to_not have_content(merchant_2.name)
+      end
+
+      within("tr#dm-#{merchant_4.id}") do
+        expect(page).to have_content(merchant_4.name)
+        expect(page).to_not have_content(merchant_1.name)
+        expect(page).to_not have_content(merchant_2.name)
+      end
     end
   end
 end
