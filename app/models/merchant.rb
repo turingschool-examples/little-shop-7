dd3a2b8b75bbbf8444bf5fbdc534eb7dce292743
andi lovetto <andilovetto@gmail.com>
2023-07-29 14:14:10 -0600

    feat: Adds class method top_5_merchant_by_revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -34,4 +34,13 @@ class Merchant < ApplicationRecord
   def self.disabled_merchants
     where(status: 1)
   end
-end
+
+  def self.top_5_merchants_by_revenue
+    joins(invoices: :transactions)
+      .where(transactions: { result: 0 })
+      .select("merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as total_revenue")
+      .group(:id)
+      .order(total_revenue: :desc)
+      .limit(5)
+  end
+end    
