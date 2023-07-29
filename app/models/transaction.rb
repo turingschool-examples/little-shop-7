29fbea73992c8278a889808f9ddf48a68c2884a8
Allen Russell <garyrusselljr@gmail.com>
2023-07-29 08:32:08 -0600

    add validate length for credit card number

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,7 +3,7 @@ class Transaction < ApplicationRecord
 
   enum result: { 'success': 0, 'failed': 1 }
 
-  validates :credit_card_number, presence: true
+  validates_length_of :credit_card_number, is: 16
   validates :credit_card_expiration_date, presence: true
   validates :result, presence: true
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Transaction, type: :model do
   end
 
   describe "validations" do
-    it { should validate_presence_of(:credit_card_number) }
+    it { should validate_length_of(:credit_card_number).is_equal_to(16) }
     it { should validate_presence_of(:credit_card_expiration_date) }
     it { should validate_presence_of(:result) }
     it { should define_enum_for(:result) }
