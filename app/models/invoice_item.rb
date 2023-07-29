394091120822ec2bb020610374e2ed68160077ca
Ethan Black <ethanrossblack@gmail.com>
2023-07-29 17:42:17 -0600

    feat: add #dollar_price to InvoiceItem model (with tests)

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,8 @@ class InvoiceItem < ApplicationRecord
   enum :status, { pending: 0, packaged: 1, shipped: 2 }
   belongs_to :item
   belongs_to :invoice
+
+  def dollar_price
+    unit_price * 0.01
+  end
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe InvoiceItem, type: :model do
     it { should belong_to :item }
     it { should belong_to :invoice }
   end
+
+  before :each do
+    @customer = Customer.create!(first_name: 'Joey', last_name: 'Ondricka')
+    @merchant = Merchant.create!(name: 'Schroeder-Jerde')
+    @item = @merchant.items.create!(name: 'Qui Esse', description: 'Nihil autem sit odio inventore deleniti', unit_price: 75107)
+    @invoice = @customer.invoices.create!(status: 'in progress', created_at: Time.new(2000))
+    @invoice_item = InvoiceItem.create!(invoice_id: @invoice.id, item_id: @item.id, quantity: 5, unit_price: 13635, status: 'packaged')
+
+  end
+
+  describe "instance methods" do
+    describe "#dollar_price" do
+      it "converts the unit price (cents) into dollars" do
+        expect(@invoice_item.dollar_price).to eq(136.35)
+      end
+    end
+  end
 end
