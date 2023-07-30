b6abf0d080e59c7c32d0bd2be523f620de432669
Zanna Fitch <zannafitch2004@gmail.com>
2023-07-29 19:35:05 -0500

    feat: add a button to enable or disable item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_28_223332) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_220018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_28_223332) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id", null: false
+    t.integer "status", default: 0
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
