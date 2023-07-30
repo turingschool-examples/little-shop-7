00f821a6e051b8868b22db81944514127ed22351
Jorja Fleming <aset284@gmail.com>
2023-07-30 11:34:15 -0600

    Made changes to story 34 and added price_to_currency method to model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,8 @@ class InvoiceItem < ApplicationRecord
   enum :status, { pending: 0, packaged: 1, shipped: 2 }
   belongs_to :item
   belongs_to :invoice
+
+  def price_to_currency
+    sprintf('$%.2f', unit_price.to_f / 100)
+  end
 end
