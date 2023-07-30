20081cf46d71fa96d9805dc724e89cc1b80c2e1d
Jorja Fleming <aset284@gmail.com>
2023-07-30 10:59:22 -0600

    Fix: Made requested changes and added show page

--- spec/features/admin/admin_invoice_show_spec.rb
+++ spec/features/admin/admin_invoice_show_spec.rb
@@ -1,6 +1,6 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Admin Invoice Page", type: :feature do
+RSpec.describe "Admin Invoice Show Page", type: :feature do
   before :each do
     @merchant_1 = Merchant.create!(name: "Bob's Burgers")
     @merchant_2 = Merchant.create!(name: "Kwik-E-Mart")
@@ -31,46 +31,34 @@ RSpec.describe "Admin Invoice Page", type: :feature do
     @invoice_item_6 = InvoiceItem.create!(item_id: @item_6.id, invoice_id: @invoice_3.id, quantity: 4, unit_price: 1375, status: 1)
 
   end
+  # US 33  
+  it "I can see each invoice id links to the admin invoice show page" do
 
-# US 32
-  describe "As an admin when I visit the admin invoices page" do
-    it "I can see a list of all invoice ids in the system" do
-      visit admin_invoices_path
+    visit admin_invoices_path
+    click_link(@invoice_1.id)
 
-      Invoice.all.each do |invoice|
-        expect(page).to have_link("#{invoice.id}")
-      end
-    end
+    expect(current_path).to eq(admin_invoice_path(@invoice_1))
+    expect(page).to have_content(@invoice_1.id)
+    expect(page).to have_content(@invoice_1.status)
+    expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %d, %Y"))
+    expect(page).to have_content(@customer_1.first_name)
+    expect(page).to have_content(@customer_1.last_name)
+    expect(page).not_to have_content(@customer_2.first_name)
+  end
 
-# US 33  
-    it "I can see each invoice id links to the admin invoice show page" do
+  # US 34
+  it "I can see all of the item information on the invoice" do
 
-      visit admin_invoices_path
-      click_link(@invoice_1.id)
+    visit admin_invoice_path(@invoice_1)
 
-      expect(current_path).to eq(admin_invoice_path(@invoice_1))
-      expect(page).to have_content(@invoice_1.id)
-      expect(page).to have_content(@invoice_1.status)
-      expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %d, %Y"))
-      expect(page).to have_content(@customer_1.first_name)
-      expect(page).to have_content(@customer_1.last_name)
-      expect(page).not_to have_content(@customer_2.first_name)
-    end
-
-# US 34
-    it "I can see all of the item information on the invoice" do
-
-      visit admin_invoice_path(@invoice_1)
-
-      expect(page).to have_content(@item_1.name)
-      expect(page).to have_content(@invoice_item_1.quantity)
-      expect(page).to have_content(@invoice_item_1.unit_price)
-      expect(page).to have_content(@invoice_item_1.status)
-      expect(page).to have_content(@item_2.name)
-      expect(page).to have_content(@invoice_item_2.quantity)
-      expect(page).to have_content(@invoice_item_2.unit_price)
-      expect(page).to have_content(@invoice_item_2.status)
-      expect(page).not_to have_content(@item_3.name)
-    end
+    expect(page).to have_content(@item_1.name)
+    expect(page).to have_content(@invoice_item_1.quantity)
+    expect(page).to have_content(@invoice_item_1.unit_price)
+    expect(page).to have_content(@invoice_item_1.status)
+    expect(page).to have_content(@item_2.name)
+    expect(page).to have_content(@invoice_item_2.quantity)
+    expect(page).to have_content(@invoice_item_2.unit_price)
+    expect(page).to have_content(@invoice_item_2.status)
+    expect(page).not_to have_content(@item_3.name)
   end
 end
