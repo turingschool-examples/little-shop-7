0925d630dbd8aa0ff88776f2088f5423c40df616
Zanna Fitch <zannafitch2004@gmail.com>
2023-07-29 19:35:05 -0500

    feat: add a button to enable or disable item

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -18,8 +18,12 @@ class MerchantItemsController < ApplicationController
     merchant = Merchant.find(params[:merchant_id])
     item = merchant.items.find(params[:id])
     if item.update(item_params)
-      flash[:alert] = "Merchant information updated successfully!"
-      redirect_to merchant_item_path(merchant, item)
+      if !params[:item][:status].nil?
+        redirect_to merchant_items_path(merchant)
+      else
+        flash[:alert] = "Merchant information updated successfully!"
+        redirect_to merchant_item_path(merchant, item)
+      end
     else
       redirect_to  edit_merchant_item_path(merchant, item)
       flash[:alert] = "Error: #{error_message(item.errors)}"
@@ -29,6 +33,6 @@ class MerchantItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :unit_price) 
+    params.require(:item).permit(:name, :description, :unit_price, :id, :status) 
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord 
+  enum :status, {disabled: 0, enabled: 1}
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
@@ -9,5 +10,13 @@ class Item < ApplicationRecord
   def current_price
     price = (unit_price * 0.01).round(3)
     "$#{price}"
+  end
+
+  def self.enabled_items
+    where('status = 1')
+  end
+
+  def self.disabled_items
+    where('status = 0')
   end
 end 
