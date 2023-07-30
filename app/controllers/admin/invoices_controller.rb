56bba592b70c7314dd6e5f4b68937da1e9be4d66
Connor Richmond <richmond.connor@gmail.com>
2023-07-29 20:41:13 -0400

    Add total revenue for invoice on show page

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -7,6 +7,7 @@ class Admin::InvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:id])
     @invoice_items = @invoice.invoice_items.includes(:item)
+    @total_revenue = @invoice_items.sum { |item| item.quantity * item.unit_price }
   end
 
 end
