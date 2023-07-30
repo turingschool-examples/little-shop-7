64482f7101fed5d4d3fc45a64b59e1d100d2df86
Kaina Cockett <kainacockett@gmail.com>
2023-07-30 09:36:22 -0600

    fix: update test for link check

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -5,12 +5,16 @@ RSpec.describe "Admin/merchants index page", type: :feature do
   describe "As an admin" do
     describe "When I click on the name of a merchant from the admin merchants index page (/admin/merchants)" do
       it "Then I am taken to that merchant's admin show page (/admin/merchants/:merchant_id) And I see the name of that merchant" do
-        merchant = create(:merchant)
-        visit admins_merchant_path(merchant.id)
-        save_and_open_page
-        within "#merchant_info" do
-          expect(page).to have_content(merchant.name)
+        merchants = create_list(:merchant, 8)
+        visit admins_merchants_path
+        
+        within "#merchant_list" do
+          click_link("#{merchants[0].name}")
         end
+        
+        save_and_open_page
+        expect(current_path).to eq("/admins/merchants/#{merchants[0].id}")
+        expect(page).to have_content("#{merchants[0].name}")
       end
     end
   end
