96d587f1b7001ae6915ad006a84b944a56d2b4e6
Ethan Black <ethanrossblack@gmail.com>
2023-07-29 18:01:31 -0600

    feat: add @merchant_invoice_items to merchant invoice controller show action

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -6,5 +6,7 @@ class MerchantInvoicesController < ApplicationController
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:id])
+    # Return the invoice items specific to that merchant
+    @merchant_invoice_items = @invoice.invoice_items.joins(:item).where("items.merchant_id = #{@merchant.id}")
   end
 end
