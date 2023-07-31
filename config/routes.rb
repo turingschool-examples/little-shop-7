354c66063b5966385596c4819f7e6b3f38d4bb5b
Ethan Black <ethanrossblack@gmail.com>
2023-07-31 11:24:56 -0600

    feat: add basic edit invoice_item_test

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,9 @@ Rails.application.routes.draw do
 
     resources :items, only: [:index, :show, :edit, :update], controller: "merchant_items"
 
-    resources :invoices, only: [:index, :show], controller: "merchant_invoices"
+    resources :invoices, only: [:index, :show], controller: "merchant_invoices" do
+      resources :invoice_items, only: [:update], controller: "merchant_invoice_items"
+    end
 
-    resources :invoices, only: [:index, :show]
   end
 end
