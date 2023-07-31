c7c4b6f6d5883a810244d49500ca2475dba9b018
Gabriel Torres <thomastgtorres21@gmail.com>
2023-07-30 22:23:36 -0500

    Refactors

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -4,13 +4,15 @@ RSpec.describe "As a merchant" do
   describe "When I visit my items index page" do
     it "Then next to each item I see a button to disable or enable that item" do
       merchant = create(:merchant)
-      item = create(:item, merchant: merchant)
+      items = create_list(:item, 4, merchant: merchant)
 
       visit "/merchants/#{merchant.id}/items"
 
       
-      expect(page).to have_css("##{item.id}_button")
-      # expect(page).to have_button("Disable", id: "#{item.id}_button")
+      
+        items.each do |item|
+          expect(page).to have_css("##{item.id}_button")
+      end
     end
     
     # it "When I click this button, Then I am redirected back to the items index page where I see the item's status has changed" do
