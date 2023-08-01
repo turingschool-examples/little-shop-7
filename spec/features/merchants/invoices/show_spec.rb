7c2f2fe00a3bb4d82533c000d8025c21e669d3a0
Chris Asercion <chris.asercion@gmail.com>
2023-08-01 16:22:32 -0600

    Adds spec tests

---
+++ spec/features/merchants/invoices/show_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "Merchant invoice show" do
+  describe "US15" do
+              #     As a merchant
+              # When I visit my merchant's invoice show page (/merchants/:merchant_id/invoices/:invoice_id)
+              # Then I see information related to that invoice including:
+              # - Invoice id
+              # - Invoice status
+              # - Invoice created_at date in the format "Monday, July 18, 2019"
+              # - Customer first and last name
+    it "Visits " do
+      
+    end
+  end
+end
