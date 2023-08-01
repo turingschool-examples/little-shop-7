670cd50c0c7a44fc82160f2c44b03b67a3c41a3f
Allen Russell <garyrusselljr@gmail.com>
2023-07-31 18:14:01 -0600

    Merge branch '74-data-validations' into merge-conflict-resolution

---
+++ config/initializers/configs.rb
@@ -0,0 +1,6 @@
+Unsplash.configure do |config|
+  config.application_access_key = "bYIXfett42m43KwCekkq5zhedK5hO1Vd__p-8sd-0Xw"
+  config.application_secret = "tj0GExPjTOLnbGZJhdonvem65UfN-yCVYqv6osH3360"
+  config.utm_source = "little-etsy-shop-7"
+
+end
