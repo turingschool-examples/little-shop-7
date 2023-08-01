6a9afb2fa7d1de788074592251c3dae79221dc9f
Allen Russell <garyrusselljr@gmail.com>
2023-08-01 09:30:04 -0600

    fix: make vcr only available in test environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,16 +74,18 @@ RSpec.configure do |config|
   end
 
   VCR.configure do |config|
-    vcr_mode = ENV['VCR_MODE'] =~ /rec/i ? :all : :once
-    config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-    config.hook_into :webmock
-  
-    config.default_cassette_options = {
-      record: vcr_mode,
-      match_requests_on: %i[method uri body]
-    }
-  
-    config.allow_http_connections_when_no_cassette = true
-    config.configure_rspec_metadata!
+    if Rails.env.test?
+      vcr_mode = ENV['VCR_MODE'] =~ /rec/i ? :all : :once
+      config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+      config.hook_into :webmock
+    
+      config.default_cassette_options = {
+        record: vcr_mode,
+        match_requests_on: %i[method uri body]
+      }
+    
+      config.allow_http_connections_when_no_cassette = true
+      config.configure_rspec_metadata!
+    end
   end
 end
