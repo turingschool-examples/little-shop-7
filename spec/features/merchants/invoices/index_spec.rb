6154255121da6ce8a9b1b515bc4e18b4690e3a65
Chris Asercion <chris.asercion@gmail.com>
2023-08-01 15:55:23 -0600

    before each block

--- spec/features/merchants/invoices/index_spec.rb
+++ spec/features/merchants/invoices/index_spec.rb
@@ -6,31 +6,34 @@ RSpec.describe "Merchant invoice index" do
     #Then I see all of the invoices that include at least one of my merchant's items
     #And for each invoice I see its id
     #And each id links to the merchant invoice show page
+    before :each do
+      @merchant = create(:merchant)
+      
+      @item = create(:item, merchant:@merchant)
+      @item2 = create(:item, merchant: @merchant)
+    
+      @customer1 = create(:customer)
+      @customer2 = create(:customer)
+
+      @invoice1 = create(:invoice, customer: @customer1)
+      @invoice2 = create(:invoice, customer: @customer2)
+      @non_merchant_invoice = create(:invoice, customer: @customer2)
+      
+      @invoice_item = create(:invoice_item, item: @item, invoice: @invoice1, status: 0)
+      @invoice_item = create(:invoice_item, item: @item2, invoice: @invoice2, status: 0)
+    end
 
     it "Shows all invoices from a particular merchant" do
       
-      merchant = create(:merchant)
-
       
-      item = create(:item, merchant: merchant)
-      item2 = create(:item, merchant: merchant)
-    
-
-      customer1 = create(:customer)
-      customer2 = create(:customer)
-
-      invoice1 = create(:invoice, customer: customer1)
-      invoice2 = create(:invoice, customer: customer2)
-      non_merchant_invoice = create(:invoice, customer: customer2)
+      visit merchant_invoices_path(@merchant)
       
-      invoice_item = create(:invoice_item, item: item, invoice: invoice1, status: 0)
-      invoice_item = create(:invoice_item, item: item2, invoice: invoice2, status: 0)
-
-      visit merchant_invoices_path(merchant)
-      save_and_open_page
-      expect(page).to have_content(invoice1.id)
-      expect(page).to have_content(invoice2.id)
-      expect(page).to_not have_content(non_merchant_invoice.id)
+      expect(page).to have_content(@invoice1.id)
+      expect(page).to have_content(@invoice2.id)
+      expect(page).to_not have_content(@non_merchant_invoice.id)
     end
+
+    it "has a link to the "
+    
   end
 end
