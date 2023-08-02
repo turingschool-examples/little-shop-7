30f437dd618fea109fbd863104447f139fe6f447
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-08-02 14:22:50 -0600

    merchant item photo functionality

--- app/services/unsplash_service.rb
+++ app/services/unsplash_service.rb
@@ -12,6 +12,12 @@ class UnsplashService
     Photo.new(parsed_body)
   end
 
+  def get_merchant_item_photo(item_name)
+    response = connection.get("/search/photos?query=#{item_name}")
+    parsed_body = JSON.parse(response.body)
+    Photo.new(parsed_body["results"].first)
+  end
+
   def connection
     Faraday.new(url:'https://api.unsplash.com', params: {"client_id" => ENV["UNSPLASH_KEY"]})
   end
