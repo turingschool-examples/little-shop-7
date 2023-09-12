5b064f825d5590c37d1b2dcd7159d7bf73ceb078
Reed Hillmar <reedhillmar@gmail.com>
2023-09-12 11:52:10 -0600

    setup migrations

---
+++ db/migrate/20230912172855_create_invoices.rb
@@ -0,0 +1,11 @@
+class CreateInvoices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :invoices do |t|
+      t.string :status
+      t.references :customer, null: false, foreign_key: true
+
+
+      t.timestamps
+    end
+  end
+end
