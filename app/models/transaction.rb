821e3e88eebea991d0a8aa1df87617a8f8ea33fb
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-09-12 12:52:25 -0500

    updated models with realtionships and validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,7 @@
 class Transaction < ApplicationRecord
+  belongs_to :invoice
+
+  validates :credit_card_number, presence: true
+  validates :credit_card_expiration_date, presence: true
+  validates :result, presence: true
 end
