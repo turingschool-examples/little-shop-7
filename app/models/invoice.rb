10b78b903cef62df5792ab6fdb18f2f89643b781
Blake Sergesketter <blakesergesketter@gmail.com>
2023-09-12 12:47:49 -0500

    enum alterations.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,5 @@
 require "csv"
 
 class Invoice < ApplicationRecord
-  # enum :status, [:completed, :cancelled, :in_progress]
-  enum :status, in_progress: "in progress", completed: "completed", cancelled: "cancelled"
+  enum status: [:completed, :cancelled, :in_progress]
 end
