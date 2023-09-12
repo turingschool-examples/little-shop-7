50208ffc3a5eec9e6ee0a17cb2657db45b3f887b
Joop Stark <stark.joop@gmail.com>
2023-09-12 16:25:16 -0600

    factory_bots: working customers started

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,9 +2,16 @@ require "rails_helper"
 
 RSpec.describe Customer do
   describe "factory tester" do
-    let(:abel) { create(:abel) }
+    let(:abel) { FactoryBot.create(:customer) }
+    let(:bruce) { FactoryBot.create(:bruce) }
+
     it "has a first name" do
       expect(abel.first_name).to eq("Abel")
     end
+
+    it "also has a first name" do
+      expect(bruce.first_name).to eq("Bruce")
+    end
+
   end
 end
