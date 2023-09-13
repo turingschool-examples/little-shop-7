94a1fdf34be1c676ce7411384c56fc7ec8ea377e
Nicholas McEnroe <nsmcenro@gmail.com>
2023-09-13 15:07:07 -0600

    Edit: Add WIP for US 20

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,12 @@ class Customer < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def self.order_customers_by_transactions
+    joins(invoices: :transactions)
+    .where(transactions: {result: 1})
+    .group('customer.*')
+    .order("COUNT(transactions.id) DESC")
+    .limit(5)
+  end
 end
--- spec/features/admin/dashboard/index_spec.rb
+++ spec/features/admin/dashboard/index_spec.rb
@@ -20,4 +20,37 @@ RSpec.describe "the admin index" do
       expect(page).to have_link("Invoices")
     end
   end
+
+  it "displays the top 5 customers" do
+    customer_1 = create(:customer)
+    customer_2 = create(:customer)
+    customer_3 = create(:customer)
+    customer_4 = create(:customer)
+    customer_5 = create(:customer)
+    customer_6 = create(:customer)
+    
+
+    invoice_1 = create(:invoice, customer: customer_1)
+    invoice_2 = create(:invoice, customer: customer_2)
+    invoice_3 = create(:invoice, customer: customer_3)
+    invoice_4 = create(:invoice, customer: customer_4)
+    invoice_5 = create(:invoice, customer: customer_5)
+    invoice_6 = create(:invoice, customer: customer_6)
+
+    transactions_for_invoice_1 = create_list(:transactions, 4, invoice: invoice_1)
+    transactions_for_invoice_2 = create_list(:transactions, 5, invoice: invoice_2)
+    transactions_for_invoice_3 = create_list(:transactions, 6, invoice: invoice_3)
+    transactions_for_invoice_4 = create_list(:transactions, 1, invoice: invoice_4)
+    transactions_for_invoice_5 = create_list(:transactions, 2, invoice: invoice_5)
+    transactions_for_invoice_6 = create_list(:transactions, 3, invoice: invoice_6)
+    
+    visit "/admin"
+
+    expect(customer_3).to appear_before(customer_2)
+    expect(customer_2).to appear_before(customer_1)
+    expect(customer_1).to appear_before(customer_6)
+    expect(customer_6).to appear_before(customer_5)
+    expect(page).to have_content(customer_5)
+    expect(page).not_to have_content(customer_4)
+  end
 end
