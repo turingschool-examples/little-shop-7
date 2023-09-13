4096ac78b0d345074e69b5f21e4516558674101f
Blake Sergesketter <blakesergesketter@gmail.com>
2023-09-13 15:33:00 -0500

    test: admin dashboard links.

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -6,4 +6,11 @@ RSpec.feature "Admin Dashboard" do
 
     expect(find(".header")).to have_content("Admin Dashboard")
   end
+
+  it "has link to the admin merchants index" do 
+    visit '/admin'
+
+    expect(page).to have_link("Admin: All Merchants")
+    expect(page).to have_link("Admin: All Invoices")
+  end
 end
