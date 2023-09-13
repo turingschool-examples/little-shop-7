d499a79d70ee8e7e4f608f93992baafc5828095b
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-09-13 14:12:54 -0600

    complete user story 2 - by adding links to the merchant show page

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe "the merchant show" do
 
     expect(page).to have_content(merchant.name)
   end
+
+  it "displays a link to the merchant items idex page and a link to the merchant invoices index" do 
+    merchant = Merchant.create(name: "Bob Burger")
+
+    visit "/merchants/#{merchant.id}/dashboard"
+
+    expect(page).to have_link('items_index')
+    expect(page).to have_link('invoices_index')
+
+  end
 end
+
+
+# As a merchant,
+# When I visit my merchant dashboard (/merchants/:merchant_id/dashboard)
+# Then I see link to my merchant items index (/merchants/:merchant_id/items)
+# And I see a link to my merchant invoices index (/merchants/:merchant_id/invoices)
