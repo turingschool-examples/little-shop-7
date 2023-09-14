cb330f779c5b5f1aa5ed6d77506be3354e63a23d
Joop Stark <stark.joop@gmail.com>
2023-09-14 10:15:22 -0600

    bug: Customer only counts successful transacations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices, through: :items
+  has_many :transactions, through: :invoices
   # has_many :invoices
   has_many :customers, through: :invoices
 end
