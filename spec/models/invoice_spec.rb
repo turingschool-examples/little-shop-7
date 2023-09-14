c2b3a0d42bb0fd9424b6be97bb82f3702f48a081
Joop Stark <stark.joop@gmail.com>
2023-09-14 14:59:44 -0600

    model test: invoice.formatted_date

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -23,6 +23,14 @@ RSpec.describe Invoice, type: :model do
       
       expect(Invoice.incomplete).to eq([@invoice_2a, @invoice_2d, @invoice_3a, @invoice_3b])
     end
+
+    it "can format a date" do
+      @customer_1 = Customer.create!(first_name: "Frodo", last_name: "Baggins")
+
+      @invoice_1k = Invoice.create!(status: "completed", customer: @customer_1, created_at: "2012-03-25 09:54:09 UTC")
+
+      expect(@invoice_1k.formatted_date).to eq("Sunday, March 25, 2012")
+    end
   end
 
 
