4c630829317b7132e586610883be91f7a74a9fbb
Joop Stark <stark.joop@gmail.com>
2023-09-13 22:32:50 -0600

    model: top_customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,16 @@ require "csv"
 
 class Customer < ApplicationRecord
   has_many :invoices
+  has_many :transactions, through: :invoices
+
+  def self.top_customers
+    find_by_sql(
+      "SELECT customers.*, COUNT(transactions.result)
+	    FROM customers 
+	    INNER JOIN invoices ON invoices.customer_id = customers.id 
+	    INNER JOIN transactions ON transactions.invoice_id = invoices.id
+	    GROUP BY customers.id
+	    ORDER BY count DESC
+	    LIMIT 5")
+  end
 end
