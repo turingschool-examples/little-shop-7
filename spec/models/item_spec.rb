f2a286e872698b0608088d91004f6d38ceeff374
Allan Evans <oneblankpage27@gmail.com>
2023-09-14 16:15:53 -0600

    Mostly completion of US 5, just need check for format

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through(:invoice_items) }
   end
+
+  it "can format a date" do
+    @customer_1 = Customer.create!(first_name: "Frodo", last_name: "Baggins")
+
+    @invoice_1k = Invoice.create!(status: "completed", customer: @customer_1, created_at: "2012-03-25 09:54:09 UTC")
+
+    expect(@invoice_1k.formatted_date).to eq("Sunday, March 25, 2012")
+  end
 end
