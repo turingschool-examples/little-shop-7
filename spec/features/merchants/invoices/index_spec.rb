7674d6bf9d9259a8998ca7d61b1052d53d212673
Joop Stark <stark.joop@gmail.com>
2023-09-15 16:43:03 -0600

    refactor: removed lauchies

--- spec/features/merchants/invoices/index_spec.rb
+++ spec/features/merchants/invoices/index_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe "Merchant Invoice Index page" do
     within "#merchant_name" do 
       expect(page).to have_content(@merchant1.name)
     end
-    save_and_open_page
     within "#merchant_invoices" do 
       expect(page).to have_content(@invoice_1.id)
     end
