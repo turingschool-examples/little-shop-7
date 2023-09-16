1d2da2263586cdf9b1dd5b999db85c20f45f827c
Cory Powell <coryrpowell@gmail.com>
2023-09-16 17:15:23 -0400

    started the tests for user story 4

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe "merchant#show" do
   before(:each) do
     @merchant_1 = create(:merchant)
     @merchant_2 = create(:merchant)
+    @customer_1 = create(:first_name, :last_name)
+    @customer_2 = create(:first_name, :last_name)
+    @customer_3 = create(:first_name, :last_name)
+    @customer_4 = create(:first_name, :last_name)
+    @customer_5 = create(:first_name, :last_name)
     @item_1 = create(:item, merchant_id: @merchant_1.id, unit_price: 1234)
     @item_2 = create(:item, merchant_id: @merchant_1.id)
     @item_3 = create(:item, merchant_id: @merchant_1.id)
@@ -13,8 +18,10 @@ RSpec.describe "merchant#show" do
     @item_7 = create(:item, merchant_id: @merchant_2.id)
     @item_8 = create(:item, merchant_id: @merchant_2.id)
     @item_9 = create(:item, merchant_id: @merchant_2.id)
+    @invoice_1 = create(:invoice, customer_id: @customer_1.id)
+    @invoice_2 = create(:invoice, customer_id: @customer_1.id)
 
-    # @customer_1 = create(:first_name, :last_name)
+    @invoice_item_1 = create(:invoice_item, :item_id @item_1.id, :status "packaged")
   end
 
   describe "display merchant info" do
@@ -36,14 +43,40 @@ RSpec.describe "merchant#show" do
         expect(page).to have_link("Invoices")
       end
     end
+  end
 
+  describe "merchant top 5 customers" do 
     it "shows the top 5 customers with the largest successful transactions with this merchant" do
       visit "/merchants/#{@merchant_1.id}/dashboard"
 
       within("#top_five_customers-#{@merchant_1.id}") do
-        expect(page).to have_content([@customer_1.name, @customer_2.name, @customer_3.name, @customer_4.name, @customer_6.name])
-        expect(page).to have_content([@customer_1.amount_of_transactions, @customer_2.amount_of_transactions, @customer_3.amount_of_transactions, @customer_4.amount_of_transactions, @customer_6.amount_of_transactions])
+        expect(page).to have_content([@customer_1.first_name, @customer_2.first_name, @customer_3.first_name, @customer_4.first_name, @customer_5.first_name])
+        expect(page).to have_content([@customer_1.amount_of_transactions, @customer_2.amount_of_transactions, @customer_3.amount_of_transactions, @customer_4.amount_of_transactions, @customer_5.amount_of_transactions])
       end
     end
+  end
+
+  describe "merchant items ready to ship" do
+    it "has a list of items that have been ordered and not yet shipped" do
+      visit "/merchants/#{@merchant_1.id}/dashboard"
+
+      within("#items_to_ship-#{@merchants.id}") do
+        expect(page).to have_content(@item_1.name)
+      end
+
+    end
+
+
+    #not sure if this will work and I can't check until I get a working method
+    
+    # it "displays the id of the invoice for the ordered item and a link to the merchant's invoice show page" do
+    #   visit "/merchants/#{@merchant_1.id}/dashboard"
+
+    #   within("#items_to_ship-#{@merchants.id}") do
+    #   expect(page).to have_link("#{@item_1.invoice.id}")
+
+    # end
+
+
   end
 end
