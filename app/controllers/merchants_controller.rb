f6f550fdb1f3a1bdd5053c986200efd4057b4f6f
Cory Powell <coryrpowell@gmail.com>
2023-09-16 15:40:50 -0400

    added the instance variable to display items ready_to_ship method

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,6 +6,7 @@ class MerchantsController < ApplicationController
   def show
     @merchants = Merchant.find(params[:merchant_id])
     @top_5 = @merchants.customers.order_customers_by_transactions
+    @items_to_ship = @merchants.items.ready_to_ship
   end
 
 
