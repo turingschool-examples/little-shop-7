d6bbc45114c9d9c8bebe886d4f2819acfdea84fc
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-09-19 12:37:02 -0600

    test: rearrange tests in item_spec to be under instance/method categories, add test to merchant_spec for most_popular_item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,21 +12,23 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of(:description) }
   end
 
-  describe "unit_price_formatted" do
-    it "can convert unit_price to dollars" do
-      item = Item.new(unit_price: 12345)
-      expect(item.unit_price_formatted).to eq(123.45)
+  describe "instance methods" do
+    describe "unit_price_formatted" do
+      it "can convert unit_price to dollars" do
+        item = Item.new(unit_price: 12345)
+        expect(item.unit_price_formatted).to eq(123.45)
+      end
+  
+      it "can handle zero" do
+        item = Item.new(unit_price: 0)
+        expect(item.unit_price_formatted).to eq(0)
+      end
     end
-
-    it "can handle zero" do
-      item = Item.new(unit_price: 0)
-      expect(item.unit_price_formatted).to eq(0)
-    end
-  end
-
-  describe "ready_to_ship" do
-    it "can display items with a status of 'packaged'" do
-      
+  
+    describe "ready_to_ship" do
+      it "can display items with a status of 'packaged'" do
+        
+      end
     end
   end
 end
