04b61c3ceecf785650aa54c3d3f356a558bf108e
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-09-19 09:53:25 -0500

    update the invoice method total_revenue to be written in ar

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -12,6 +12,6 @@ class Invoice < ApplicationRecord
   end
 
   def total_revenue
-    invoice_items.sum { |item| item.quantity * item.unit_price }
+    invoice_items.sum("quantity * unit_price")
   end
 end
