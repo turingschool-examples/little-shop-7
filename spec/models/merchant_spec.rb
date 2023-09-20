c51313a13207b053bf246849e93c5ed9b50dc755
Cory Powell <coryrpowell@gmail.com>
2023-09-20 15:11:25 -0400

    test for the ready_to_ship method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -122,4 +122,43 @@ RSpec.describe Merchant, type: :model do
       expect(merchant_3.top_date).to eq(Date.new(2021, 4, 10))
     end
   end
+
+  describe "ready_to_ship" do
+    it "finds all the invoice items with the status" do
+      merchant_1 = create(:merchant)
+      customer_1 = create(:customer)
+      customer_2 = create(:customer)
+      customer_3 = create(:customer)
+      customer_4 = create(:customer)
+      customer_5 = create(:customer)
+      
+      item_1 = create(:item, merchant_id: merchant_1.id)
+      item_2 = create(:item, merchant_id: merchant_1.id)
+      item_3 = create(:item, merchant_id: merchant_1.id)
+      item_4 = create(:item, merchant_id: merchant_1.id)
+      item_5 = create(:item, merchant_id: merchant_1.id)
+
+      invoice_1 = create(:invoice, customer_id: customer_1.id, created_at: "2012-03-25 09:54:09 UTC")
+      invoice_2 = create(:invoice, customer_id: customer_2.id, created_at: "2012-03-26 09:54:09 UTC")
+      invoice_3 = create(:invoice, customer_id: customer_3.id, created_at: "2012-03-27 09:54:09 UTC")
+      invoice_4 = create(:invoice, customer_id: customer_4.id, created_at: "2012-03-28 09:54:09 UTC")
+      invoice_5 = create(:invoice, customer_id: customer_5.id)
+
+      invoice_item_1 = create(:invoice_item, item_id: item_1.id, invoice_id: invoice_1.id, status: 1)
+      invoice_item_2 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_2.id, status: 1)
+      invoice_item_3 = create(:invoice_item, item_id: item_3.id, invoice_id: invoice_3.id, status: 0)
+      invoice_item_4 = create(:invoice_item, item_id: item_4.id, invoice_id: invoice_4.id, status: 1)
+      invoice_item_5 = create(:invoice_item, item_id: item_5.id, invoice_id: invoice_5.id, status: 1)
+
+      transaction_1 = create(:transaction, invoice_id: invoice_1.id, result: 1)
+      transaction_2 = create(:transaction, invoice_id: invoice_2.id, result: 1)
+      transaction_3 = create(:transaction, invoice_id: invoice_3.id, result: 1)
+      transaction_4 = create(:transaction, invoice_id: invoice_4.id, result: 1)
+      transaction_5 = create(:transaction, invoice_id: invoice_5.id, result: 1)
+
+      expect(merchant_1.ready_to_ship).to match_array([item_1, item_2, item_4, item_5])
+      expect(merchant_1.ready_to_ship).to_not include([item_3])
+    end
+  end
 end
+
