9cd333b33a2d7821a34aa2e9410acd39e21f519e
Joseph Lee <jhjlee702@gmail.com>
2023-10-30 17:04:34 -0600

    set up and import invoices from csv

---
+++ db/migrate/20231030225709_create_invoice.rb
@@ -0,0 +1,10 @@
+class CreateInvoice < ActiveRecord::Migration[7.0]
+  def change
+    create_table :invoices do |t|
+      t.integer :customer_id
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_30_221028) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_225709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_30_221028) do
     t.integer "invoice_id"
     t.integer "quantity"
     t.integer "unit_price"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.integer "customer_id"
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
