7624ccf940d3ef456765314cc0bf1386105f69fa
Joseph Lee <jhjlee702@gmail.com>
2023-10-30 16:24:17 -0600

    imported invoice_items from csv

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_30_213836) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_221028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invoice_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "invoice_id"
+    t.integer "quantity"
+    t.integer "unit_price"
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ lib/tasks/import_invoice_items.rake
@@ -0,0 +1,19 @@
+namespace :import_invoice_items do
+  desc "TODO"
+  task invoice_items: :environment do
+    require "csv"
+
+    csv_text = File.read(Rails.root.join("db", "data", "invoice_items.csv"))
+    csv = CSV.parse(csv_text, :headers => true, :encoding => "ISO-8859-1")
+    csv.each do |row|
+      t = InvoiceItem.new
+      t.item_id = row["item_id"]
+      t.invoice_id = row["invoice_id"]
+      t.quantity = row["quantity"]
+      t.unit_price = row["unit_price"]
+      t.status = row["status"]
+      t.save
+    end
+  end  
+  puts "Seeded bro bro"
+end
