b66bf37db805966966d719c8e95c5467cbe6dd27
Joseph Lee <jhjlee702@gmail.com>
2023-10-30 17:10:29 -0600

    set up and import item data from csv

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_30_225709) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_230729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_30_225709) do
   create_table "invoices", force: :cascade do |t|
     t.integer "customer_id"
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "unit_price"
+    t.integer "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
