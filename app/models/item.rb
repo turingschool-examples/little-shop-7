2e7d24264831631acba58cb36534de25fb4eee74
Jimmy Trulock <ttrulock23@gmail.com>
2023-10-31 15:08:41 -0600

    add many relationship to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   validates :name
   validates :description
   validates :unit_price
+  has_many :merchant_id
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Item, type: :model do
+  describe "relationships" do
+    it { should have_many(:merchant_id)}
+  end
+
   describe "validations" do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:unit_price)}
