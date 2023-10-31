b587a4f5761779d69c15029f69b39286e0a8a114
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-10-30 20:24:44 -0500

    update rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require "factory_bot_rails"
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
@@ -34,7 +35,6 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryBot::Syntax::Methods
