d881249b9d1f723e2d65390bfd05d70642319fb4
Sam Tran <samuelttran@gmail.com>
2023-10-31 11:57:05 -0600

    Add factory bot config line

---
+++ spec/features/merchants/invoices/index_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Invoices Index", type: :feature do
+  # As a merchant,
+  # When I visit my merchant's invoices index (/merchants/:merchant_id/invoices)
+  # Then I see all of the invoices that include at least one of my merchant's items
+  # And for each invoice I see its id
+  # And each id links to the merchant invoice show page
+  describe "When a user visits merchant's invoices index, there is information" do
+    it "They see all invoices with at least one of merchant's items" do
+      visit "/merchants/#{@merchant_1.id}/invoices"
+
+      expect(page).to have_content("My Invoices", count:1)
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,6 +66,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  # Factory Bot
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
