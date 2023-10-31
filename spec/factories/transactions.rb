be6ef1f6a1c2aaef4e86bd5e857f64e2c7061331
Sam Tran <samuelttran@gmail.com>
2023-10-31 14:07:37 -0600

    Correct faker method to credit_card_number

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryBot.define do
   factory :transaction do
-    credit_card_number { Faker::Business.product_name.delete('-') }
+    credit_card_number { Faker::Business.credit_card_number.delete('-') }
     credit_card_expiration_date { "0#{rand(1..9)}/#{25..28}" }
     result { rand(0..1)}
   end
