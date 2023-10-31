af952c217bbe9d50cab84bd4ff1cdbae1e76a53c
Blake Sergesketter <blakesergesketter@gmail.com>
2023-10-31 10:04:13 -0500

    US19- admin dashboard. Testing for path, route, controller, dashboard page. All passing.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "welcome#index"
+  namespace :admin do 
+    get "/", to: "dashboards#welcome" 
+  end
 end
