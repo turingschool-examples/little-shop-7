46b9d2b4dfec2c63d2311ef62f66bfd57b81715a
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-10-31 21:08:13 -0600

    Model and feature testing for US2, all tests pass

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,4 @@
 class Invoice < ApplicationRecord
+  belongs_to :merchant
   enum status: {"cancelled": 0, "in progress": 1, "completed": 2}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   get "/merchants/:id/items", to: "merchant_items#index"
   get "/merchants/:id/items/:item_id", to: "merchant_items#show"
+  get "/merchants/:id/invoices", to: "merchant_invoices#show"
 
 end
