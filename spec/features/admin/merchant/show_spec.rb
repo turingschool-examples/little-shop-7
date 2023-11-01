cfeb5810b5a366b6406fd3e4654b3825a3d9a6e3
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-01 15:54:17 -0600

    Add update test to admin merchant show page

--- spec/features/admin/merchant/show_spec.rb
+++ spec/features/admin/merchant/show_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Admin Merchant Show Page" do 
   it "a user can click on any Merchant in Admin Merchant Index to get taken to that Merchan'ts Show Page" do 
+    # as an admin
     merchant = Merchant.create!({name: "Schroeder-Jerde"})
 
     visit "/admin/merchants"
@@ -10,6 +11,20 @@ RSpec.describe "Admin Merchant Show Page" do
 
     expect(current_path).to eq("/admin/merchants/#{merchant.id}")
     expect(page).to have_content("#{merchant.name}")
+  end
+
+  it "can update the merchant's information" do 
+    # as an admin
+    visit "/admin/merchants/#{merchant.id}"
+
+    expect(page).to have_link("Update")
+
+    click("Update")
+
+    visit "/admin/merchants/#{merchant.id}/edit"
+
+    expect(page).to have_content
+
   end
 
 end 
