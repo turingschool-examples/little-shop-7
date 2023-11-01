22255e1f23765c03ba0c474c8b825b3c048765dd
Anthea Yur <acyur6@gmail.com>
2023-11-01 14:38:24 -0500

    Finish relationship adding and creating new migration for Invoice Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_01_191946) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_01_193628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,11 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_01_191946) do
   end
 
   create_table "invoice_items", force: :cascade do |t|
+    t.bigint "invoice_id", null: false
+    t.bigint "item_id", null: false
     t.integer "quantity"
     t.integer "unit_price"
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
+    t.index ["item_id"], name: "index_invoice_items_on_item_id"
   end
 
   create_table "invoices", force: :cascade do |t|
@@ -59,5 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_01_191946) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "items"
   add_foreign_key "items", "merchants"
 end
