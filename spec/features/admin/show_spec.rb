8193130225b3389253a1423a0960245346ab0047
Kam Kennedy <kameronk013@gmail.com>
2023-11-01 17:17:25 -0600

    write test and method for top 5 customers

--- spec/features/admin/show_spec.rb
+++ spec/features/admin/show_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe "admin dashboard index page" do
 
     expect(page).to have_content("Welcome to the Admin Dashboard")
   end
+
+  it "Displays the names of the top 5 customers" do
+    visit admin_path
+    expect(page).to have_content("Ilene Phannerstill, 7")
+    expect(page).to have_content("Markus Grady, 6")
+    expect(page).to have_content("Rollin Morar, 6")
+    expect(page).to have_content("Amelia Schuster, 6")
+    expect(page).to have_content("Austen Wyman, 6")
+  end
 end
