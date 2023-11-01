e15695efc41b30ddaee4f8e0c7e69680c49033e1
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-01 16:17:52 -0600

    Feat/3US Update merchant model spec setup to use factories

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,29 +10,32 @@ RSpec.describe Merchant, type: :model do
   end
 
   before(:each) do
-    @merchant_1 = create(:merchant)
-    @customer_1 = create(:customer)
-    @customer_2 = create(:customer)
-    @customer_3 = create(:customer)
-    @customer_4 = create(:customer)
-    @customer_5 = create(:customer)
-    @customer_6 = create(:customer)
-    @transaction_1 = create(:transaction)
-    @transaction_2 = create(:transaction)
-    @transaction_3 = create(:transaction)
-    @transaction_4 = create(:transaction)
-    @transaction_5 = create(:transaction)
-    @transaction_6 = create(:transaction)
-    @transaction_7 = create(:transaction)
-    @transaction_8 = create(:transaction)
-    @transaction_9 = create(:transaction)
-    @transaction_10 = create(:transaction)
+    @merchant = create(:merchant)
+    @customer_succ_1 = create(:customer)
+    @customer_succ_2 = create(:customer)
+    @customer_succ_3 = create(:customer)
+    @customer_succ_4 = create(:customer)
+    @customer_succ_5 = create(:customer)
+    @customer_fail = create(:customer)
+    @invoice_succ_1 = create(:invoice, customer: @customer_succ_1)
+    @invoice_succ_2 = create(:invoice, customer: @customer_succ_2)
+    @invoice_succ_3 = create(:invoice, customer: @customer_succ_3)
+    @invoice_succ_4 = create(:invoice, customer: @customer_succ_4)
+    @invoice_succ_5 = create(:invoice, customer: @customer_succ_5)
+    @invoice_fail = create(:invoice, customer: @customer_fail)
+    @items = create_list(:item, 20)
+    @transactions_successful_1 = create_list(:transaction, 5, result: 0, invoice: @invoice_succ_1)
+    @transactions_successful_2 = create_list(:transaction, 4, result: 0, invoice: @invoice_succ_2)
+    @transactions_successful_3 = create_list(:transaction, 3, result: 0, invoice: @invoice_succ_3)
+    @transactions_successful_4 = create_list(:transaction, 2, result: 0, invoice: @invoice_succ_4)
+    @transactions_successful_5 = create_list(:transaction, 1, result: 0, invoice: @invoice_succ_5)
+    @transactions_successful_5 = create_list(:transaction, 5, result: 1, invoice: @invoice_fail)
   end
 
   describe "instance methods" do
     describe "#top_customers" do
       it "returns a list of customers with largest number of successful transactions" do
-        expect(@merchant_1.top_customers).to eq()
+        expect(@merchant.top_customers).to eq()
       end
     end
   end
