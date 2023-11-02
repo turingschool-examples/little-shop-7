24f75c11071a18ff0a5c8f5b0be394cc33bbef93
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-11-02 14:52:13 -0600

    Write test for user story 23 and update invoices to include explicit created at and updated at

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -12,16 +12,17 @@ RSpec.describe '/admin' do
         @customer6 = Customer.create!(first_name: "Heber", last_name: "Kuhn")
         @customer7 = Customer.create!(first_name: "Parker", last_name: "Daugherty")
 
-        @invoice1 = Invoice.create!(status: "completed", customer_id: @customer1.id)
-        @invoice2 = Invoice.create!(status: "completed", customer_id: @customer2.id)
-        @invoice3 = Invoice.create!(status: "completed", customer_id: @customer3.id)
-        @invoice4 = Invoice.create!(status: "completed", customer_id: @customer4.id)
-        @invoice5 = Invoice.create!(status: "completed", customer_id: @customer5.id)
-        @invoice6 = Invoice.create!(status: "completed", customer_id: @customer6.id)
-        @invoice7 = Invoice.create!(status: "completed", customer_id: @customer7.id)
-        @invoice8 = Invoice.create!(status: "in progress", customer_id: @customer7.id)
-        @invoice9 = Invoice.create!(status: "in progress", customer_id: @customer7.id)
-        @invoice10 = Invoice.create!(status: "cancelled", customer_id: @customer7.id)
+        @invoice1 = Invoice.create!(status: "completed", customer_id: @customer1.id, created_at: "2012-03-25 09:54:09 UTC", updated_at: "2012-03-25 09:54:09 UTC")
+        @invoice2 = Invoice.create!(status: "completed", customer_id: @customer2.id, created_at: "2012-03-12 05:54:09 UTC", updated_at: "2012-03-12 05:54:09 UTC")
+        @invoice3 = Invoice.create!(status: "completed", customer_id: @customer3.id, created_at: "2012-03-10 00:54:09 UTC", updated_at: "2012-03-10 00:54:09 UTC")
+        @invoice4 = Invoice.create!(status: "completed", customer_id: @customer4.id, created_at: "2012-03-24 15:54:10 UTC", updated_at: "2012-03-24 15:54:10 UTC")
+        @invoice5 = Invoice.create!(status: "completed", customer_id: @customer5.id, created_at: "2012-03-07 19:54:10 UTC", updated_at: "2012-03-07 19:54:10 UTC")
+        @invoice6 = Invoice.create!(status: "completed", customer_id: @customer6.id, created_at: "2012-03-09 01:54:10 UTC", updated_at: "2012-03-09 01:54:10 UTC")
+        @invoice7 = Invoice.create!(status: "completed", customer_id: @customer7.id, created_at: "2012-03-07 21:54:10 UTC", updated_at: "2012-03-07 21:54:10 UTC")
+        @invoice8 = Invoice.create!(status: "in progress", customer_id: @customer7.id, created_at: "2012-03-13 16:54:10 UTC", updated_at: "2012-03-13 16:54:10 UTC")
+        @invoice9 = Invoice.create!(status: "in progress", customer_id: @customer7.id, created_at: "2012-03-07 12:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
+        @invoice10 = Invoice.create!(status: "cancelled", customer_id: @customer7.id, created_at: "2012-03-06 21:54:10 UTC", updated_at: "2012-03-06 21:54:10 UTC")
+        @invoice11 = Invoice.create!(status: "in progress", customer_id: @customer7.id, created_at: "2012-03-08 20:54:10 UTC", updated_at: "2012-03-07 12:54:10 UTC")
 
         @tranaction1 = Transaction.create!(invoice_id: @invoice1.id, credit_card_number: "4654405418249632", credit_card_expiration_date: "04/27", result: "success")
         @tranaction2 = Transaction.create!(invoice_id: @invoice1.id, credit_card_number: "4654405418249632", credit_card_expiration_date: "04/27", result: "success")
@@ -97,15 +98,31 @@ RSpec.describe '/admin' do
 
         expect(page).to have_content("Incomplete Invoices")
         within('section', :text => "Incomplete Invoices") do
-          expect(page).to have_link(@invoice8.id)
-          expect(page).to have_link(@invoice9.id)
-          expect(page).to_not have_link(@invoice7.id)
-          expect(page).to_not have_link(@invoice10.id)
+          expect(page).to have_link("#{@invoice8.id}")
+          expect(page).to have_link("#{@invoice9.id}")
+          expect(page).to_not have_link("#{@invoice7.id}")
+          expect(page).to_not have_link("#{@invoice10.id}")
+          expect(page).to have_link("#{@invoice11.id}")
         end
 
-        click_link(@invoice8.id)
+        click_link("#{@invoice8.id}")
 
         expect(current_path).to eq("/admin/invoices/#{@invoice8.id}")
+      end
+
+      # US 23, Admin Dashboard Invoices sorted by least recent
+      it 'has invoice created date ordered by oldest to newest' do
+        visit '/admin'
+
+        expect(page).to have_content("Incomplete Invoices")
+        within('section', :text => "Incomplete Invoices") do
+          expect("#{@invoice9.id}").to appear_before("#{@invoice11.id}")
+          expect("#{@invoice11.id}").to appear_before("#{@invoice8.id}")
+          expect(page).to have_content("#{@invoice9.id}: Created Wednesday, March 7, 2012")
+          expect(page).to have_content("#{@invoice11.id}: Created Thursday, March 8, 2012")
+          expect(page).to have_content("#{@invoice8.id}: Created Tuesday, March 13, 2012")
+        end
+
       end
     end
   end
