16d6daafc388c27bef4268a73e0618692dbf2870
Charles Ren <cren04@syr.edu>
2023-11-01 23:09:19 -0700

    psuedocode

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -13,10 +13,10 @@ class Merchant < ApplicationRecord
   # And next to each customer name I see the number of successful transactions they have
   # conducted with my merchant
   def top_five_customers
-    customers.joins(:transactions)
-    .where(transactions: {result: 0})
-    .group("customers.id")
-    .order("count(transactions.id) DESC")
+    customers.joins(:transactions) # can join directly bc of our relations above
+    .where(transactions: {result: 0}) # for all successful transactions
+    .group("customers.id") # group customers
+    .order("count(transactions.id) DESC") # sort by # of transactions in desc order
     .limit(5)
     # .count('transactions.id') to get the successful transactions for each customer in hash
   end
