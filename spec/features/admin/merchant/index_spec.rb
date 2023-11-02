0dafe3fec980049860c11122a28b66c1895192f2
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-01 23:56:11 -0600

    Implement FactoryBot merchant creator, removing 14 lines of set up code, and allowing developer to test 'n' number of records.

--- spec/features/admin/merchant/index_spec.rb
+++ spec/features/admin/merchant/index_spec.rb
@@ -1,27 +1,16 @@
 require "rails_helper"
 
-RSpec.describe "Admin Merchants Index", type: feature do 
-  it "displays the name of each merchant in the system" do 
+RSpec.describe "Admin Merchants Index", type: feature do
 
-    merch1 = Merchant.create!({name: "Schroeder-Jerde"})
-    merch2 = Merchant.create!({name: "Klein, Rempel and Jones"})
-    merch3 = Merchant.create!({name: "Willms and Sons"})
-    merch4 = Merchant.create!({name: "Cummings-Thiel"})
-    merch5 = Merchant.create!({name: "Williamson Group"})
-    merch6 = Merchant.create!({name: "Bernhard-Johns"})
-    merch7 = Merchant.create!({name: "Osinski, Pollich and Koelpin"})
-    merch8 = Merchant.create!({name: "Hand-Spencer"})
+  it "displays the name of each merchant in the system" do 
+    merch_list = create_list(:merchant, 50)
 
     visit "/admin/merchants"
 
-    expect(page).to have_content("#{merch1.name}")
-    expect(page).to have_content("#{merch2.name}")
-    expect(page).to have_content("#{merch3.name}")
-    expect(page).to have_content("#{merch4.name}")
-    expect(page).to have_content("#{merch5.name}")
-    expect(page).to have_content("#{merch6.name}")
-    expect(page).to have_content("#{merch7.name}")
-    expect(page).to have_content("#{merch8.name}")
+    merch_list.each do |merch|
+      expect(page).to have_content(merch.name)
+    end
+    save_and_open_page
   end
   
 end 
