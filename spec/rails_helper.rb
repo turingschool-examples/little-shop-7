ffb0fb5d602b99f656e15106b9601540d302c07d
Sooyung Kim <kimx1279@gmail.com>
2023-11-01 18:20:32 -0600

    Add require simplecov on rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'simplecov'
 SimpleCov.start
 require 'support/faker'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
