10e08fd62e1c2b107597928d0f6c01855b67a85f
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-02 09:16:37 -0600

    Feat/3US Begin implementing top customers in merchants_controller and show page

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,6 @@
 class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:merchant_id])
+    @top_customers = Customer.top_five_customers(@merchant)
   end
 end
