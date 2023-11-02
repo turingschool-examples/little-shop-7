e0db776781a7b7c4e1b22ca2cfa841c0b7e39a02
Scott DeVoss <scottdevoss@yahoo.com>
2023-11-02 13:42:24 -0600

    added item default status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_01_195622) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_193628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_01_195622) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id", null: false
+    t.integer "status", default: 1
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
