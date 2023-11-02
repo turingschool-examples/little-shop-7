a19a52202bb1698b4f1776d23d4981bca8ae38f8
Charles Ren <cren04@syr.edu>
2023-11-01 18:19:24 -0700

    delete extraneous line

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe Customer, type: :model do
   before :each do
     @customer = create(:customer)
-    # @item_1 = create(:item)
   end
 
   describe "relationships" do
