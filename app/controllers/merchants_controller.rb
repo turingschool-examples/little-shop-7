072cf543cc22a48de63fa2c312f4923773c7773a
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-02 16:17:02 -0600

    Ref/6US Comment out non-functional code

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,7 +1,7 @@
 class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:merchant_id])
-    @invoice = Invoice.find(params[:merchant_id])
-    @item = Item.find(params[:merchant_id])
+    # @invoice = Invoice.find(params[:merchant_id])
+    # @item = Item.find(params[:merchant_id])
   end
 end
