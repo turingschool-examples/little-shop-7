1775a3cc5d9202aa2dfdc85f1d562ffa23df0336
Nathan Trautenberg <Natester123@mac.com>
2023-11-02 10:44:21 -0600

    fixed routes, views, test file, and controller for user story 2.

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     credit_card_number { (Faker::Business.credit_card_number).delete("-") }
     credit_card_expiration_date { "0#{rand(1..9)}/#{rand(25..28)}" }
     result { rand(0..1) }
+    association :invoice, factory: :invoice
   end
 end
