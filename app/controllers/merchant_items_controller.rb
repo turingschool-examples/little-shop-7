84209df06b76ec0f14baed0e9dbcc3efe7c1805e
Sooyung Kim <kimx1279@gmail.com>
2023-11-02 11:32:39 -0600

    Update routes

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -2,7 +2,7 @@ class MerchantItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
-
+  
   def show
     @item = Item.find(params[:item_id])
   end
