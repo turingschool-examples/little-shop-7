966f5a4e3d909e70fc4ac69770ddd10b9b5cc602
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-03 06:40:13 -0600

    Feat/7US Write tests in merchant items index spec

---
+++ spec/features/merchants/items/show_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "merchant item show page" do
+  before :each do
+    @merchant_1 = create(:merchant)
+    @item_1 = create(:item, merchant: @merchant_1)
+  end
+
+  # US7
+  it "" do
+    
+  end
+end
