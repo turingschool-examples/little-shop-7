fc74223d149f80dd7765f8cfd250262b3b7979a8
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-03 09:37:59 -0600

    RSpec tests to confirm the new button to disable/enable merchant links in the admin merchants index page is working as expected - disabling the link when currently enabled and button is clicked or enabling the link if it is currently disabled and the button is clicked

--- spec/features/admin/merchant/index_spec.rb
+++ spec/features/admin/merchant/index_spec.rb
@@ -3,14 +3,41 @@ require "rails_helper"
 RSpec.describe "Admin Merchants Index", type: feature do
 
   it "displays the name of each merchant in the system" do 
-    merch_list = create_list(:merchant, 50)
+    merch_list = create_list(:merchant, 5)
 
     visit "/admin/merchants"
 
     merch_list.each do |merch|
       expect(page).to have_content(merch.name)
     end
-    save_and_open_page
+  end
+  it "has a button next to each merchant's name to disable or enable that merchant" do 
+    
+    merch_list = create_list(:merchant, 5)
+    merchant = merch_list.sample
+    
+    visit "/admin/merchants"
+
+    within "#index-#{merchant.id}" do 
+      expect(page).to have_link(merchant.name)
+      expect(page).to have_button("Disable")
+      click_button "Disable"
+    end
+
+    expect(page).to have_content("The status has been disabled")
+    
+    within "#index-#{merchant.id}" do 
+    expect(page).to_not have_link(merchant.name)
+    expect(page).to have_button("Enable")
+    click_button "Enable"
+  end
+  
+    expect(page).to have_content("The status has been enabled")
+
+    within "#index-#{merchant.id}" do
+      expect(page).to have_link(merchant.name)
+      expect(page).to have_button("Disable")
+    end
   end
   
 end 
