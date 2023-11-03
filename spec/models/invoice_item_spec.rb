a601498042e3cb23eb268169f90c6647ba470c7e
Anthea Yur <acyur6@gmail.com>
2023-11-03 14:58:05 -0500

    Add model testing for price method on invoice item

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 RSpec.describe InvoiceItem, type: :model do
+  before :each do
+    test_data 
+  end
+
   describe "relationships" do
     it { should belong_to(:invoice) }
     it { should belong_to(:item) }
@@ -19,4 +23,10 @@ RSpec.describe InvoiceItem, type: :model do
     it { should define_enum_for(:status).with_values(pending: 0, packaged: 1, shipped: 2) }
   end
 
+  describe '#price' do
+    it 'returns the invoice item unit price with a decimal' do
+      testing_invoice = create(:invoice_item, unit_price: 2599)
+      expect(testing_invoice.price).to eq(25.99)
+    end
+  end
 end
