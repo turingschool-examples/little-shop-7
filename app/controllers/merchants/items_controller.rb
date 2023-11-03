595943981837404ebb7909c001e0fd89aa97aec2
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-03 09:46:11 -0600

    Feat/8US Write tests for merchant item edit page

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -14,6 +14,7 @@ class Merchants::ItemsController < ApplicationController
   end
 
   def update
-    
+    # @merchant = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
   end
 end
