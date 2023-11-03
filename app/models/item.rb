57209e3fdfc95026e1eac76be8c2d5dc80e789e9
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-02 19:57:57 -0600

    Resolved broken US5 test from US4 update

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,11 +21,11 @@ end
 
   def packaged
     return unless self.invoice_items.includes(:invoice_items).where(status: 1).exists?
-    return self
+    self
   end
 
   def invoice_id
-    return self.invoice_items.pluck(:invoice_id).first
+    return self.invoice_items.pluck(:invoice_id)
     
   end
 end
