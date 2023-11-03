a9ad0daf3d305dd217343ac55878f5442b63e32b
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-03 11:14:26 -0600

    Feat/8US Functionalize merchant item edit (still needs flash messages)

--- spec/features/merchants/items/edit_spec.rb
+++ spec/features/merchants/items/edit_spec.rb
@@ -11,12 +11,9 @@ RSpec.describe "merchant item edit page" do
     visit edit_merchant_item_path(@merchant_1, @item_1)
 
     within("#update_item_form") do
-      expect(page).to have_field(:name)
-      expect(page).to have_content(@item_1.name)
-      expect(page).to have_field(:description)
-      expect(page).to have_content(@item_1.description)
-      expect(page).to have_field(:unit_price)
-      expect(page).to have_content(@item_1.unit_price)
+      expect(page).to have_field(:name, with: @item_1.name)
+      expect(page).to have_field(:description, with: @item_1.description)
+      expect(page).to have_field(:unit_price, with: @item_1.unit_price)
       expect(page).to have_button("Update Item")
     end
   end
@@ -33,9 +30,9 @@ RSpec.describe "merchant item edit page" do
     end
 
     expect(current_path).to eq(merchant_item_path(@merchant_1, @item_1))
-    expect(page).to have_content(@item_1.name)
-    expect(page).to have_content(@item_1.description)
-    expect(page).to have_content(@item_1.format_price)
+    expect(page).to have_content("Fantastic Fidget")
+    expect(page).to have_content("a fantastically fun fidget!")
+    expect(page).to have_content("$123.45")
   end
 
   # US8
