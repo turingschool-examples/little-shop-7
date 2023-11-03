dc0c75bb5503fc6163384aa405e3905bbeaccdbd
Joseph Lee <jhjlee702@gmail.com>
2023-11-03 10:16:44 -0600

    take out invoice_item numericality check and add define_enum check in the respective spec

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,6 +7,6 @@ class InvoiceItem < ApplicationRecord
   validates :item_id, presence: true, numericality: true
   validates :quantity, presence: true, numericality: true
   validates :unit_price, presence: true, numericality: true
-  validates :status, presence: true, numericality: true
+  validates :status, presence: true
   
 end
