8a1645706f56e2833bfc004ad90238dba53557e5
Kam Kennedy <kameronk013@gmail.com>
2023-11-02 21:28:31 -0600

    pass all tests

--- spec/features/admin/show_spec.rb
+++ spec/features/admin/show_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "admin dashboard index page" do
-  before :each do
-    @merchant_1 = create(:merchant)
+  before(:each) do
+    @merchant = create(:merchant)
     @customer_1 = create(:customer)
     @customer_2 = create(:customer)
     @customer_3 = create(:customer)
@@ -15,18 +15,18 @@ RSpec.describe "admin dashboard index page" do
     @invoice_4 = create(:invoice, customer: @customer_4)
     @invoice_5 = create(:invoice, customer: @customer_5)
     @invoice_6 = create(:invoice, customer: @customer_6)
-    @item_1= create(:item, merchant: @merchant_1)
-    @item_2 = create(:item, merchant: @merchant_1)
-    @item_3 = create(:item, merchant: @merchant_1)
-    @item_4 = create(:item, merchant: @merchant_1)
-    @item_5 = create(:item, merchant: @merchant_1)
-    @item_6 = create(:item, merchant: @merchant_1)
-    @invoice_item_1 = create(:invoice_item, invoice: @invoice_1, item: @item_1, status: 1)
-    @invoice_item_2 = create(:invoice_item, invoice: @invoice_2, item: @item_2, status: 1)
-    @invoice_item_3 = create(:invoice_item, invoice: @invoice_3, item: @item_3, status: 0)
-    @invoice_item_4 = create(:invoice_item, invoice: @invoice_4, item: @item_4, status: 0)
+    @item_1 = create(:item, merchant: @merchant)
+    @item_2 = create(:item, merchant: @merchant)
+    @item_3 = create(:item, merchant: @merchant)
+    @item_4 = create(:item, merchant: @merchant)
+    @item_5 = create(:item, merchant: @merchant)
+    @item_6 = create(:item, merchant: @merchant)
+    @invoice_item_1 = create(:invoice_item, invoice: @invoice_1, item: @item_1)
+    @invoice_item_2 = create(:invoice_item, invoice: @invoice_2, item: @item_2)
+    @invoice_item_3 = create(:invoice_item, invoice: @invoice_3, item: @item_3)
+    @invoice_item_4 = create(:invoice_item, invoice: @invoice_4, item: @item_4)
     @invoice_item_5 = create(:invoice_item, invoice: @invoice_5, item: @item_5, status: 2)
-    @invoice_item_6 = create(:invoice_item, invoice: @invoice_6, item: @item_6, status: 2)
+    @invoice_item_6 = create(:invoice_item, invoice: @invoice_6, item: @item_6)
     @transaction_1 = create_list(:transaction, 5, invoice: @invoice_1, result: 0)
     @transaction_2 = create_list(:transaction, 4, invoice: @invoice_2, result: 0)
     @transaction_3 = create_list(:transaction, 3, invoice: @invoice_3, result: 0)
@@ -80,13 +80,14 @@ RSpec.describe "admin dashboard index page" do
   #user story 22 & 23
   it "Displays incomplete invoices with oldest first" do
     visit admin_path
-    expect(page).to have_content(@invoice_1.id)
-    expect(page).to have_content(@invoice_2.id)
-    expect(page).to have_content(@invoice_3.id)
-    expect(page).to have_content(@invoice_4.id)
-    expect(page).not_to have_content(@invoice_5.id)
-    expect(page).to have_link("#{@invoice_item_20.invoice_id}")
-    click_link("#{@invoice_item_20.invoice_id}")
-    expect(current_path).to eq("/admin/invoices/#{@invoice_item_20.invoice_id}")
+    save_and_open_page
+    expect(page).to have_content(@invoice_item_1.invoice_id)
+    expect(page).to have_content(@invoice_item_2.invoice_id)
+    expect(page).to have_content(@invoice_item_3.invoice_id)
+    expect(page).to have_content(@invoice_item_4.invoice_id)
+    expect(page).not_to have_content(@invoice_item_5.invoice_id)
+    expect(page).to have_link("#{@invoice_item_1.invoice_id}")
+    click_link("#{@invoice_item_1.invoice_id}")
+    expect(current_path).to eq("/admin/invoices/#{@invoice_item_1.invoice_id}")
   end
 end
