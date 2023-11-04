52f0a99cdfc55c08287f95c82375f9cb33066f27
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-04 16:15:26 -0600

    New merchant default value for 'status' attribute changed from 1, being 'enabled,' to 2, for 'disabled,' following user story instruction that new merchants be created with a default status of 'disabled.'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_02_193628) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_04_204154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_02_193628) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status", default: 1
+    t.integer "status", default: 0
   end
 
   create_table "transactions", force: :cascade do |t|
