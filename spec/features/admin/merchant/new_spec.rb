8f658aa597eaf97561407baf0f27f5dace487048
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-04 16:31:07 -0600

    Create feature tests for new admin merchant specs defined in user story #29.  Ensure the process admin users follow to create a new merchant follows user story 29 exactly - including being displayed in the 'Disabled Merchants' section when first created since their default status should be set to 'disabled' when first created.  All feature tests to create the new user pass.

---
+++ spec/features/admin/merchant/new_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "creating a new Merchant" do
+  it "links to the page from the admin merchants index" do 
+    visit admin_merchants_path
+
+    click_link("Create a new merchant")
+    expect(current_path).to eq(new_admin_merchant_path)
+  end
+
+  it "creates a new merchant" do 
+    new_merchant = create(:merchant)
+
+    visit new_admin_merchant_path
+
+    fill_in("Name", with: new_merchant.name)
+    click_button("Submit")
+
+    expect(current_path).to eq("/admin/merchants")
+
+    within(".disabled") do 
+      expect(page).to have_content(new_merchant.name)
+    end
+
+    within("#index-#{new_merchant.id}") do
+      expect(page).to have_button("Enable")
+    end
+
+    save_and_open_page
+  end
+end 
