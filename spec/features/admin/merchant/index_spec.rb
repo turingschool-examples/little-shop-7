040ba1bdc5ed6a9f712b681a8499dca53886a613
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-03 23:52:17 -0600

    Tests to ensure the merchants are accurately displayed in the section representing their current status - 'Enabled Merchants' or 'Disabled Merchants'

--- spec/features/admin/merchant/index_spec.rb
+++ spec/features/admin/merchant/index_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe "Admin Merchants Index", type: feature do
       expect(page).to have_content(merch.name)
     end
   end
+  
   it "has a button next to each merchant's name to disable or enable that merchant" do 
     
     merch_list = create_list(:merchant, 5)
@@ -27,10 +28,10 @@ RSpec.describe "Admin Merchants Index", type: feature do
     expect(page).to have_content("The status has been disabled")
     
     within "#index-#{merchant.id}" do 
-    expect(page).to_not have_link(merchant.name)
-    expect(page).to have_button("Enable")
-    click_button "Enable"
-  end
+      expect(page).to_not have_link(merchant.name)
+      expect(page).to have_button("Enable")
+      click_button "Enable"
+    end
   
     expect(page).to have_content("The status has been enabled")
 
@@ -39,5 +40,55 @@ RSpec.describe "Admin Merchants Index", type: feature do
       expect(page).to have_button("Disable")
     end
   end
-  
+
+  it "displays merchants in two sections - 'Enabled Merchants' and 'Disabled Merchants'" do 
+    # reset db, all merchants 'enabled', which is the default value in schema
+    merch_list = create_list(:merchant, 8)
+    
+    visit "/admin/merchants"
+
+    # check that all merchants are enabled
+    merch_list.each do |merch|
+      within ".enabled" do 
+        expect(page).to have_content("Enabled Merchants")
+        expect(page).to have_content(merch.name)
+      end
+    end
+    
+    # disable four merchants with lowest id's
+    idx = Merchant.minimum(:id)
+    stop = idx + 3
+    
+    while idx <= stop
+      merch = Merchant.find(idx)
+      merch.update({status: 0})
+      idx += 1
+    end
+    
+    merch_list_enabled = Merchant.where("id > ?", stop)
+    merch_list_disabled = Merchant.where("id <= ?", stop)
+
+    # disable the first four companies
+    merch_list_disabled.each do |merch| 
+      merch.update({status: 0})
+    end
+
+    visit "/admin/merchants"
+    
+    # check to make sure half of the merchants are in 'enabled' section and other half are in the 'disabled' section
+    merch_list_enabled.each do |merch|
+      within ".enabled" do 
+        expect(page).to have_content("Enabled Merchants")
+        expect(page).to have_content(merch.name)
+      end
+    end
+
+    merch_list_disabled.each do |merch|
+      within ".disabled" do
+        expect(page).to have_content("Disabled Merchants")
+        expect(page).to have_content(merch.name)
+      end
+    end
+  end
+
 end 
