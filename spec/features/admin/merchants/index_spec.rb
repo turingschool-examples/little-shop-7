cc3355e8ae64da96f648841cf095be3714bb6e3c
Charles Ren <cren04@syr.edu>
2023-11-04 19:18:59 -0700

    US 25, name link functionality

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe "Admin Merchants Index", type: :feature do
     expect(page).to have_content(@merchant2.name)
     expect(page).to have_content(@merchant3.name)
   end
+
+  #   As an admin,
+# When I click on the name of a merchant from the admin merchants index page (/admin/merchants),
+# Then I am taken to that merchant's admin show page (/admin/merchants/:merchant_id)
+# And I see the name of that merchant
+  # US 25
+  it "clicking the name redirects to merchant admin show page and displays the name" do
+    visit admin_merchants_path
+
+    expect(page).to have_link("#{@merchant1.name} ##{@merchant1.id}")
+    click_link("#{@merchant1.name} ##{@merchant1.id}")
+
+    expect(current_path).to eq(admin_merchant_path(@merchant1.id))
+
+    expect(page).to have_content(@merchant1.name)
+  end
 end
---
+++ spec/features/admin/merchants/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Admin Merchant Shoe Page", type: :feature do
+  before :each do
+    @merchant1 = create(:merchant)
+    @merchant2 = create(:merchant)
+    @merchant3 = create(:merchant)
+  end
+
+  # US 26
+  xit "" do
+
+  end
+end
