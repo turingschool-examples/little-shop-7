a16e590eb7d4a94ae5b3a7a0280514ab9774434d
Charles Ren <cren04@syr.edu>
2023-11-04 21:20:48 -0700

    US 26, displays a link to update merchant information

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,29 @@ class Admin::MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+
+    if merchant.update({name: params[:name]})
+
+      merchant.save
+      redirect_to admin_merchant_path(merchant.id)
+      flash[:alert] = "Merchant Successfully Updated"
+    else
+      redirect_to admin_merchant_path(merchant.id)
+      flash[:alert] = "Merchant Unsuccessfully Updated"
+    end
+  end
+
+  # if pet.update(pet_params)
+  #   redirect_to "/pets/#{pet.id}"
+  # else
+  #   redirect_to "/pets/#{pet.id}/edit"
+  #   flash[:alert] = "Error: #{error_message(pet.errors)}"
+  # end
 end
--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -8,7 +8,20 @@ RSpec.describe "Admin Merchant Shoe Page", type: :feature do
   end
 
   # US 26
-  xit "" do
+  it "displays a link to update merchant information" do
+    visit admin_merchant_path(@merchant1.id)
 
+    expect(page).to have_content("#{@merchant1.name} Show Page")
+    expect(page).to have_link("Update Merchant")
+    click_link("Update Merchant")
+
+    expect(current_path).to eq(edit_admin_merchant_path(@merchant1.id))
+
+    fill_in "Name", with: "Merchant 2.0"
+    click_on "Submit"
+
+    expect(current_path).to eq(admin_merchant_path(@merchant1.id))
+    expect(page).to have_content("Merchant 2.0 Show Page")
+    expect(page).to have_content("Merchant Successfully Updated")
   end
 end
