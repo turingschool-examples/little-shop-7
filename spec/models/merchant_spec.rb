5709cda703bcee9b5bf62104b5d84032569f18cf
Anthea Yur <acyur6@gmail.com>
2023-11-06 02:53:48 -0600

    Complete model testing for merchant to also include top_5

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,9 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Merchant, type: :model do
-  before :each do
-    test_data_2
-  end
 
   describe "relationships" do
     it { should have_many(:items) }
@@ -15,10 +12,19 @@ RSpec.describe Merchant, type: :model do
     it { should validate_presence_of(:name) }
   end
 
+  describe '#top_5_customers_from_transactions' do
+    it 'will list top 5 customers based on item purchases through invoices' do
+      test_data
+      expected_order = [@customer1, @customer2, @customer3, @customer4, @customer5]
+      expect(@merchant1.top_5_customers_from_transactions).to eq(expected_order)
+    end
+  end
 
   describe '#popular_items' do
     it 'list popular items ordered by revenue' do
+      test_data_2
       expect(@merchant1.popular_items).to eq([@item4, @item3, @item2, @item1, @item7])
     end
   end
+
 end
