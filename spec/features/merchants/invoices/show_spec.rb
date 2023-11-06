aca8c15df885098ef6f212f44dcef1daed192fca
Nathan Trautenberg <Natester123@mac.com>
2023-11-06 14:29:13 -0700

    created spec test for us16. added item and invoice_item elements to merchant invoice show page.

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -21,5 +21,49 @@ RSpec.describe 'merchant invoices index page (/merchants/:merchant_id/invoices)'
         expect(page).to have_content(@invoice1.created_at.strftime('%A, %B %d, %Y'))
         expect(page).to have_content(@invoice1.customer.name)
       end
+      it "shows all items on the invoice" do
+        # 16. Merchant Invoice Show Page: Invoice Item Information
+        # As a merchant
+        # When I visit my merchant invoice show page (/merchants/:merchant_id/invoices/:invoice_id)
+        # Then I see all of my items on the invoice including:
+        # - Item name
+        # - The quantity of the item ordered
+        # - The price the Item sold for
+        # - The Invoice Item status
+        # And I do not see any information related to Items for other merchants
+        visit "/merchants/#{@merchant1.id}/invoices/#{@invoice1.id}"
+        expect(page).to have_content(@item1.name)
+        expect(page).to have_content(@item2.name)
+        expect(page).to have_content(@item3.name)
+        expect(page).to have_content(@item4.name)
+        expect(page).to have_content(@item6.name)
+        expect(page).to have_content(@item7.name)
+        expect(page).to have_content(@item8.name)
+
+        expect(page).to have_content(@item1.unit_price)
+        expect(page).to have_content(@item2.unit_price)
+        expect(page).to have_content(@item3.unit_price)
+        expect(page).to have_content(@item4.unit_price)
+        expect(page).to have_content(@item6.unit_price)
+        expect(page).to have_content(@item7.unit_price)
+        expect(page).to have_content(@item8.unit_price)
+
+
+        expect(page).to have_content(@invoice_item1.status)
+        expect(page).to have_content(@invoice_item2.status)
+        expect(page).to have_content(@invoice_item3.status)
+        expect(page).to have_content(@invoice_item4.status)
+        expect(page).to have_content(@invoice_item6.status)
+        expect(page).to have_content(@invoice_item7.status)
+        expect(page).to have_content(@invoice_item8.status)
+
+        expect(page).to have_content(@invoice_item1.quantity)
+        expect(page).to have_content(@invoice_item2.quantity)
+        expect(page).to have_content(@invoice_item3.quantity)
+        expect(page).to have_content(@invoice_item4.quantity)
+        expect(page).to have_content(@invoice_item6.quantity)
+        expect(page).to have_content(@invoice_item7.quantity)
+        expect(page).to have_content(@invoice_item8.quantity)
+      end
     end
   end
