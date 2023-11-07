211535b31b0102628f87529975edd75bdba5450b
Nathan Trautenberg <Natester123@mac.com>
2023-11-07 12:35:19 -0700

    converted items ready to ship into ar query method from ruby method.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -24,19 +24,11 @@ class Merchant < ApplicationRecord
   end
 
   def items_ready_to_ship_ordered_oldest_to_newest
-    items_ready_to_ship = []
-
-    self.items.each do |item|
-      item.invoice_items.each do |invoice_item|
-        if invoice_item.status != 'shipped'
-          items_ready_to_ship << {
-            name: item.name,
-            invoice_id: invoice_item.invoice_id,
-            date: invoice_item.invoice.created_at.strftime('%A, %B %e, %Y')
-          }
-        end
-      end
-    end
-    items_ready_to_ship.sort_by! { |item| Date.strptime(item[:date], '%A, %B %e, %Y') }
+    self.items
+    .joins(:invoices)
+    .select("invoice_items.*", "items.name", "invoices.created_at AS date")
+    .where("invoice_items.status = ? OR invoice_items.status = ?", "0", "1")
+    .group('invoice_items.id', 'items.name', 'invoices.created_at')
+    .order("invoices.created_at asc")
   end
 end
