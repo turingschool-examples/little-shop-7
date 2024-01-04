d5a5fa8cd9474c20ababa85267730a38b265e8c5
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-04 14:57:55 -0700

    Remove unnecessary id creation code after PK merchant table issue fixed.

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
   factory :merchant do
-#     # id { rand(1..99) }
-#     # id { (1..110).to_a.sample }
-#     initialize_with { new(id: rand(1..99)) }
     name { Faker::Company.name }
   end
 end
