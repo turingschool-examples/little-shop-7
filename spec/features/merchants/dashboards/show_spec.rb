bdbe85091d2db8753e1239f94b85e1892f768d74
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-04 15:57:19 -0700

    Replace setup merchant with factory bot merchant

--- spec/features/merchants/dashboards/show_spec.rb
+++ spec/features/merchants/dashboards/show_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe "Merchant Dashboards", type: :feature do
   before(:each) do 
     @merchant = create(:merchant)
-    # @merchant = Merchant.create!(name: "Home Depot")
   end
 
   it "shows the name of the merchant" do 
