708eacbec4fc46b5b11fb14ae0a382981cb0def0
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-04 14:59:00 -0700

    Change merchant setup data to use factory bot (no longer needing hard-coded id).

--- spec/features/merchants/dashboards/show_spec.rb
+++ spec/features/merchants/dashboards/show_spec.rb
@@ -2,14 +2,14 @@ require "rails_helper"
 
 RSpec.describe "Merchant Dashboards", type: :feature do
   before(:each) do 
-    @merchant = Merchant.create!(id: 1, name: "Home Depot")
+    @merchant = create(:merchant)
+    # @merchant = Merchant.create!(name: "Home Depot")
   end
 
   it "shows the name of the merchant" do 
-    require 'pry'; binding.pry
     visit "/merchants/#{@merchant.id}/dashboard"
     
-    expect(page).to have_content(@merchants.first.name)
+    expect(page).to have_content(@merchant.name)
   end
   
 end 
