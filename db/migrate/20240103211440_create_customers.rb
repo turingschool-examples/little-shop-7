91de2c6d34a2f1879e59cc58dadb12ef95c2b2a3
Sam Puttman <daputter2@gmail.com>
2024-01-04 12:37:01 -0500

    Modify existing migrations to fix bug with primary keys. Add migration to define foreign keys.

--- db/migrate/20240103211440_create_customers.rb
+++ db/migrate/20240103211440_create_customers.rb
@@ -1,7 +1,6 @@
 class CreateCustomers < ActiveRecord::Migration[7.0]
   def change
-    create_table :customers, id: false do |t|
-      t.integer :id
+    create_table :customers do |t|
       t.string :first_name
       t.string :last_name
       t.datetime :created_at
--- db/migrate/20240103221346_create_merchants.rb
+++ db/migrate/20240103221346_create_merchants.rb
@@ -1,11 +1,9 @@
 class CreateMerchants < ActiveRecord::Migration[7.0]
   def change
-    create_table :merchants, id: false do |t|
-      t.integer :id
+    create_table :merchants do |t|
       t.string :name
       t.datetime :created_at
       t.datetime :updated_at
-
     end
   end
 end
