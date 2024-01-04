e5255a787bee704fe0cc5d969cbd02d5bf05fda9
Victor Sanchez <vsanantone@gmail.com>
2024-01-04 15:38:10 -0600

    Factorybot/fix: association :transaction

--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :transaction do
+    association :invoice
     credit_card_number { "1234 5567 8659 3020" }
     credit_card_expiration_date { "09/28" }
     result { [0,1].sample } #look into this 
