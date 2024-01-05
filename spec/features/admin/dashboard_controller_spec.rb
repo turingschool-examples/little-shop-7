6011d11e4b3831aeb1ba2e930d6ef32bf47d511b
Sam Puttman <daputter2@gmail.com>
2024-01-05 15:51:44 -0500

    Add tests for US-20

--- spec/features/admin/dashboard_controller_spec.rb
+++ spec/features/admin/dashboard_controller_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe "the admin dashboard show" do
     # Then I see a header indicating that I am on the admin dashboard
     expect(page).to have_content("Admin Dashboard")
   end
+
+  # 20. Admin Dashboard Links
+  it 'When I visit the admin dashboard' do
+    # As an admin,
+    # When I visit the admin dashboard (/admin)
+    visit "/admin/"
+    # Then I see a link to the admin merchants index (/admin/merchants)
+    expect(page).to have_link('Merchants', href: admin_merchants_path)
+    # And I see a link to the admin invoices index (/admin/invoices)
+    expect(page).to have_link('Invoices', href: admin_invoices_path)
+  end
 end
