c662021c59e4c25083b0691ff04d2f82b6d054dd
Victor Sanchez <vsanantone@gmail.com>
2024-01-05 17:58:20 -0600

    fix: refactor admin invoices/merchants routes and add dashboard index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    resources :invoices, only: [:index, :show]
-  end  
-
 
   # Defines the root path route ("/")
   # root "articles#index"
-  namespace :admin do 
-    resources :merchants, only: [:index]
 
+  namespace :admin do
+     root to: "dashboard#index"
+     resources :invoices, only: [:index, :show]
+     resources :merchants, only: [:index]
+  end
   # get "/merchants/:id/dashboard", to: "merchants/dashboard#show"
 
   resources :merchants do
