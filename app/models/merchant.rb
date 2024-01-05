3ae75f39cd75fce23f647cf904bc8c35eade9d8b
Nathan Lambertson <lambertones@gmail.com>
2024-01-04 21:44:37 -0600

    complete US 3, but needs much refactoring and further research

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,20 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
-  
+
   has_many :items
+
+  def transactions
+    Transaction.joins(invoice: { invoice_items: :item }).where(items: { merchant_id: id })
+  end
+
+  def top_customers
+    Customer.joins(invoices: :transactions)
+            .joins("JOIN invoice_items ON invoice_items.invoice_id = invoices.id")
+            .joins("JOIN items ON items.id = invoice_items.item_id AND items.merchant_id = #{id}")
+            .select('customers.*, COUNT(transactions.id) AS transactions_count')
+            .where(transactions: { result: 'success' })  # Adjust based on how you define a successful transaction
+            .group('customers.id')
+            .order('transactions_count DESC')
+            .limit(5)
+  end
 end
