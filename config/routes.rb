f3dff6f3dfa8db5e32fe8382faaf97c52cbce517
Quin Nordmark <quinnordmark@gmail.com>
2024-01-04 19:50:33 -0700

    items#show route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/merchants/:merchant_id/items", to: "merchant_items#index"
+  get "/merchants/:merchant_id/items",          to: "merchant_items#index"
+  get "/merchants/:merchant_id/items/:item_id", to: "items#show"
 
   namespace :admin do
     get '/', to: 'dashboard#index'
