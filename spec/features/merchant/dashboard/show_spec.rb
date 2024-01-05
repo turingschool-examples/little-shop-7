5c94322b543a4a3edd3ef5d91177572dffc35564
Dylan Perry <dylanbperry@gmail.com>
2024-01-04 16:02:07 -0800

    Update dashboard show test to utilize factory bot test object

--- spec/features/merchant/dashboard/show_spec.rb
+++ spec/features/merchant/dashboard/show_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.describe "the merchant dashboard show page" do
   it "shows the merchant's name" do
-    merchant_lego = Merchant.create(name: "Lego")
+    merchant = create(:merchant)
 
-    visit "/merchants/#{merchant_lego.id}/dashboard"
+    visit "/merchants/#{merchant.id}/dashboard"
 
     expect(page).to have_content(merchant_lego.name)
   end
