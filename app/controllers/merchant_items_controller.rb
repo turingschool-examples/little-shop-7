791d1eaa23e22892a4dbb67e06464cb4fd64400f
Quin Nordmark <47016826+n0rdie@users.noreply.github.com>
2024-01-04 17:00:11 -0700

    Deleted comment from controller

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -1,7 +1,6 @@
 class MerchantItemsController < ApplicationController
     def index
         @merchant = Merchant.find(params[:merchant_id])
-        #@merchant_items = @merchant.items
     end
 
     private
