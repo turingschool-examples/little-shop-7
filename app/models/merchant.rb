eb237c3ea92c6e0b8f41dc422b37decbe1e767d7
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-01-05 13:04:17 -0700

    fix: Make sure that status for Merchant is a Hash Enum

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,5 +4,8 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
   validates :name, presence: true
 
-  enum status: [:disabled, :enabled]
+  enum status: {
+    disabled: 0,
+    enabled: 1
+  }
 end
