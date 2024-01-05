c8b04ad2ce67610aa0e4ada17e18115e4a19cdd4
Nash <141100397+s2an@users.noreply.github.com>
2024-01-05 11:04:19 -0500

    Complete User Story 32 in two different ways

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -4,20 +4,32 @@ RSpec.describe "admin/invoices/index_spec.rb", type: :feature do
   it "test User Story 32: Admin Invoices Index Page" do
     customers = create_list(:customer, 3)
     invoices = create_list(:invoice, 3)
+
     # As an admin, 
-    # is there a way to test for this? Are there permissions?
+    # is there a way to test for "as an admin"? Are there permissions?
+
     # When I visit the admin Invoices index (/admin/invoices)
     visit admin_invoices_path
-    # require 'pry'; binding.pry
     # Then I see a list of all Invoice ids in the system
     Invoice.all.each do |invoice|
       expect(page).to have_content(invoice.id)
-    # Each id links to the admin invoice show page
+      # Each id links to the admin invoice show page
       expect(page).to have_link(href: admin_invoice_path(invoice))
     end
-    # # Click link to confirm it works
-    #   click_link(admin_invoice_path(invoice))
-    # # Confirm that the link is to the correct path
-    #   expect(current_path).to eq(admin_invoice_path(invoice))
+  end
+
+  it "tests US32 differently" do
+    # The above test should be fine, due to my understanding of href, 
+    # but this might be more thourough?
+    # This also requires the actual creation of the show page
+    customer = create(:customer)
+    invoice = create(:invoice)
+
+    visit admin_invoices_path
+  
+    # Click link to confirm it works
+    click_link(invoice.id.to_s)
+    # Confirm that the link is to the correct path
+    expect(current_path).to eq(admin_invoice_path(invoice))
   end
 end
