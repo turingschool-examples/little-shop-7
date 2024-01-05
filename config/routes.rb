f9b18dcb1c70cce1c7a5058aebf494b468b2c554
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-01-05 00:02:04 -0700

    remove duplicate info from routes to try to get rid of merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  # get "/merchants/:id/dashboard", to: "merchants/dashboard#show"
-
-  resources :merchants do
-    resources :dashboard
-  end
   get "/merchants/:id/invoices", to: "merchants#invoices"
-  
 end
