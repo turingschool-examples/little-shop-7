9899adb71572ff4f8c1dfbcd991455d38c03b5bc
Quin Nordmark <quinnordmark@gmail.com>
2024-01-04 19:48:09 -0700

    US-07 spec testing

--- spec/features/merchant_items/index_spec.rb
+++ spec/features/merchant_items/index_spec.rb
@@ -19,4 +19,17 @@ RSpec.describe "MerchantItem index", type: :feature do
         # And I do not see items for any other merchant
         expect(page).to_not have_content("Big Mac")
     end 
+
+    it "7. Merchant Items Show Page" do
+        # (merchants/:merchant_id/items)
+        visit "/merchants/#{@merchant_1.id}/items"
+        # When I click on the name of an item from the merchant items index page, 
+        click_on("Moldy Cheese")
+        # Then I am taken to that merchant's item's show page (/merchants/:merchant_id/items/:item_id)
+        expect(page).to have_current_path("/merchants/#{@merchant_1.id}/items/#{@item_1.id}")
+        # And I see all of the item's attributes including: Name, Description, Current Selling Price
+        expect(page).to have_content("Moldy Cheese")
+        expect(page).to have_content("ew")
+        expect(page).to have_content("1199")
+    end
 end
