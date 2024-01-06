dbbd678b9a6f194377c6c9898406a98f5ab75952
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-06 14:21:04 -0700

    Add conditions to #update in merch_items controller for enable/disable feature

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -18,9 +18,27 @@ class MerchantItemsController < ApplicationController
     merchant = Merchant.find(params[:merchant_id])
     item = Item.find(params[:id])
     item.update(item_params)
-
-    redirect_to "/merchants/#{merchant.id}/items/#{item.id}"
-    flash[:alert] = "Information has been successfully updated"
+    if params[:commit].starts_with?("Update")
+      redirect_to "/merchants/#{merchant.id}/items/#{item.id}"
+      flash[:alert] = "Information has been successfully updated"
+    # elsif params[:commit] == 0 || 1
+    #   require 'pry'; binding.pry
+    #   params[:item][:enabled] || params[:item][:disabled]
+    #   item.update(status: params[:commit])
+    #   redirect_to "/merchants/#{merchant.id}/items"
+      elsif params[:item][:enabled]
+        item.update(status: 0)
+        # item.update(status: params[:item][:enabled])
+        redirect_to "/merchants/#{merchant.id}/items"
+      else params[:item][:disabled]
+        item.update(status: 1)
+        # item.update(status: params[:item][:disabled])
+        redirect_to "/merchants/#{merchant.id}/items"
+      end
+    # else
+    #   redirect_to "/merchants/#{merchant.id}/items/#{item.id}"
+    #   flash[:alert] = "Information has been successfully updated"
+    
 
   end
 
