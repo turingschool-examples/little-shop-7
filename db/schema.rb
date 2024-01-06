9a7128b6f2347c88f21d82639a84c3fe21a94fc8
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-06 14:23:22 -0700

    Add status as integer and default of 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_03_183301) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_06_190654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_03_183301) do
     t.bigint "merchant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
