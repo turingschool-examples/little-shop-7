696ce7238fa0ce3e22dddebe9f65c646cf767bb6
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-06 14:24:27 -0700

    Add test for items status enums

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe Item, type: :model do
     it { should have_many(:invoice_items) }
     it { should have_many(:invoices).through :invoice_items }
   end
+
+  describe "enums" do
+    it "has an enum for status" do
+      merchant = Merchant.create!(name: "Test Merchant")
+      item1 = merchant.items.create!(name: "Test Item 1", description: "Test Description 1", unit_price: 100, status: 0)
+      item2 = merchant.items.create!(name: "Test Item 2", description: "Test Description 2", unit_price: 200, status: 1)
+      
+      expect(item1.enabled?).to be true
+      expect(item2.disabled?).to be true
+    end
+  end
+
+
 end
