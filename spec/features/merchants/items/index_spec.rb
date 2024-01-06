e28f5f45760c2c38cb7ac0da8572386548545efd
Nathan Lambertson <lambertones@gmail.com>
2024-01-06 16:43:28 -0600

    Delete more old comments

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -1,7 +1,5 @@
 require "rails_helper"
-# Merchant Items Index Page: As a merchant, When I visit my merchant items index page
-# (merchants/:merchant_id/items) I see a list of the names of all of my items
-# And I do not see items for any other merchant
+
 RSpec.describe "Merchants/Items Index Page", type: :feature do
   it "shows a list of all of my items and not for other merchants" do
     merchant_1 = Merchant.create!(name: "Walmart")
@@ -31,11 +29,6 @@ RSpec.describe "Merchants/Items Index Page", type: :feature do
   end
 
   it "has a disable/enable status and a link to change status next to each item" do
-    # 9. Merchant Item Disable/Enable: As a merchant When I visit my items index page 
-    # (/merchants/:merchant_id/items) Next to each item name I see a button to disable 
-    # or enable that item. When I click this button Then I am redirected back to the items index 
-    # And I see that the items status has changed
-
     merchant_1 = Merchant.create!(name: "Walmart")
     merchant_2 = Merchant.create!(name: "Amazon")
     walmart_items = create_list(:item, 37, merchant_id: merchant_1.id)
@@ -43,13 +36,13 @@ RSpec.describe "Merchants/Items Index Page", type: :feature do
 
     visit "/merchants/#{merchant_1.id}/items"
 
-    within "#item-#{merchant_1.items.first.id}" do 
+    within "#item-#{merchant_1.items.first.id}" do
       expect(page).to have_content("enabled")
       expect(page).to have_button("enable")
       expect(page).to have_button("disable")
     end
 
-    within "#item-#{merchant_1.items.last.id}" do 
+    within "#item-#{merchant_1.items.last.id}" do
       expect(page).to have_content("enabled")
       expect(page).to have_button("enable")
       expect(page).to have_button("disable")
@@ -58,10 +51,40 @@ RSpec.describe "Merchants/Items Index Page", type: :feature do
     end
 
     expect(current_path).to eq("/merchants/#{merchant_1.id}/items")
-    within "#item-#{merchant_1.items.last.id}" do 
+    within "#item-#{merchant_1.items.last.id}" do
       expect(page).to have_content("disabled")
     end
-
   end
 
+  it "has two sections, one for 'Enabled Items' and one for 'Disabled Items'" do
+    merchant = Merchant.create!(name: "Snow-Globes and Used Clothing")
+    items1 = create_list(:item, 23, merchant_id: merchant.id, status: "enabled")
+    items2 = create_list(:item, 17, merchant_id: merchant.id, status: "disabled")
+    items3 = create_list(:item, 11, merchant_id: merchant.id, status: "enabled")
+    items4 = create_list(:item, 13, merchant_id: merchant.id, status: "disabled")
+
+    visit "/merchants/#{merchant.id}/items"
+
+    within('section#enabled-items') do
+      expect(page).to have_content("Status: enabled")
+      items1.each do |item|
+        expect(page).to have_content(item.name)
+      end
+
+      items2.each do |item|
+        expect(page).to have_content(item.name)
+      end
+    end
+
+    within('section#disabled-items') do
+      expect(page).to have_content("Status: disabled")
+      items2.each do |item|
+        expect(page).to have_content(item.name)
+      end
+
+      items4.each do |item|
+        expect(page).to have_content(item.name)
+      end
+    end
+  end
 end
