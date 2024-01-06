4511fe21da0a47de1b3079cda426f4c10c1e6ab4
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-01-06 14:24:13 -0700

    Add test for US-9 including some within blocks

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -29,4 +29,39 @@ RSpec.describe "Merchants/Items Index Page", type: :feature do
       expect(page).to_not have_content(walmart_item.name)
     end
   end
+
+  it "has a disable/enable status and a link to change status next to each item" do
+    # 9. Merchant Item Disable/Enable: As a merchant When I visit my items index page 
+    # (/merchants/:merchant_id/items) Next to each item name I see a button to disable 
+    # or enable that item. When I click this button Then I am redirected back to the items index 
+    # And I see that the items status has changed
+
+    merchant_1 = Merchant.create!(name: "Walmart")
+    merchant_2 = Merchant.create!(name: "Amazon")
+    walmart_items = create_list(:item, 37, merchant_id: merchant_1.id)
+    amazon_items = create_list(:item, 43, merchant_id: merchant_2.id)
+
+    visit "/merchants/#{merchant_1.id}/items"
+
+    within "#item-#{merchant_1.items.first.id}" do 
+      expect(page).to have_content("enabled")
+      expect(page).to have_button("enable")
+      expect(page).to have_button("disable")
+    end
+
+    within "#item-#{merchant_1.items.last.id}" do 
+      expect(page).to have_content("enabled")
+      expect(page).to have_button("enable")
+      expect(page).to have_button("disable")
+
+      click_button "disable"
+    end
+
+    expect(current_path).to eq("/merchants/#{merchant_1.id}/items")
+    within "#item-#{merchant_1.items.last.id}" do 
+      expect(page).to have_content("disabled")
+    end
+
+  end
+
 end
