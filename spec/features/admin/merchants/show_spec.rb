d5231a7159945703b7ac4e58643a54f5f6b8ebcf
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2024-01-06 18:22:32 -0500

    User Story 25 Not Completed

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -6,16 +6,16 @@ require 'rails_helper'
 RSpec.describe "Admin Merchant Show Page" do 
   before(:each) do 
     @merchants = create_list(:merchant, 10)
-    @merchant_1 = @merchants.first
+    @merchant_1 = create(:merchant)
   end
     
    it "can see the name of that merchant" do 
-     visit "/admin_merchants/#{@merchant_1.id}"
-     #visit admin_merchants_path(@merchant_1.id)
+     visit "/admin/merchants/#{@merchant_1.id}"
+     #visit admin_merchant_path(@merchant_1)
 
-     within "admin_merchants-{@merchant_1.id}"
-     @merchants.each do |merchant|
-      expect(page).to have_content(merchant.name)
+     within "admin_merchants-#{@merchant_1.id}" do
+     #@merchants.each do |merchant|
+      expect(page).to have_content(@merchant_1.name)
     end
   end
 end
