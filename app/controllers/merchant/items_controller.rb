527fc6501a9c379cf7b4136aa6fe2556e9d85859
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-07 01:32:47 -0700

    Routes working fine, shortened them to reduce verbosity, add simplicity

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,6 +1,12 @@
-class Merchant::ItemsController < ApplicationController
-    def index
-        @merchant = Merchant.find(params[:merchant_id])
+class Merchant::ItemsController < ApplicationController 
+    def index 
+      @merchant = Merchant.find(params[:merchant_id])
+      @items = @merchant.items
+    end
+
+    def show 
+      @item = Item.find(params[:id])
+      @merchant = @item.merchant
     end
 
 end
