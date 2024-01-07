e2c0541163a80a8c15ed9ac74e605c9641a78c60
Sam Puttman <daputter2@gmail.com>
2024-01-07 13:15:40 -0500

    Add method to customer model to retrieve top 5 customers by successful transactions. Implemented method in admin dashboard controller. Updated admin dashboard index view to show top 5 customers.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < ApplicationController
   layout 'admin/layouts/admin'
 
   def index
+    @top_customers = Customer.top_customers_by_successful_transaction
   end
 end
