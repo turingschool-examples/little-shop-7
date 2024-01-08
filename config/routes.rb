e4f45e096be16a55c1080c945c5963b92d1e4484
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2024-01-07 19:14:35 -0500

    Add edit and update resources to Admin Merchants routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
 
   namespace :admin do
      root to: "dashboard#index"
-    resources :merchants, only:[:index,:show]
+    resources :merchants, only:[:index,:show, :edit,:update]
     resources :invoices, only: [:index, :show]
   end  
 
   # get "/merchants/:id/dashboard", to: "merchants/dashboard#show"
-
   resources :merchants do
    resources :dashboard
   end
