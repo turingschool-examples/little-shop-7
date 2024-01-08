c5dcc29664b5ae83b364bd6214a47549f3e78325
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2024-01-07 20:57:56 -0500

    Add validation test to admin merchant merchant_spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Merchant, type: :model do
   describe "assocations" do
     it { should have_many :items }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
 end
