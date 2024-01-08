8e2d2bb0f54096960b11ef18f51a3bd2e8591c82
Dylan Perry <dylanbperry@gmail.com>
2024-01-08 13:02:31 -0800

    Reformat test blocks with user story information

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -1,21 +1,34 @@
 require "rails_helper"
 
 RSpec.describe "the merchant dashboard index page" do
-  it "shows the merchant's name (User Story 1)" do
-    merchant = create(:merchant)
+  describe "User Story 1" do
+    # As a merchant,
+    # When I visit my merchant dashboard (/merchants/:merchant_id/dashboard)
+    # Then I see the name of my merchant
 
-    visit "/merchants/#{merchant.id}/dashboard"
+    it "shows the merchant's name" do
+      merchant = create(:merchant)
 
-    expect(page).to have_content(merchant.name)
+      visit "/merchants/#{merchant.id}/dashboard"
+
+      expect(page).to have_content(merchant.name)
+    end
   end
 
-  it "shows links to the merchant's item and invoice indices (User Story 2)" do
-    merchant = create(:merchant)
+  describe "User Story 2" do
+    # As a merchant,
+    # When I visit my merchant dashboard (/merchants/:merchant_id/dashboard)
+    # Then I see link to my merchant items index (/merchants/:merchant_id/items)
+    # And I see a link to my merchant invoices index (/merchants/:merchant_id/invoices)
+    
+    it "shows links to the merchant's item and invoice indices" do
+      merchant = create(:merchant)
 
-    visit "/merchants/#{merchant.id}/dashboard"
+      visit "/merchants/#{merchant.id}/dashboard"
 
 
-    expect(page).to have_link 'View Items', href: "/merchants/#{merchant.id}/items"
-    expect(page).to have_link 'View Invoices', href: "/merchants/#{merchant.id}/invoices"
+      expect(page).to have_link 'View Items', href: "/merchants/#{merchant.id}/items"
+      expect(page).to have_link 'View Invoices', href: "/merchants/#{merchant.id}/invoices"
+    end
   end
 end
