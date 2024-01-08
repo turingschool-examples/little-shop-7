3dabc889947083c571005a31b3df7d73960fddfb
Nathan Lambertson <lambertones@gmail.com>
2024-01-08 15:03:32 -0600

    update tes data

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -152,7 +152,7 @@ RSpec.describe "Merchants/Items Index Page", type: :feature do
     invoice_item5 = invoice2.invoice_items.create!(item_id: item5.id, quantity: 4, unit_price: 4, status: 2)
     invoice_item6 = invoice3.invoice_items.create!(item_id: item6.id, quantity: 1, unit_price: 1030, status: 2)
     invoice_item7 = invoice3.invoice_items.create!(item_id: item7.id, quantity: 4, unit_price: 1070, status: 2)
-    invoice_item8 = invoice4.invoice_items.create!(item_id: item8.id, quantity: 3, unit_price: 1200, status: 2)
+    invoice_item8 = invoice4.invoice_items.create!(item_id: item8.id, quantity: 3, unit_price: 1200000, status: 2)
     invoice_item9 = invoice4.invoice_items.create!(item_id: item9.id, quantity: 5, unit_price: 600, status: 2)
     invoice_item10 = invoice4.invoice_items.create!(item_id: item10.id, quantity: 1, unit_price: 2100, status: 2)
     invoice_item11 = invoice5.invoice_items.create!(item_id: item11.id, quantity: 5, unit_price: 2345100, status: 2)
@@ -169,16 +169,16 @@ RSpec.describe "Merchants/Items Index Page", type: :feature do
     transaction7 = invoice7.transactions.create!(credit_card_number: 1238553460128476, credit_card_expiration_date: "04/23", result: 0)
 
     visit merchant_items_path(merchant_1)
-
+save_and_open_page
     expect(current_path).to eq("/merchants/#{merchant_1.id}/items")
     expect(page).to have_content("Top 5 Money-Makers:")
     expect(page).to have_content("Product: elephant Earned: $117255.0")
     expect(page).to have_content("Product: car seat cooler Earned: $1617.0")
     expect(page).to have_content("Product: sorry notes Earned: $312.9")
     expect(page).to have_content("Product: syrup Earned: $260.8")
-    expect(page).to have_content("Product: zoozah Earned: $63.72")
-    
-    expect("Earned: $117255.0").to appear_before("Earned: $63.72")
+    expect(page).to have_content("Product: stash Earned: $42.8")
+
+    expect("Earned: $117255.0").to appear_before("Earned: $42.8")
   end
 end
 # Merchant Items Index: 5 most popular items. As a merchant When I visit my items index page
