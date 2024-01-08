f74fb5232db35897ba11714c9852f96d94cb495b
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2024-01-07 19:06:34 -0500

    Add Update and edit Action To Admin Merchant

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,4 +6,15 @@ class Admin::MerchantsController < ApplicationController
   def show 
     @merchant = Merchant.find(params[:id])
   end
+
+  def edit 
+    @merchant = Merchant.find(params[:id])
+  end
+  
+  def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(name: params[:"Merchant Name"])
+    redirect_to admin_merchant_path(merchant)
+    flash[:success]="Information has succesfully been updated"
+  end
 end
