9639cd19c877f827a4beba6369a4882f017786c8
Sam Puttman <daputter2@gmail.com>
2024-01-08 18:13:11 -0500

    Fix spacing

--- spec/features/admins/dashboard_controller_spec.rb
+++ spec/features/admins/dashboard_controller_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe "the admin dashboard show" do
     @customer4 = Customer.create!(first_name: "Second")
     @customer5 = Customer.create!(first_name: "First")
     @customer6 = Customer.create!(first_name: "Bob")
-
     @invoice = Invoice.create!(customer: @customer1)
     Transaction.create!(invoice: @invoice, result: "success")
 
@@ -36,14 +35,11 @@ RSpec.describe "the admin dashboard show" do
     @test_customer = Customer.create!(first_name: "Test", last_name: "Customer")
     @merchant = Merchant.create!(name: "Test Merchant")
     @item = Item.create!(name: "Test Item", description: "Test Description", unit_price: 100, merchant: @merchant)
-    
 
     @incomplete_invoice1 = Invoice.create!(customer: @test_customer, status: "in progress")
     InvoiceItem.create!(invoice: @incomplete_invoice1, item: @item, status: "packaged")
-
     @incomplete_invoice2 = Invoice.create!(customer: @test_customer, status: "in progress")
     InvoiceItem.create!(invoice: @incomplete_invoice2, item: @item, status: "pending")
-
     @complete_invoice = Invoice.create!(customer: @test_customer, status: "completed")
     InvoiceItem.create!(invoice: @complete_invoice, item: @item, status: "shipped")
   end
@@ -58,10 +54,8 @@ RSpec.describe "the admin dashboard show" do
   describe "User Story 20" do
     it "has all necessary elements" do
       visit admin_dashboard_path
-
       expect(page).to have_selector("h1", text: "Little Esty Shop")
       expect(page).to have_selector("h1", text: "Admin Dashboard")
-
       expect(page).to have_link("Merchants", href: admin_merchants_path)
       expect(page).to have_link("Invoices", href: admin_invoices_path)
     end
@@ -69,7 +63,6 @@ RSpec.describe "the admin dashboard show" do
 
   describe "User Story 21" do
     it "displays the top customers" do
-
       visit admin_dashboard_path
       within(".top-customers") do
         expect(page).to have_selector("h2", text: "Top Customers")
@@ -84,15 +77,11 @@ RSpec.describe "the admin dashboard show" do
 
   describe "User Story 22" do
     it "lists the IDs of incomplete invoices as links" do
-      
       visit admin_dashboard_path
-
-      within('.incomplete-invoices') do
+      within(".incomplete-invoices") do
         expect(page).to have_selector("h2", text: "Incomplete Invoices")
-
         expect(page).to have_link(@incomplete_invoice1.id, href: admin_invoice_path(@incomplete_invoice1))
         expect(page).to have_link(@incomplete_invoice2.id, href: admin_invoice_path(@incomplete_invoice2))
-
         expect(page).not_to have_link(@complete_invoice.id, href: admin_invoice_path(@complete_invoice))
       end
     end
