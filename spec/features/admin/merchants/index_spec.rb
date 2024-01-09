79dec8d2ad32851e151cce791e21f21579bc151b
Nash <141100397+s2an@users.noreply.github.com>
2024-01-09 15:54:32 -0500

    Reformat previous user stories RSpec tests

--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -6,15 +6,32 @@ RSpec.describe "admin merchant index" do
     @merchants = create_list(:merchant, 10)
   end
 
-  it " can see the name of each merchant in the system" do
-    # 24. Admin Merchants Index
-
+  describe "User Story 24" do
     # As an admin,
     # When I visit the admin merchants index (/admin/merchants)
-    visit(admin_merchants_path) 
     # Then I see the name of each merchant in the system
-    @merchants.each do |merchant|
-      expect(page).to have_content(merchant.name)
+    
+    it "can see the name of each merchant in the system" do
+
+      visit(admin_merchants_path)
+
+      @merchants.each do |merchant|
+        expect(page).to have_content(merchant.name)
+      end
     end
   end
+
+  describe "User Story 27" do
+    # As an admin,
+    # When I visit the admin merchants index (/admin/merchants)
+    # Then next to each merchant name I see a button to disable or enable that merchant.
+    # When I click this button
+    # Then I am redirected back to the admin merchants index
+    # And I see that the merchant's status has changed
+    
+    it "adds an enable/disable button" do
+      
+    end
+  end
+
 end
