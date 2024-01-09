183784c203010e2afa91a42d6705a309e2842c7c
Sam Puttman <daputter2@gmail.com>
2024-01-09 18:03:58 -0500

    write test for table

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "the admin invoices show" do
     @item = Item.create!(name: "Test Item", description: "Test Description", unit_price: 100, merchant: @merchant)
 
     @incomplete_invoice1 = Invoice.create!(customer: @test_customer, created_at: 5.days.ago, status: "in progress")
-    InvoiceItem.create!(invoice: @incomplete_invoice1, item: @item, status: "packaged")
+    InvoiceItem.create!(invoice: @incomplete_invoice1, item: @item, quantity: 5, unit_price: 100, status: "packaged")
   end
 
   it "displays the correct information for an invoice" do
@@ -22,5 +22,16 @@ RSpec.describe "the admin invoices show" do
 
     expect(page).to have_content(@incomplete_invoice1.customer.first_name)
     expect(page).to have_content(@incomplete_invoice1.customer.last_name)
+  end
+
+  it "displays invoice item data on a table" do
+    visit admin_invoice_path(@incomplete_invoice1)
+
+    within "table" do
+      expect(page).to have_content(@item.name)
+      expect(page).to have_content("5")
+      expect(page).to have_content("$1.00")
+      expect(page).to have_content("Packaged")
+    end
   end
 end
