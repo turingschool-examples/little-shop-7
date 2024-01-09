f96190ada3d9188422c5e79dc8acc99764105c30
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-09 02:21:00 -0700

    Changes to merchant dash index feature spec tests, taking advantage of factory bots create_list method, but with the ability to create predictable data for values that need to remain consistent from test to test.

--- spec/features/merchant/dashboard/index_spec.rb
+++ spec/features/merchant/dashboard/index_spec.rb
@@ -1,44 +1,42 @@
 require "rails_helper"
-
 RSpec.describe "Merchant Dashboards", type: :feature do
   before(:each) do 
-    @merchants = create_list(:merchant, 5)
-    @merchants.first.items = create_list(:item, 5)
-    # @merchant_2 = create(:merchant)
-    # @merchants = create_list(:merchant, 10)
-    # @merchants.each do |merch|
-    #   Merchant.create!(name: merch.name)
-    # end
-    require 'pry'; binding.pry
-    # @items_merch_1.createcreate_list(:item, 10)
-   
-  end
+    @merchants = create_list(:merchant, 10)
+    @customers = create_list(:customer, 10)
+    @invoices = create_list(:invoice, 20)
+    @items = create_list(:item, 5)
+    @invoice_items = create_list(:invoice_item, 20)
+    @transactions = create_list(:transaction, 5)
+  end 
 
   it "shows the name of the merchant" do 
-    visit "/merchants/#{@merchant.id}/dashboard"
-    
-    expect(page).to have_content(@merchant.name)
+    visit "/merchants/#{@merchants.first.id}/dashboard"
+
+    expect(page).to have_content(@merchants.first.name)
   end
 
   it "has links to the merchant items index and the merchant invoices index" do 
-    visit "/merchants/#{@merchant.id}/dashboard"
+    visit "/merchants/#{@merchants.last.id}/dashboard"
     
     expect(page).to have_link("Invoices")
     expect(page).to have_link("Items")
 
     click_link "Items" 
-    expect(current_path).to eq("/merchants/#{@merchant.id}/items")
+    expect(current_path).to eq("/merchants/#{@merchants.last.id}/items")
 
-    visit "/merchants/#{@merchant.id}/dashboard"
+    visit "/merchants/#{@merchants.first.id}/dashboard"
 
     click_link "Invoices" 
-    expect(current_path).to eq("/merchants/#{@merchant.id}/invoices")
+    expect(current_path).to eq("/merchants/#{@merchants.first.id}/invoices")
   end
 
   it "has a list of the top five customers who have conducted the largest number of transactions with a merchant" do 
-    visit "/merchants/#{@merchant.id}/dashboard"
+    load_data
 
-    expect(page).to have
+    visit "/merchants/#{@merchants.first.id}/dashboard"
+    require 'pry'; binding.pry
+    create_transaction(5, 10)
+    
   end
-  
+
 end 
