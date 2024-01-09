f150828b6aeb251a011354bc7f8390074b0021d7
Martin Chavez <martinchavez1203@gmail.com>
2024-01-08 23:10:53 -0700

    US 36 complete

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -6,4 +6,16 @@ class Admin::InvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:id])
   end
+
+  def update
+    invoice = Invoice.find(params[:id])
+    if params[:status] == "cancelled"
+      invoice.update!(status: :cancelled)
+    elsif params[:status] == "completed"
+      invoice.update!(status: :completed)
+    elsif params[:status] == "in progress"
+      invoice.update!(status: :in_progress)
+    end 
+    redirect_to admin_invoice_path(invoice)
+  end
  end
