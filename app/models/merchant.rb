f006f0b79145f9005f0a7d182b9b54829725c360
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-01-09 12:52:59 -0700

    feat: Add database schema to doc

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,4 +8,14 @@ class Merchant < ApplicationRecord
     disabled: 0,
     enabled: 1
   }
+
+  def revenue
+    # Notes on Revenue Calculation:
+      # - Only invoices with at least one successful transaction should count towards revenue
+        .where("transactions.result = ?", "success")
+      # - Revenue for an invoice should be calculated as the sum of the revenue of all invoice items
+      # - Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price)
+    require 'pry'; binding.pry
+    .where("transactions.result = ?", "success")
+  end
 end
