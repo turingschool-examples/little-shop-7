e998a03ffe58c709073dfb70faada93ac63c6d6f
Sam Puttman <daputter2@gmail.com>
2024-01-08 19:17:39 -0500

    Refactor test data. Add test for User Story 23.

--- spec/features/admins/dashboard_controller_spec.rb
+++ spec/features/admins/dashboard_controller_spec.rb
@@ -36,17 +36,19 @@ RSpec.describe "the admin dashboard show" do
     @merchant = Merchant.create!(name: "Test Merchant")
     @item = Item.create!(name: "Test Item", description: "Test Description", unit_price: 100, merchant: @merchant)
 
-    @incomplete_invoice1 = Invoice.create!(customer: @test_customer, status: "in progress")
+    @incomplete_invoice1 = Invoice.create!(customer: @test_customer, created_at: 5.days.ago, status: "in progress")
     InvoiceItem.create!(invoice: @incomplete_invoice1, item: @item, status: "packaged")
-    @incomplete_invoice2 = Invoice.create!(customer: @test_customer, status: "in progress")
+    @incomplete_invoice2 = Invoice.create!(customer: @test_customer, created_at: 3.days.ago, status: "in progress")
     InvoiceItem.create!(invoice: @incomplete_invoice2, item: @item, status: "pending")
-    @complete_invoice = Invoice.create!(customer: @test_customer, status: "completed")
+    @incomplete_invoice3 = Invoice.create!(customer: @test_customer, created_at: 1.day.ago, status: "in progress")
+    InvoiceItem.create!(invoice: @incomplete_invoice3, item: @item, status: "pending")
+    @complete_invoice = Invoice.create!(customer: @test_customer, created_at: 1.day.ago, status: "completed")
     InvoiceItem.create!(invoice: @complete_invoice, item: @item, status: "shipped")
   end
 
   describe "User Story 19" do
     it "displays the header" do
-      visit "/admin/"
+      visit admin_dashboard_path
       expect(page).to have_content("Admin Dashboard")
     end
   end
@@ -54,6 +56,7 @@ RSpec.describe "the admin dashboard show" do
   describe "User Story 20" do
     it "has all necessary elements" do
       visit admin_dashboard_path
+
       expect(page).to have_selector("h1", text: "Little Esty Shop")
       expect(page).to have_selector("h1", text: "Admin Dashboard")
       expect(page).to have_link("Merchants", href: admin_merchants_path)
@@ -64,6 +67,7 @@ RSpec.describe "the admin dashboard show" do
   describe "User Story 21" do
     it "displays the top customers" do
       visit admin_dashboard_path
+
       within(".top-customers") do
         expect(page).to have_selector("h2", text: "Top Customers")
         expect(@customer5.first_name).to appear_before(@customer4.first_name)
@@ -78,11 +82,28 @@ RSpec.describe "the admin dashboard show" do
   describe "User Story 22" do
     it "lists the IDs of incomplete invoices as links" do
       visit admin_dashboard_path
+
       within(".incomplete-invoices") do
         expect(page).to have_selector("h2", text: "Incomplete Invoices")
         expect(page).to have_link(@incomplete_invoice1.id, href: admin_invoice_path(@incomplete_invoice1))
         expect(page).to have_link(@incomplete_invoice2.id, href: admin_invoice_path(@incomplete_invoice2))
         expect(page).not_to have_link(@complete_invoice.id, href: admin_invoice_path(@complete_invoice))
+      end
+    end
+  end
+
+  describe "User Story 23" do
+    it "lists the incomplete invoices in order from oldest to newest" do
+      visit admin_dashboard_path
+
+
+      within(".incomplete-invoices") do
+        expect(page).to have_content(@incomplete_invoice1.id)
+        expect(page).to have_content(@incomplete_invoice2.id)
+        expect(page).to have_content(@incomplete_invoice3.id)
+
+        expect(@incomplete_invoice1.id.to_s).to appear_before(@incomplete_invoice2.id.to_s)
+        expect(@incomplete_invoice2.id.to_s).to appear_before(@incomplete_invoice3.id.to_s)
       end
     end
   end
