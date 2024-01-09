0035ef098c46495ebc23259f1e6aaa2ff4987701
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-01-09 12:42:54 -0700

    test: Start the test writing for Merchant #revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoices).through(:items)}
     it { should have_many(:transactions).through(:invoices)}
   end
+
+  describe "#revenue" do
+    it "can calculate the total amount of sales for a merchant" do
+      # Notes on Revenue Calculation:
+      #   - Only invoices with at least one successful transaction should count towards revenue
+      #   - Revenue for an invoice should be calculated as the sum of the revenue of all invoice items
+      #   - Revenue for an invoice item should be calculated as the invoice item unit price multiplied by the quantity (do not use the item unit price)
+
+      
+    end
+  end
+
+  describe "#top_5_merchants" do
+    it "returns the top merchants with the highest revenue generated" do
+
+    end
+  end
 end
