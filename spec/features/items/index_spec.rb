4c8d82712b8c3c5b18e6831e6108b238b0759725
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-01-09 23:47:48 -0700

    add test for enable/disable buttons on merchant items index page

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -30,4 +30,25 @@ RSpec.describe "the merchant items index page" do
       expect(page).to have_content(@item_3.name)
     end
   end
+
+  describe "User Story 9" do
+    # As a merchant
+    # When I visit my items index page (/merchants/:merchant_id/items)
+    # Next to each item name I see a button to disable or enable that item.
+    # When I click this button
+    # Then I am redirected back to the items index
+    # And I see that the items status has changed
+
+    it "displays enable and disable buttons to update the item status" do
+      visit "/merchants/#{@merchant_1.id}/items"
+
+      expect(page).to have_button("Enable")
+
+      click_button("Enable")
+
+      expect(current_path).to_eq("/merchants/#{@merchant_1.id}/items")
+
+      expect(page).to have_button("Disable")
+    end
+  end
 end
