74a202ee85f5a581d572a7a923088af5fe9cf4e8
Quin Nordmark <quinnordmark@gmail.com>
2024-01-10 11:02:17 -0700

    Fixed US-08 error that popped up for unknown reaon

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -6,4 +6,15 @@ class Merchant::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(name: params[:name], description: params[:description], unit_price: params[:unit_price])
+    redirect_to "/merchants/#{item.merchant_id}/items/#{item.id}"
+    flash[:notice] = "Update successful!"
+  end
 end
