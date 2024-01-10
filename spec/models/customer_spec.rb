d5905ba25682b8d87dcae6416c276827ff75e51d
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-10 14:02:23 -0700

    Add ::top_customers_by_successful_transaction back to test file after realizing subtle difference b/w methods - 'top_cust' for admin dash returns top 5 customers in entire database, 'top_cust_per_merchant' returns the top five customers for each specific merchant.

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -9,9 +9,15 @@ describe Customer, type: :model do
   end
 
   describe "class methods" do 
-    describe "::top_five_by_transaction" do 
+    describe "::top_customers_by_successful_transaction" do 
       it "returns the top five customers with the most transactions" do 
-        expect(Customer.top_five_by_transaction).to match_array([@customers[4], @customers[7], @customers[1], @customers[3], @customers[5]])
+        expect(Customer.top_customers_by_successful_transaction).to match_array([@customers[4], @customers[7], @customers[1], @customers[3], @customers[5]])
+      end
+    end
+
+    describe "::top_five_cust_per_merchant" do 
+      it "returns the top five customers with the most transactions" do 
+        expect(Customer.top_five_cust_per_merchant(@merchant.id)).to match_array([@customers[4], @customers[7], @customers[1], @customers[3], @customers[5]])
       end
     end
   end
