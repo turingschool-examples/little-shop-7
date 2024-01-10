54f61093a8657161049142140f6073b5b66837b6
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-10 10:29:42 -0700

    Create ::top_five_by_transaction for that lists the top five customers by transax for the merchant dash view page

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,13 @@
 class Customer < ApplicationRecord
   has_many :invoices
-  
+
+  def self.top_five_by_transaction
+    Customer.joins(invoices: :transactions)
+      .select("customers.*, COUNT(transactions.id) AS num_trans")
+      .where("transactions.result = 'success'")
+      .group("customers.id")
+      .order("num_trans DESC")
+      .limit(5)
+  end
+
 end
