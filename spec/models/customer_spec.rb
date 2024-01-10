464ace28b7de1c2607371b4a0ae1cf2b28be2e16
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-10 10:32:30 -0700

    New ::top_five_by_transaction customer model test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,7 +1,18 @@
 require "rails_helper"
 
 describe Customer, type: :model do
+  before(:each) do 
+    load_data
+  end
   describe "relationships" do
     it { should have_many(:invoices) }
+  end
+
+  describe "class methods" do 
+    describe "::top_five_by_transaction" do 
+      it "returns the top five customers with the most transactions" do 
+        expect(Customer.top_five_by_transaction).to match_array([@customers[4], @customers[7], @customers[1], @customers[3], @customers[5]])
+      end
+    end
   end
 end
