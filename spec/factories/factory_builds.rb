e55022664697db895007dcfc5ff18e6f10cb210a
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-10 11:12:22 -0700

    Add _md to all factories I am using so as not to conflict with already registered factories

--- spec/factories/factory_builds.rb
+++ spec/factories/factory_builds.rb
@@ -1,26 +1,26 @@
 FactoryBot.define do
-  factory :merchant do
+  factory :merchant_md do
     name { Faker::Commerce.brand }
   end
 
-  factory :customer do 
+  factory :customer_md do 
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
   end
 
-  factory :item do 
+  factory :item_md do 
       name { Faker::Commerce.product_name }
       description { Faker::Commerce.promotion_code(digits: 0) }
       unit_price { rand(2000..99999) }
   end
 
-  factory :invoice do 
+  factory :invoice_md do 
       status { rand(0..2) }
       created_at { Faker::Date.backward(days: 365) }
       updated_at { Faker::Date.between(from: created_at, to: Date.today) }
   end
 
-  factory :transaction do
+  factory :transaction_md do
     association :invoice 
       credit_card_number { Faker::Business.credit_card_number }
       credit_card_expiration_date { Faker::Business.credit_card_expiry_date }
@@ -29,7 +29,7 @@ FactoryBot.define do
       updated_at { Faker::Date.between(from: created_at, to: Date.today) }
   end
 
-  factory :invoice_item do 
+  factory :invoice_item_md do 
       quantity {rand( 1..99) }
       unit_price { rand(5000..15000) }
       status { rand(0..2) }
