78e34fa7209b750d0babbda617e291b3d7519b0a
Matt Darlington <matthewsdarlington@gmail.com>
2024-01-11 05:23:30 -0700

    New class method ::top_5_by_revenue that lists in descending order the items that generate the most revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,13 @@ class Item < ApplicationRecord
   attribute :status, default: -> { 0 }
 
   enum status: {"Disabled" => 0, "Enabled" => 1}
+
+  def self.top_5_by_revenue 
+    self.joins(invoice_items: { invoice: :transactions })
+      .select("items.*, MAX(invoice_items.unit_price * quantity) AS max_rev")
+      .where("transactions.result = 1")
+      .group("items.id")
+      .order("max_rev DESC")
+      .limit(5)
+  end
 end
