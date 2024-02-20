c0e76e07075891fed49f028ddd49a8829e2482aa
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-20 14:31:17 -0800

    feat: Add validations, enum, and relationship to transaction

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
+RSpec.describe Transaction, type: :model do
+  describe 'Validations' do
+    it {should validate_presence_of :credit_card_number}
+    it {should validate_presence_of :credit_card_expiration_date}
+  end
 
+  describe 'Relationships' do
+    it {should belong_to :invoice}
+  end
+
+  describe 'Enums' do
+    xit 'enums tests' do
+    end
+  end
 end
