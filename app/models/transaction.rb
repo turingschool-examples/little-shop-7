5370e996120384e3108b046426df24d21823ddd7
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-20 15:54:35 -0800

    fix: Fix missing rails helper info

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,6 +3,7 @@ class Transaction < ApplicationRecord
 
   validates :credit_card_number, presence: true
   validates :credit_card_expiration_date, presence: true
+  validates :result, presence: true
 
-  enum status: ["success", "failed"]
+  enum result: ["success", "failed"]
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Transaction, type: :model do
   describe 'Validations' do
     it {should validate_presence_of :credit_card_number}
     it {should validate_presence_of :credit_card_expiration_date}
+    it {should validate_presence_of :result}
   end
 
   describe 'Relationships' do
