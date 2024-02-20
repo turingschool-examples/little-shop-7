d0b32bee6b43117c9338caa13a0a15eb55946249
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-20 14:16:13 -0800

    test: Add tests for ralationships and validations to invoice_item

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Relationships" do
+    it {should belong_to :item}
+    it {should belong_to :invoice}
+  end
+
+  describe "Validations" do
+    it {should validate_presence_of :quantity}
+    it {should validate_presence_of :unit_price}
+    it {should validate_presence_of :status}
+  end
+
+  describe "Enums" do
+    xit "enum tests" do
+    end
+  end
 end
