2e9ebb16c55926aaefb98e811c412a5a329fd6cb
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-21 14:24:58 -0800

    feat: Add incomplete_invoices class method to invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,16 @@
 class Invoice < ApplicationRecord
   belongs_to :customer
   has_many :transactions, dependent: :destroy
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items
 
   validates :status, presence: true
   
   enum status: ["In Progress", "Cancelled", "Completed"]
+
+  # class method for checking status of invoice
+  def self.incomplete_invoices
+    Invoice.select("invoices.*").joins(:invoice_items).where("invoice_items.status != 2")
+  end
+
 end
