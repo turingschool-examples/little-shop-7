b65bf5feb001cbaee02d4fad656ffd892b35f160
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-20 21:07:20 -0700

    Feat: Add instance method successful transactions for Customer Model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,8 @@ class Customer < ApplicationRecord
   def self.top_customers
     Customer.select("customers.*, count(transactions.result = 0) as successful_transactions").joins(:transactions).group('customers.id').order('successful_transactions desc').limit(5)
   end
+
+  def successful_transactions_count 
+    transactions.count("transactions.result = 0")
+  end
 end
