20189a85ee9e6eb8598b7f48a631aa9262fbdd31
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-21 13:50:32 -0800

    test: Add tests for admin dashboard to view all invoices with items that have not currently shipped and links to those invoice admin show pages, US 22

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     item { association :item }
     invoice { association :invoice }
     quantity { Faker::Number.number(digits: 1) }
-    unit_price {  }
+    unit_price { 1 }
     status { 1 }
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :item do
     name { Faker::Commerce.product_name }
-    description { Faker::Commerce.product_description }
-    unit_price { Faker::Commerce.price(100..99999).to_i }
+    description { Faker::Commerce.material }
+    unit_price { Faker::Commerce.price(range: 100..99999).to_i }
     merchant { association :merchant }
   end
 end
