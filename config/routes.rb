72ca56ddc29f38f6db165719ebc1bda5cca882bd
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-22 14:53:38 -0700

    Routes: add routing for edit and update merchant from admin nested controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :admin, only: :index
 
   namespace :admin do
-    resources :merchants, only: [:index, :show]
+    resources :merchants, only: [:index, :show, :edit, :update]
   end
 
   resources :merchants do 
