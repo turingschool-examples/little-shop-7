6c0c0718fa06f1a27b3b4781d88820333e6cfd07
Hoa Dam <hoangocdam@gmail.com>
2024-02-22 11:00:54 -0800

    feat & test: user story 4-5

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :merchants, only: [] do
     resources :dashboard, only: [:show]
     resources :items, only: [:index]
-    resources :invoices, only: [:index]
+    resources :invoices, only: [:index, :show]
   end
 
   namespace :admin do
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:invoices).through(:items) }
     it { should have_many(:customers).through(:invoices) }
     it { should have_many(:transactions).through(:customers) }
+    it { should have_many(:invoice_items).through(:items) }
   end
 
   describe "validations" do
@@ -35,6 +36,26 @@ RSpec.describe Merchant, type: :model do
     end
     it "returns the top 5 customers with their success transactions counts in descending order" do
       expect(merchant.top_customers).to match_array(top_customers)
+    end
+  end
+
+  describe "#items_ready_to_ship" do
+    let(:merchant) { FactoryBot.create(:merchant) }
+    let(:item_1) { FactoryBot.create(:item, merchant: merchant) }
+    let(:item_2) { FactoryBot.create(:item, merchant: merchant) }
+    let(:item_3) { FactoryBot.create(:item, merchant: merchant) }
+    let(:customer) { FactoryBot.create(:customer) }
+
+    it "returns the array of invoice_items that do not have status 'shipped' and in ascending order of invoice created_at" do
+      invoice_1 = FactoryBot.create(:invoice, customer: customer)
+      invoice_2 = FactoryBot.create(:invoice, customer: customer)
+      invoice_3 = FactoryBot.create(:invoice, customer: customer)
+
+      invoice_item_1 = InvoiceItem.create!(invoice: invoice_1, item: item_1, status: "pending")
+      invoice_item_2 = InvoiceItem.create!(invoice: invoice_2, item: item_2, status: "packaged")
+      invoice_item_3 = InvoiceItem.create!(invoice: invoice_3, item: item_3, status: "shipped")
+
+      expect(merchant.items_ready_to_ship).to eq([invoice_item_2])
     end
   end
 end
