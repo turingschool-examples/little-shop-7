50854c7e35b5b4c57a387f4d4d9e976a50bcd61f
Odell McFarland IV <odell.mac4@gmail.com>
2024-02-21 18:41:07 -0800

    Feat: admin merchants index shows the name of each merchant

--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Admin dashboard' do
         it 'displays admin dashboard header' do
             # As an admin,
             # When I visit the admin dashboard (/admin)
-            visit admin_index_path
+            visit admin_path
             # Then I see a header indicating that I am on the admin dashboard
             expect(page).to have_content("Admin Dashboard")
         end
@@ -15,11 +15,11 @@ RSpec.describe 'Admin dashboard' do
         it 'displays admin dashboard links to merchant and invoice index' do
             # As an admin,
             # When I visit the admin dashboard (/admin)
-            visit admin_index_path
+            visit admin_path
             # Then I see a link to the admin merchants index (/admin/merchants)
-            expect(page).to have_link("Merchants", href: "/admin/merchants")
+            expect(page).to have_link("Merchants", href: admin_merchants_path)
             # And I see a link to the admin invoices index (/admin/invoices)
-            expect(page).to have_link("Invoices", href: "/admin/invoices")
+            expect(page).to have_link("Invoices", href: admin_invoices_path)
         end
     end
 end
---
+++ spec/features/admin/merchants/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin merchants index' do
+    describe 'User story 24' do
+        it 'shows the name of each merchant' do
+            odell = Merchant.create!(name: "Odell")
+            nico = Merchant.create!(name: "Nico")
+            # As an admin,
+            # When I visit the admin merchants index (/admin/merchants)
+            visit admin_merchants_path
+            # Then I see the name of each merchant in the system
+            expect(page).to have_content("Nico")
+            expect(page).to have_content("Odell")
+        end
+    end
+end
