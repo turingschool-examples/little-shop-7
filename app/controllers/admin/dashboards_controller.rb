4a2045b942b8d1b8e4d019b7524609958c2306bc
Odell McFarland IV <odell.mac4@gmail.com>
2024-02-21 18:07:08 -0800

    displays admin dashboard links to merchants and invoices index

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,5 +1,4 @@
 class Admin::DashboardsController < ApplicationController
     def index
-        
     end
 end
--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -5,9 +5,21 @@ RSpec.describe 'Admin dashboard' do
         it 'displays admin dashboard header' do
             # As an admin,
             # When I visit the admin dashboard (/admin)
-            visit "/admin"
+            visit admin_index_path
             # Then I see a header indicating that I am on the admin dashboard
             expect(page).to have_content("Admin Dashboard")
+        end
+    end
+
+    describe 'User story 20' do
+        it 'displays admin dashboard links to merchant and invoice index' do
+            # As an admin,
+            # When I visit the admin dashboard (/admin)
+            visit admin_index_path
+            # Then I see a link to the admin merchants index (/admin/merchants)
+            expect(page).to have_link("Merchants", href: "/admin/merchants")
+            # And I see a link to the admin invoices index (/admin/invoices)
+            expect(page).to have_link("Invoices", href: "/admin/invoices")
         end
     end
 end
