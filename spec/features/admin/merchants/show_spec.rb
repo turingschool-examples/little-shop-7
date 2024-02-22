d4c5a9fb322caaa432ea52a2956e000dd7707329
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-22 14:52:01 -0700

    Test: displays a link to update merchants information, and updates it successfully for Admin Merchants Show, user story 26

--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -58,7 +58,7 @@ RSpec.describe 'Admin Merchants Show', type: :feature do
   end
 
   #User Story 25. Admin Merchant Show
-  it '' do
+  it 'displays merchants name' do
     # As an admin, When I click on the name of a merchant from the admin merchants index page (/admin/merchants),
     visit admin_merchants_path
     click_link("Amazon")
@@ -66,6 +66,20 @@ RSpec.describe 'Admin Merchants Show', type: :feature do
     expect(current_path).to eq(admin_merchant_path(@merchant_1.id))
     # And I see the name of that merchant    
     expect(page).to have_content("Amazon")
+  end
+
+  #User Story 26.
+  it 'displays a link to update merchants information, and updates it successfully' do
+    # As an admin,
+    # When I visit a merchant's admin show page (/admin/merchants/:merchant_id)
+    visit admin_merchant_path(@merchant_1.id)
+    # Then I see a link to update the merchant's information. When I click the link
+
+    click_link("Update #{@merchant_1.name}'s Information")
+    # Then I am taken to a page to edit this merchant
+    expect(current_path).to eq(edit_admin_merchant_path(@merchant_1.id))
+
+    #Test continues in spec/features/admin/merchants/edit_spec.rb
   end
  end
 end
