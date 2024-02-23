1c9da9d93fc6f63fd037ca9516a5c1624cc2988d
Hoa Dam <hoangocdam@gmail.com>
2024-02-23 08:23:45 -0800

    feat:controller and views

---
+++ app/controllers/invoice_items_controller.rb
@@ -0,0 +1,17 @@
+class InvoiceItemsController < ApplicationController
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    invoice_item = InvoiceItem.find(params[:id])
+    if invoice_item.update(invoice_item_params)
+      flash[:notice] = "Item #{invoice_item.item.name} is updated successfully"
+      redirect_to merchant_invoice_path(merchant, invoice_item.invoice)
+
+    end
+  end
+
+  private
+
+  def invoice_item_params
+    params.require(:invoice_item).permit(:status)
+  end
+end
--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -8,5 +8,6 @@ class InvoicesController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = @merchant.invoices.find(params[:id])
     @invoice_items = @invoice.invoice_items.joins(:item).where(items: {merchant_id: @merchant.id})
+    @total_revenue = @invoice_items.sum("invoice_items.unit_price * quantity")
   end
 end
