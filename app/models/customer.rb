5113946668d4c9fec63775ee609f66edf8c76e9e
Martin Chavez <martinchavez1203@gmail.com>
2024-02-23 16:01:29 -0700

    Add successful_trans_count & top_five_customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,16 @@ class Customer < ApplicationRecord
    has_many :invoices
    has_many :items, through: :invoices
    has_many :transactions, through: :invoices
+   
+   def self.top_five_customers
+      Customer.select("customers.*, count(transactions.id) as count_transactions")
+      .joins(:transactions).where(transactions: {result: "success"})
+      .group('customers.id')
+      .order("count_transactions DESC")
+      .limit(5)
+   end
+
+   def successful_trans_count
+      transactions.success.count
+   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,14 @@ class Merchant < ApplicationRecord
    has_many :items
    has_many :invoices, through: :items
    has_many :customers, through: :invoices
+
+   def top_five_customers
+      Customer.select("customers.*, count(transactions.id) as count_transactions")
+      .joins(:transactions).where(transactions: {result: "success"})
+      .group('customers.id')
+      .order("count_transactions DESC")
+      .limit(5)
+   end
+
+   
 end
