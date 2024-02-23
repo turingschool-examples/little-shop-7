55b4aeed1ae09d48d6d1a6590d152a6e69f1fe85
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-02-23 10:54:04 -0700

    user story 10 complete with spec

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Dashboard Show Page' do
   let!(:merchant_1) { Merchant.create!(name: "John Doe")}
+
   let!(:item1) { merchant_1.items.create!(name: "test", description: "im a good item", unit_price: 31211)}
-  let!(:item2) { merchant_1.items.create!(name: "test2", description: "im a better item", unit_price: 41211)}
+  let!(:item2) { merchant_1.items.create!(name: "test2", description: "im a better item", unit_price: 41211, status: 1)}
   describe "merchant index" do
     it "shows specific merchant items" do
       visit merchant_items_path(merchant_id: merchant_1.id)
@@ -29,6 +30,23 @@ RSpec.describe 'Merchant Dashboard Show Page' do
       expect(current_path).to eq(merchant_items_path(merchant_id: merchant_1.id))
     
 
-  end
+    end
+
+    it "separates disabled and enabled" do
+      visit merchant_items_path(merchant_id: merchant_1.id)
+      
+      within("#disabled") do
+      
+
+        expect(page).to have_button("Enable")
+      end
+      within("#enabled") do
+      
+
+        expect(page).to have_button("Disable")
+      end
+      
+
+    end
   end
 end
