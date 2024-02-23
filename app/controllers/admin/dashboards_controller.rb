3ebb0c183f46011983d56b171971c1ff056c3ead
Odell McFarland IV <odell.mac4@gmail.com>
2024-02-22 16:00:41 -0800

    Feat: displays top 5 customers out all customers not just one specific merchant

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardsController < ApplicationController
     def index
+        # require'pry';binding.pry
+        @top_customers = Merchant.top_customers
     end
 end
--- spec/features/admin/index_spec.rb
+++ spec/features/admin/index_spec.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin dashboard' do
+    let(:merchant) { FactoryBot.create(:merchant) }
+    let(:item) { FactoryBot.create(:item, merchant: merchant) }
+    let(:top_customers) { FactoryBot.create_list(:customer, 5) }
+    let(:customers) { FactoryBot.create_list(:customer, 5) }
+
+    before do
+        top_customers.each do |customer|
+          invoice = FactoryBot.create(:invoice, customer: customer)
+          invoice.items = [item]
+          FactoryBot.create_list(:transaction, Random.rand(2..5), invoice: invoice)
+          invoice.save!
+        end
+        customers.each do |customer|
+          invoice = FactoryBot.create(:invoice, customer: customer)
+          invoice.items = [item]
+          FactoryBot.create_list(:transaction, 1, invoice: invoice)
+          invoice.save!
+        end
+    end
+
     describe 'User story 19' do
         it 'displays admin dashboard header' do
             # As an admin,
             # When I visit the admin dashboard (/admin)
             visit admin_path
             # Then I see a header indicating that I am on the admin dashboard
-            expect(page).to have_content("Admin Dashboard")
+            expect(page).to have_content("Little Etsy Shop Admin Dashboard")
         end
     end
 
@@ -20,6 +40,26 @@ RSpec.describe 'Admin dashboard' do
             expect(page).to have_link("Merchants", href: admin_merchants_path)
             # And I see a link to the admin invoices index (/admin/invoices)
             expect(page).to have_link("Invoices", href: admin_invoices_path)
+        end
+    end
+
+    describe 'User Story 21' do
+        
+        it 'displays top 5 customers and count of successful transactions' do
+            # As an admin,
+            # When I visit the admin dashboard (/admin)
+            visit admin_path
+            # Then I see the names of the top 5 customers
+            # who have conducted the largest number of successful transactions
+            # And next to each customer name I see the number of successful transactions they have
+            # conducted
+            expect(page).to have_content("Little Etsy's Favorite Customers")
+
+            top_customers.each do |top_customer|
+                within "#top_customer-#{top_customer.id}" do
+                    expect(page).to have_content("#{top_customer.first_name} #{top_customer.last_name}: #{top_customer.transactions.count} transactions")
+                end
+            end
         end
     end
 end
