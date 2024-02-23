43af3944e0e8665803d363dc460a74e4ce7079fd
Hoa Dam <hoangocdam@gmail.com>
2024-02-23 08:22:23 -0800

    feat:test user_story 18

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'Merchant Invoice Show Page' do
 
     describe "User Story 15" do
       it "shows information related to the invoice" do
-        visit merchant_invoice_path(merchant_id: merchant.id, id: invoice.id)
+        visit merchant_invoice_path(merchant, invoice)
 
         expect(page).to have_content(invoice.id)
         expect(page).to have_content(invoice.status)
@@ -30,20 +30,46 @@ RSpec.describe 'Merchant Invoice Show Page' do
 
     describe "User Story 16" do
       it "shows all of the items on the invoice with the items' information" do
-        visit merchant_invoice_path(merchant_id: merchant.id, id: invoice.id)
+        visit merchant_invoice_path(merchant, invoice)
 
         expect(page).to have_content(item_1.name)
         expect(page).to have_content(invoice_item_1.quantity)
         expect(page).to have_content(invoice_item_1.unit_price)
-        expect(page).to have_content(invoice_item_1.status)
+
         expect(page).to have_content(item_2.name)
         expect(page).to have_content(invoice_item_2.quantity)
         expect(page).to have_content(invoice_item_2.unit_price)
-        expect(page).to have_content(invoice_item_2.status)
+
         expect(page).to have_content(item_3.name)
         expect(page).to have_content(invoice_item_3.quantity)
         expect(page).to have_content(invoice_item_3.unit_price)
-        expect(page).to have_content(invoice_item_3.status)
+      end
+    end
+
+    describe "User Story 17" do
+      it "shows the total revenue of the invoice" do
+        visit merchant_invoice_path(merchant, invoice)
+
+        expect(page).to have_content("Total Revenue: 85")
+      end
+    end
+
+    describe "User Story 18" do
+      before do
+        visit merchant_invoice_path(merchant, invoice)
+      end
+      it "shows each invoice item's current status is a select field, when I click this select field I can select a new status for the Item, shows a button to 'Update Item Status' when I click this button I am taken back to the invoice show page and my Item's status has now been updated" do
+        visit merchant_invoice_path(merchant, invoice)
+        within "#item_#{invoice_item_1.item.id}" do
+        expect(page).to have_select("invoice_item_status", selected: "Packaged")
+        save_and_open_page
+        select "Shipped", from: "invoice_item_status"
+        click_on "Update Item Status"
+
+        expect(current_path).to eq(merchant_invoice_path(merchant, invoice))
+        expect(page).to have_content("Item #{item_1.name} is updated successfully")
+        expect(page).to have_select("invoice_item_status", selected: "Shipped")
+        end
       end
     end
   end
