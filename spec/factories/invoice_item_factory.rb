d49b78578e6800dcc0d9a7ae530ebf007d4983e1
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-02-23 17:48:24 -0500

    Changed status on line 6 invoice_item_factory.rb

--- spec/factories/invoice_item_factory.rb
+++ spec/factories/invoice_item_factory.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :invoice_item do
-    
+
     quantity { Faker::Number.between(from: 1, to: 100) }
     unit_price { Faker::Number.between(from: 1, to: 100) }
-    status { ['pending', 'shipped', 'completed'].sample }
+    status { ['pending', 'packaged', 'shipped'] }
 
     association :item, factory: :item
     association :invoice, factory: :invoice
