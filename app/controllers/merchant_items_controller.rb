a09f8750fb35fb0840079f023dc14120230d5b4b
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-02-23 18:15:30 -0500

    Finish us 8

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -5,5 +5,23 @@ class MerchantItemsController < ApplicationController
 
   def show 
     @item = Merchant.find(params[:merchant_id]).items.find(params[:id])
+
+  end
+
+  def edit
+    @merch = Merchant.find(params[:merchant_id])
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Merchant.find(params[:merchant_id]).items.find(params[:id])
+    Item.update(strong_params)
+    flash[:alert] = "Update successful"
+    redirect_to merchant_item_path(params[:merchant_id], params[:id])
+  end
+
+  private 
+  def strong_params
+    params.permit(:name, :description, :unit_price)
   end
 end
