d34b869de06079b5d5d7907523767e85e7576d39
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-02-24 17:56:34 -0500

    Still broken

--- app/controllers/merchant_invoices_controller.rb
+++ app/controllers/merchant_invoices_controller.rb
@@ -1,3 +1,10 @@
 class MerchantInvoicesController < ApplicationController
-  
+  def index
+    @invoices = Merchant.find(params[:merchant_id]).invoices
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
 end
