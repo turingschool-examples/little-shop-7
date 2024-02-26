a8d6d9fc46c725d3164c85aaf71170225499a847
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-02-26 11:58:09 -0700

    Feat: add change status method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,7 @@ class Merchant < ApplicationRecord
    has_many :customers, through: :invoices
    
    validates :name, presence: true
+   enum :status, [ "enabled", "disabled"]
 
    def top_five_customers
       Customer.select("customers.*, count(transactions.id) as count_transactions")
@@ -12,4 +13,13 @@ class Merchant < ApplicationRecord
       .order("count_transactions DESC")
       .limit(5)
    end
+
+
+   def change_status
+      if params[:change_status].present?
+      new_status = self.status == 'enabled' ? 'disabled' : 'enabled'
+      update(status: new_status)
+      end
+   end
+
 end
