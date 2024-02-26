e0caa533d5c826ad8307a0a7cf1ef96a015bd5b4
Mel Langhoff <mlanghoff@uwalumni.com>
2024-02-26 14:06:57 -0700

    test:create spec

---
+++ spec/features/admin/merchants/create_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin merchants create page' do
+    describe 'user story 29' do
+        it 'has a form to create a new merchant' do
+            odell = Merchant.create!(name: "Odell")
+            nico = Merchant.create!(name: "Nico")
+
+            visit '/admin/merchants'
+
+            click_on 'Create Merchant'
+
+            # checks for form on page
+            expect(page).to have_selector('form')
+            expect(page).to have_button('Submit')
+        end
+
+        it 'can create a new merchant and redirect to index with new merch disabled' do
+            odell = Merchant.create!(name: "Odell")
+            nico = Merchant.create!(name: "Nico")
+
+            visit '/admin/merchants'
+
+            click_on 'Create Merchant'
+
+            fill_in 'Name', with: 'Merchant Name'
+
+            click_on 'Submit'
+
+            # within new merchant?
+            expect(page).to have_content("Merchant Name")
+            expect(page).to have_content("disabled")
+        end
+    end
+end
