ff93a7139f2c943c5a9466a460ed62ca62552d23
Odell McFarland IV <odell.mac4@gmail.com>
2024-02-27 08:30:12 -0800

    resolve merge conflict

--- spec/factories/invoice_item_factory.rb
+++ spec/factories/invoice_item_factory.rb
@@ -1,14 +1,4 @@
 FactoryBot.define do
-<<<<<<< HEAD
-    factory :invoice_item do
-      association :item 
-      association :invoice 
-      quantity { 1 }
-      unit_price { 10 }
-      status { "pending" }
-    end
-  end
-=======
   factory :invoice_item do
 
     quantity { Faker::Number.between(from: 1, to: 100) }
@@ -19,4 +9,3 @@ FactoryBot.define do
     association :invoice, factory: :invoice
   end
 end
->>>>>>> 82a3c8bd6be4c994523b1c89b4446485ba465964
