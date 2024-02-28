330281dddabce67f6cef2d33c152e75c7957b54d
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-02-28 13:48:22 -0700

    Test for paths after clicking links

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Welcome Index', type: :feature do
   describe 'As anyone' do
-    it "displays links to merchants index and admin index" do
+    it "displays links to merchants index and admin index, and after clicking both takes us to the correct path" do
       visit "/"
-      save_and_open_page
       expect(page).to have_link("Admin")
       expect(page).to have_link("Merchant")
+
+      click_link("Admin")
+      expect(current_path).to eq(admin_index_path)
+
+      click_link("Merchant")
+      expect(current_path).to eq(merchants_path)
     end
   end
 end
