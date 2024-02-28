62e8bb0b0fd3c565ed199821eb927e45b6d81f4b
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-28 12:24:49 -0800

    refactor: Refactor top_five_merchants and top_revenue_day in merchant model to utilize built in model associations

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,40 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  # before(:each) do 
-  #   @cust_1 = create(:customer)
-  #   @cust_2 = create(:customer)
-  #   @cust_3 = create(:customer)
-  #   @cust_4 = create(:customer)
-  #   @cust_5 = create(:customer)
-  #   @cust_6 = create(:customer)
-    
-  #   @invoice_1 = create(:invoice, customer_id: @cust_1.id)
-  #   @invoice_2 = create(:invoice, customer_id: @cust_2.id)
-  #   @invoice_3 = create(:invoice, customer_id: @cust_3.id)
-  #   @invoice_4 = create(:invoice, customer_id: @cust_4.id)
-  #   @invoice_5 = create(:invoice, customer_id: @cust_5.id)
-  #   @invoice_6 = create(:invoice, customer_id: @cust_6.id)
-    
-  #   @trans_1 = create(:transaction, invoice_id: @invoice_1.id)
-  #   @trans_2 = create(:transaction, invoice_id: @invoice_2.id)
-  #   @trans_3 = create(:transaction, invoice_id: @invoice_3.id)
-  #   @trans_4 = create(:transaction, invoice_id: @invoice_4.id)
-  #   @trans_5 = create(:transaction, invoice_id: @invoice_5.id)
-  #   @trans_6 = create(:transaction, invoice_id: @invoice_6.id)
-    
-  #   @merch_1 = create(:merchant, name: "Amazon") 
-
-  #   @item_1 = create(:item, unit_price: 1, merchant_id: @merch_1.id)
-
-  #   create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice_1.id, unit_price: 1, quantity: 100, status: 2)
-  #   create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice_2.id, unit_price: 1, quantity: 80, status: 2)
-  #   create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice_3.id, unit_price: 1, quantity: 60, status: 2)
-  #   create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice_4.id, unit_price: 1, quantity: 50, status: 2)
-  #   create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice_5.id, unit_price: 1, quantity: 40, status: 2)
-  #   create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice_6.id, unit_price: 1, quantity: 5)
-  # end 
-
   before(:each) do
     @customer_1 = create(:customer)
     @customer_2 = create(:customer)
